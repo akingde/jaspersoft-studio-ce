b98de3179b329493c713c3293436b0cef7264789
chicuslavic <chicuslavic@tibco.com>
2010-08-27 14:30:32 +0000

    datasources
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@83 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/editor/outline/ATreeEditPart.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/editor/outline/ATreeEditPart.java
@@ -1,25 +1,21 @@
 /*
- * Jaspersoft Open Studio - Eclipse-based JasperReports Designer.
- * Copyright (C) 2005 - 2010 Jaspersoft Corporation. All rights reserved.
- * http://www.jaspersoft.com
- *
- * Unless you have purchased a commercial license agreement from Jaspersoft,
- * the following license terms apply:
- *
+ * Jaspersoft Open Studio - Eclipse-based JasperReports Designer. Copyright (C) 2005 - 2010 Jaspersoft Corporation. All
+ * rights reserved. http://www.jaspersoft.com
+ * 
+ * Unless you have purchased a commercial license agreement from Jaspersoft, the following license terms apply:
+ * 
  * This program is part of Jaspersoft Open Studio.
- *
- * Jaspersoft Open Studio is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jaspersoft Open Studio is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with Jaspersoft Open Studio. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Jaspersoft Open Studio is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * Jaspersoft Open Studio is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with Jaspersoft Open Studio. If not,
+ * see <http://www.gnu.org/licenses/>.
  */
 package com.jaspersoft.studio.editor.outline;
 
@@ -46,27 +42,35 @@ import com.jaspersoft.studio.model.INode;
  */
 public class ATreeEditPart extends AbstractTreeEditPart implements PropertyChangeListener {
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.gef.editparts.AbstractEditPart#activate()
 	 */
 	@Override
 	public void activate() {
 		super.activate();
-		((ANode) getModel()).getPropertyChangeSupport().addPropertyChangeListener(this);
+		if (getModel() != null)
+			((ANode) getModel()).getPropertyChangeSupport().addPropertyChangeListener(this);
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.gef.editparts.AbstractEditPart#deactivate()
 	 */
 	@Override
 	public void deactivate() {
-		((ANode) getModel()).getPropertyChangeSupport().removePropertyChangeListener(this);
+		if (getModel() != null)
+			((ANode) getModel()).getPropertyChangeSupport().removePropertyChangeListener(this);
 		super.deactivate();
 
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.gef.editparts.AbstractTreeEditPart#createEditPolicies()
 	 */
 	protected void createEditPolicies() {
@@ -74,7 +78,9 @@ public class ATreeEditPart extends AbstractTreeEditPart implements PropertyChang
 		installEditPolicy(EditPolicy.PRIMARY_DRAG_ROLE, new ElementTreeEditPolicy());
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.gef.editparts.AbstractTreeEditPart#refreshVisuals()
 	 */
 	@Override
@@ -103,7 +109,9 @@ public class ATreeEditPart extends AbstractTreeEditPart implements PropertyChang
 		setWidgetText(displayText);
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see org.eclipse.gef.editparts.AbstractEditPart#getModelChildren()
 	 */
 	@Override
@@ -115,7 +123,9 @@ public class ATreeEditPart extends AbstractTreeEditPart implements PropertyChang
 		return list;
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)
 	 */
 	@Override
