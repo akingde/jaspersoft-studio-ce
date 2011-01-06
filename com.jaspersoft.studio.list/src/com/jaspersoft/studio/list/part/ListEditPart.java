2235e9ab0b520b0f5aae7ef5be1b3b609fa4b930
chicuslavic <chicuslavic@tibco.com>
2011-01-06 13:59:56 +0000

    fixed frame elements filling
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@306 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.list/src/com/jaspersoft/studio/list/part/ListEditPart.java
+++ com.jaspersoft.studio.list/src/com/jaspersoft/studio/list/part/ListEditPart.java
@@ -1,13 +1,9 @@
 package com.jaspersoft.studio.list.part;
 
-import org.eclipse.draw2d.IFigure;
-import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.gef.EditPolicy;
 
 import com.jaspersoft.studio.editor.gef.parts.EditableFigureEditPart;
 import com.jaspersoft.studio.editor.gef.parts.editPolicy.ElementEditPolicy;
-import com.jaspersoft.studio.model.ANode;
-import com.jaspersoft.studio.model.MPage;
 
 public class ListEditPart extends EditableFigureEditPart {
 
@@ -21,13 +17,4 @@ public class ListEditPart extends EditableFigureEditPart {
 		installEditPolicy(EditPolicy.COMPONENT_ROLE, new ElementEditPolicy());
 	}
 
-	@Override
-	protected void setupFigure(IFigure rect) {
-		super.setupFigure(rect);
-		if (((ANode) getModel()).getParent() instanceof MPage) {
-			// TODO maximum components size?
-			Dimension dr = rect.getSize();
-			rect.setSize(Math.max(dr.width, 0) + 2, Math.max(dr.height, 0) + 2);
-		}
-	}
 }
