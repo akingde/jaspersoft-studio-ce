8cc161442b1c4d9240ba0471c1b5efa37d192873
chicuslavic <chicuslavic@tibco.com>
2011-01-06 16:07:30 +0000

    release 0.0.8
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@309 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.table/src/com/jaspersoft/studio/table/part/TableEditPart.java
+++ com.jaspersoft.studio.table/src/com/jaspersoft/studio/table/part/TableEditPart.java
@@ -1,9 +1,14 @@
 package com.jaspersoft.studio.table.part;
 
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.gef.EditPolicy;
 
 import com.jaspersoft.studio.editor.gef.parts.EditableFigureEditPart;
 import com.jaspersoft.studio.editor.gef.parts.editPolicy.ElementEditPolicy;
+import com.jaspersoft.studio.model.ANode;
+import com.jaspersoft.studio.model.MPage;
+import com.jaspersoft.studio.table.model.MTable;
 
 public class TableEditPart extends EditableFigureEditPart {
 
@@ -17,4 +22,14 @@ public class TableEditPart extends EditableFigureEditPart {
 		installEditPolicy(EditPolicy.COMPONENT_ROLE, new ElementEditPolicy());
 	}
 
+	@Override
+	protected void setupFigure(IFigure rect) {
+		super.setupFigure(rect);
+		if (((ANode) getModel()).getParent() instanceof MPage) {
+			MTable m = (MTable) getModel();
+			Dimension d = m.getTableManager().getSize();
+			Dimension dr = rect.getSize();
+			rect.setSize(Math.max(dr.width, d.width) + 2, Math.max(dr.height, d.height) + 2);
+		}
+	}
 }
