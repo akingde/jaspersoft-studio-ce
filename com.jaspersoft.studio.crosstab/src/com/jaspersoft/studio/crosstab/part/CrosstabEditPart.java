5f82468fb23f0037c39f2700658d2c83fa8eb8fc
chicuslavic <chicuslavic@tibco.com>
2011-01-06 16:07:44 +0000

    release 0.0.8
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@310 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.crosstab/src/com/jaspersoft/studio/crosstab/part/CrosstabEditPart.java
+++ com.jaspersoft.studio.crosstab/src/com/jaspersoft/studio/crosstab/part/CrosstabEditPart.java
@@ -1,9 +1,14 @@
 package com.jaspersoft.studio.crosstab.part;
 
+import org.eclipse.draw2d.IFigure;
+import org.eclipse.draw2d.geometry.Dimension;
 import org.eclipse.gef.EditPolicy;
 
+import com.jaspersoft.studio.crosstab.model.MCrosstab;
 import com.jaspersoft.studio.editor.gef.parts.EditableFigureEditPart;
 import com.jaspersoft.studio.editor.gef.parts.editPolicy.ElementEditPolicy;
+import com.jaspersoft.studio.model.ANode;
+import com.jaspersoft.studio.model.MPage;
 
 public class CrosstabEditPart extends EditableFigureEditPart {
 
@@ -15,6 +20,17 @@ public class CrosstabEditPart extends EditableFigureEditPart {
 	@Override
 	protected void createEditPolicies() {
 		installEditPolicy(EditPolicy.COMPONENT_ROLE, new ElementEditPolicy());
+	}
+
+	@Override
+	protected void setupFigure(IFigure rect) {
+		super.setupFigure(rect);
+		if (((ANode) getModel()).getParent() instanceof MPage) {
+			MCrosstab m = (MCrosstab) getModel();
+			Dimension d = m.getCrosstabManager().getSize();
+			Dimension dr = rect.getSize();
+			rect.setSize(Math.max(dr.width, d.width) + 2, Math.max(dr.height, d.height) + 2);
+		}
 	}
 
 }
