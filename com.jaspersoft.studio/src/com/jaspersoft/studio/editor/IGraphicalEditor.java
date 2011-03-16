845788991dcda822c8bdf57b8a6e2c47e56aa516
chicuslavic <chicuslavic@tibco.com>
2011-03-16 14:30:26 +0000

    graphical editor interface
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@470 6833c83e-4fd0-4c68-a3df-283c6148db30

---
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/editor/IGraphicalEditor.java
@@ -0,0 +1,23 @@
+package com.jaspersoft.studio.editor;
+
+import org.eclipse.draw2d.FigureCanvas;
+import org.eclipse.gef.DefaultEditDomain;
+import org.eclipse.gef.GraphicalViewer;
+import org.eclipse.gef.ui.actions.ActionRegistry;
+import org.eclipse.gef.ui.parts.SelectionSynchronizer;
+
+import com.jaspersoft.studio.model.INode;
+
+public interface IGraphicalEditor {
+	public ActionRegistry getActionRegistry();
+
+	public DefaultEditDomain getEditDomain();
+
+	public INode getModel();
+
+	public GraphicalViewer getGraphicalViewer();
+
+	public SelectionSynchronizer getSelectionSynchronizer();
+
+	public FigureCanvas getEditor();
+}
