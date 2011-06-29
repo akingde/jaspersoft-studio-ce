651c672c2dcbea68cecc81db01ac28744c764320
chicuslavic <chicuslavic@tibco.com>
2011-06-29 13:38:34 +0000

    release 0.2.6
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@646 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/data/wizard/pages/DataAdaptersListPage.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/data/wizard/pages/DataAdaptersListPage.java
@@ -1,125 +1,138 @@
 /*
- * JasperReports - Free Java Reporting Library.
- * Copyright (C) 2001 - 2009 Jaspersoft Corporation. All rights reserved.
+ * JasperReports - Free Java Reporting Library. Copyright (C) 2001 - 2009 Jaspersoft Corporation. All rights reserved.
  * http://www.jaspersoft.com
- *
- * Unless you have purchased a commercial license agreement from Jaspersoft,
- * the following license terms apply:
- *
- * This program is part of JasperReports.
- *
- * JasperReports is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * JasperReports is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
  * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with JasperReports. If not, see <http://www.gnu.org/licenses/>.
+ * Unless you have purchased a commercial license agreement from Jaspersoft, the following license terms apply:
+ * 
+ * This program is part of JasperReports.
+ * 
+ * JasperReports is free software: you can redistribute it and/or modify it under the terms of the GNU Lesser General
+ * Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any
+ * later version.
+ * 
+ * JasperReports is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with JasperReports. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
 package com.jaspersoft.studio.data.wizard.pages;
 
-import org.eclipse.jface.viewers.ListViewer;
+import org.eclipse.jface.viewers.ColumnWeightData;
+import org.eclipse.jface.viewers.TableLayout;
+import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.jface.wizard.WizardPage;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.events.SelectionListener;
-import org.eclipse.swt.layout.FillLayout;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.List;
+import org.eclipse.swt.widgets.Table;
+import org.eclipse.swt.widgets.TableColumn;
 import org.eclipse.ui.PlatformUI;
 
 import com.jaspersoft.studio.data.DataAdapterFactory;
 import com.jaspersoft.studio.data.DataAdapterManager;
+import com.jaspersoft.studio.swt.widgets.table.ListContentProvider;
 
 public class DataAdaptersListPage extends WizardPage {
 
-	
-	List list = null;
 	java.util.List<DataAdapterFactory> dataAdapterFactories = null;
-		
+
+	private TableViewer tviewer;
+	private Table wtable;
+
 	/**
 	 * Create the wizard.
 	 */
 	public DataAdaptersListPage() {
 		super("dataAdapterslistpage");
-		setTitle("DataAdapters");
+		setTitle("Data Adapters");
 		setDescription("Select a DataAdapter");
 		setPageComplete(false);
 	}
 
 	/**
 	 * Create contents of the wizard.
+	 * 
 	 * @param parent
 	 */
 	public void createControl(Composite parent) {
 		Composite container = new Composite(parent, SWT.NULL);
 
 		setControl(container);
-		container.setLayout(new FillLayout(SWT.HORIZONTAL));
-		
-		
-		
-		ListViewer listViewer = new ListViewer(container, SWT.BORDER | SWT.V_SCROLL);
-		list = listViewer.getList();
-		
-		list.addSelectionListener(new SelectionListener() {
-			
+		container.setLayout(new GridLayout());
+
+		wtable = new Table(container, SWT.V_SCROLL | SWT.SINGLE | SWT.FULL_SELECTION | SWT.BORDER);
+		GridData gd = new GridData(GridData.FILL_BOTH);
+		gd.widthHint = 100;
+		gd.heightHint = 250;
+		wtable.setLayoutData(gd);
+		wtable.setHeaderVisible(false);
+
+		TableColumn[] col = new TableColumn[1];
+		col[0] = new TableColumn(wtable, SWT.NONE);
+		col[0].setText("Jar Files Path");
+		col[0].pack();
+
+		TableLayout tlayout = new TableLayout();
+		tlayout.addColumnData(new ColumnWeightData(100, false));
+		wtable.setLayout(tlayout);
+
+		tviewer = new TableViewer(wtable);
+		tviewer.setContentProvider(new ListContentProvider());
+		tviewer.setLabelProvider(new LLabelProvider());
+		wtable.addSelectionListener(new SelectionListener() {
+
 			public void widgetSelected(SelectionEvent e) {
-				factorySelected(e);
+				int ind = wtable.getSelectionIndex();
+				if (ind >= 0 && ind < dataAdapterFactories.size())
+					setMessage(dataAdapterFactories.get(ind).getDescription());
 			}
-			
+
 			public void widgetDefaultSelected(SelectionEvent e) {
-				factorySelected(e);
+				widgetSelected(e);
 			}
 		});
-		
-	  // Fill the list with the data adapters
-		updateFactoriesList(list);
-		
-		if (list.getItemCount() > 0)
-		{
-			list.setSelection(0);
-			setPageComplete( list.getSelectionCount() > 0);
+		updateFactoriesList();
+
+		if (dataAdapterFactories.size() > 0) {
+			wtable.setSelection(0);
+			setPageComplete(wtable.getSelectionCount() > 0);
+			setMessage(dataAdapterFactories.get(0).getDescription());
 		}
 	}
-	
-	public boolean canFlipToNextPage(){
-		return (list.getSelectionCount() == 1) ? true : false;
+
+	@Override
+	public boolean canFlipToNextPage() {
+		return (wtable.getSelectionCount() == 1) ? true : false;
 	}
-	
+
 	protected void factorySelected(SelectionEvent e) {
-			setPageComplete( list.getSelectionCount() > 0);
+		setPageComplete(wtable.getSelectionCount() > 0);
 	}
 
-	
-	private void updateFactoriesList(List list) {
-		
-		list.removeAll();
-
+	private void updateFactoriesList() {
 		dataAdapterFactories = DataAdapterManager.getDataAdapterFactories();
-		
-		for (DataAdapterFactory dataAdapterFactory : dataAdapterFactories) {
-			list.add(dataAdapterFactory.getLabel());
-		}
+
+		tviewer.setInput(dataAdapterFactories);
 	}
 
 	public DataAdapterFactory getSelectedFactory() {
-		
-		if (dataAdapterFactories == null) return null; // Should never be true
-		if (list.getSelectionIndex() < 0) return null; // Should never be true
-		
-		return dataAdapterFactories.get( list.getSelectionIndex() );
+
+		if (dataAdapterFactories == null)
+			return null; // Should never be true
+		if (wtable.getSelectionIndex() < 0)
+			return null; // Should never be true
+
+		return dataAdapterFactories.get(wtable.getSelectionIndex());
 	}
-	
+
 	@Override
 	public void performHelp() {
-		
-				PlatformUI.getWorkbench().getHelpSystem().displayHelp("com.jaspersoft.studio.doc.dataAdapters_wizard_list");
+		PlatformUI.getWorkbench().getHelpSystem().displayHelp("com.jaspersoft.studio.doc.dataAdapters_wizard_list");
 
 	}
 }
---
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/data/wizard/pages/LLabelProvider.java
@@ -0,0 +1,22 @@
+package com.jaspersoft.studio.data.wizard.pages;
+
+import org.eclipse.jface.viewers.LabelProvider;
+import org.eclipse.swt.graphics.Image;
+
+import com.jaspersoft.studio.data.DataAdapterFactory;
+
+public class LLabelProvider extends LabelProvider {
+	@Override
+	public Image getImage(Object element) {
+		if (element instanceof DataAdapterFactory)
+			return ((DataAdapterFactory) element).getIcon(16);
+		return super.getImage(element);
+	}
+
+	@Override
+	public String getText(Object element) {
+		if (element instanceof DataAdapterFactory)
+			return ((DataAdapterFactory) element).getLabel();
+		return super.getText(element);
+	}
+}
