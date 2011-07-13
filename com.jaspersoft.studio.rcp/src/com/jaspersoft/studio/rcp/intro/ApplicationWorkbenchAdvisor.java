f4b2292508062ac470b7978e6da23c698bd663d2
chicuslavic <chicuslavic@tibco.com>
2011-07-13 12:04:42 +0000

    bug about wrong project icon
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@756 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.rcp/src/com/jaspersoft/studio/rcp/intro/ApplicationWorkbenchAdvisor.java
+++ com.jaspersoft.studio.rcp/src/com/jaspersoft/studio/rcp/intro/ApplicationWorkbenchAdvisor.java
@@ -23,52 +23,98 @@
  */
 package com.jaspersoft.studio.rcp.intro;
 
+import java.net.URL;
+
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.application.IWorkbenchConfigurer;
 import org.eclipse.ui.application.IWorkbenchWindowConfigurer;
 import org.eclipse.ui.application.WorkbenchAdvisor;
 import org.eclipse.ui.application.WorkbenchWindowAdvisor;
 import org.eclipse.ui.ide.IDE;
+import org.eclipse.ui.internal.ide.IDEInternalWorkbenchImages;
+import org.eclipse.ui.internal.ide.IDEWorkbenchPlugin;
+import org.osgi.framework.Bundle;
 
 import com.jaspersoft.studio.ReportDesignPerspective;
 import com.jaspersoft.studio.rcp.OpenDocumentEventProcessor;
 
 public class ApplicationWorkbenchAdvisor extends WorkbenchAdvisor {
 
-	
 	private OpenDocumentEventProcessor openDocProcessor;
-	 	
-	public ApplicationWorkbenchAdvisor(OpenDocumentEventProcessor openDocProcessor) {
-	 	this.openDocProcessor = openDocProcessor;
+
+	public ApplicationWorkbenchAdvisor(
+			OpenDocumentEventProcessor openDocProcessor) {
+		this.openDocProcessor = openDocProcessor;
 	}
-	
-    @Override
-	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(IWorkbenchWindowConfigurer configurer) {
-        return new ApplicationWorkbenchWindowAdvisor(configurer);
-    }
-    
-    @Override
+
+	@Override
+	public WorkbenchWindowAdvisor createWorkbenchWindowAdvisor(
+			IWorkbenchWindowConfigurer configurer) {
+		return new ApplicationWorkbenchWindowAdvisor(configurer);
+	}
+
+	@Override
 	public void initialize(IWorkbenchConfigurer configurer) {
-        super.initialize(configurer);
-        configurer.setSaveAndRestore(true);
-    }
+		super.initialize(configurer);
+		configurer.setSaveAndRestore(true);
+
+		final String ICONS_PATH = "icons/full/";
+		final String PATH_OBJECT = ICONS_PATH + "obj16/";
+		Bundle ideBundle = Platform.getBundle(IDEWorkbenchPlugin.IDE_WORKBENCH);
+		declareWorkbenchImage(configurer, ideBundle,
+				IDE.SharedImages.IMG_OBJ_PROJECT, PATH_OBJECT + "prj_obj.gif",
+				true);
+		declareWorkbenchImage(configurer, ideBundle,
+				IDE.SharedImages.IMG_OBJ_PROJECT_CLOSED, PATH_OBJECT
+						+ "cprj_obj.gif", true);
+		declareWorkbenchImage(configurer, ideBundle,
+				IDEInternalWorkbenchImages.IMG_DLGBAN_SAVEAS_DLG, PATH_OBJECT
+						+ "saveas_wiz.png", false);
+		declareWorkbenchImage(configurer, ideBundle,
+				IDE.SharedImages.IMG_OBJ_PROJECT, PATH_OBJECT + "prj_obj.gif",
+				true);
+		declareWorkbenchImage(configurer, ideBundle,
+				IDE.SharedImages.IMG_OBJ_PROJECT_CLOSED, PATH_OBJECT
+						+ "cprj_obj.gif", true);
+
+	}
+
+	private void declareWorkbenchImage(IWorkbenchConfigurer configurer_p,
+			Bundle ideBundle, String symbolicName, String path, boolean shared) {
+		URL url = ideBundle.getEntry(path);
+		ImageDescriptor desc = ImageDescriptor.createFromURL(url);
+		configurer_p.declareImage(symbolicName, desc, shared);
+	}
+
+	@Override
+	public IAdaptable getDefaultPageInput() {
+		IWorkspace workspace = ResourcesPlugin.getWorkspace();
+		return workspace.getRoot();
+	}
 
 	@Override
 	public String getInitialWindowPerspectiveId() {
-		return ReportDesignPerspective.ID;//FIXME export this from studio plugin? 
+		return ReportDesignPerspective.ID;// FIXME export this from studio
+											// plugin?
 	}
-	
+
 	@Override
 	public void preStartup() {
 		super.preStartup();
-		
-		IDE.registerAdapters(); 
+
+		IDE.registerAdapters();
+
 	}
-	
+
 	/**
-	  * Added to process SWT.OpenDocument events.
-	  * Here we actually process the OpenDocument events.
-	  */
+	 * Added to process SWT.OpenDocument events. Here we actually process the
+	 * OpenDocument events.
+	 */
 	@Override
 	public void eventLoopIdle(Display display) {
 		openDocProcessor.openFiles();
