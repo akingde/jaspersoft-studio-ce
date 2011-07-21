db48c333c01885461f0def30b4bf11c4f71b4118
chicuslavic <chicuslavic@tibco.com>
2011-07-21 11:58:01 +0000

    release 1.0.1
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@767 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/swt/widgets/PropertiesComponent.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/swt/widgets/PropertiesComponent.java
@@ -1,9 +1,33 @@
+/*
+ * Jaspersoft Open Studio - Eclipse-based JasperReports Designer.
+ * Copyright (C) 2005 - 2010 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ *
+ * Unless you have purchased a commercial license agreement from Jaspersoft,
+ * the following license terms apply:
+ *
+ * This program is part of Jaspersoft Open Studio.
+ *
+ * Jaspersoft Open Studio is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Jaspersoft Open Studio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with Jaspersoft Open Studio. If not, see <http://www.gnu.org/licenses/>.
+ */
 package com.jaspersoft.studio.swt.widgets;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Map.Entry;
-import java.util.Properties;
 
 import org.eclipse.jface.viewers.CellEditor;
 import org.eclipse.jface.viewers.ColumnWeightData;
@@ -58,22 +82,22 @@ public class PropertiesComponent {
 		}
 	}
 
-	private Properties properties;
+	private Map<String, String> properties;
 
-	public Properties getProperties() {
+	public Map<String, String> getProperties() {
 		List<Property> list = (List<Property>) tviewer.getInput();
 		for (Property prop : list)
 			properties.put(prop.key, prop.value);
 		return properties;
 	}
 
-	public void setProperties(Properties properties) {
+	public void setProperties(Map<String, String> properties) {
 		if (properties == null)
-			properties = new Properties();
+			properties = new HashMap<String, String>();
 		this.properties = properties;
 		List<Property> list = new ArrayList<Property>();
-		for (Entry<Object, Object> entry : properties.entrySet())
-			list.add(new Property(entry.getKey().toString(), entry.getValue().toString()));
+		for (Entry<String, String> entry : properties.entrySet())
+			list.add(new Property(entry.getKey(), entry.getValue()));
 
 		tviewer.setInput(list);
 	}
