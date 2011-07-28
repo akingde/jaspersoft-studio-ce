a82f11591157cdc964f5089a4e8f073a0249779a
chicuslavic <chicuslavic@tibco.com>
2011-07-28 14:19:40 +0000

    release 1.0.1
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@775 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.rcp/src/com/jaspersoft/studio/rcp/heartbeat/Heartbeat.java
+++ com.jaspersoft.studio.rcp/src/com/jaspersoft/studio/rcp/heartbeat/Heartbeat.java
@@ -38,7 +38,7 @@ import com.jaspersoft.studio.preferences.util.PropertiesHelper;
 
 public class Heartbeat {
 	private static final String UUID_PROPERTY = "UUID";
-	public static final String VERSION = "0.0.9";
+	public static final String VERSION = "1.0.1";
 	private static String version;
 	private static String optmsg;
 
