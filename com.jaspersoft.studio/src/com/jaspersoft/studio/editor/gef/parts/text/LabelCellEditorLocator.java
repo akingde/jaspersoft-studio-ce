54f41da748396246c200fb5a21267bf6f35934cd
chicuslavic <chicuslavic@tibco.com>
2011-08-31 16:04:29 +0000

    set some minimum width and height
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@819 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/editor/gef/parts/text/LabelCellEditorLocator.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/editor/gef/parts/text/LabelCellEditorLocator.java
@@ -18,27 +18,23 @@
  * see <http://www.gnu.org/licenses/>.
  */
 /*
- * Jaspersoft Open Studio - Eclipse-based JasperReports Designer.
- * Copyright (C) 2005 - 2010 Jaspersoft Corporation. All rights reserved.
- * http://www.jaspersoft.com
- *
- * Unless you have purchased a commercial license agreement from Jaspersoft,
- * the following license terms apply:
- *
+ * Jaspersoft Open Studio - Eclipse-based JasperReports Designer. Copyright (C) 2005 - 2010 Jaspersoft Corporation. All
+ * rights reserved. http://www.jaspersoft.com
+ * 
+ * Unless you have purchased a commercial license agreement from Jaspersoft, the following license terms apply:
+ * 
  * This program is part of Jaspersoft Open Studio.
- *
- * Jaspersoft Open Studio is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jaspersoft Open Studio is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with Jaspersoft Open Studio. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Jaspersoft Open Studio is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * Jaspersoft Open Studio is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with Jaspersoft Open Studio. If not,
+ * see <http://www.gnu.org/licenses/>.
  */
 package com.jaspersoft.studio.editor.gef.parts.text;
 
@@ -62,8 +58,8 @@ final public class LabelCellEditorLocator implements CellEditorLocator {
 		figure.translateToAbsolute(rect);
 		org.eclipse.swt.graphics.Rectangle trim = text.computeTrim(0, 0, 0, 0);
 		rect.translate(trim.x, trim.y);
-		rect.width += trim.width;
-		rect.height += trim.height;
+		rect.width += Math.max(trim.width, 100);
+		rect.height += Math.max(trim.height, 30);
 		text.setBounds(rect.x, rect.y, rect.width, rect.height);
 	}
 
