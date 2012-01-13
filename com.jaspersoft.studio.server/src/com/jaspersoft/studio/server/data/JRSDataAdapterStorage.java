adc3b0bdbdfb870fc48bc300733c6cf9f80c2ad5
chicuslavic <chicuslavic@tibco.com>
2012-01-13 09:32:44 +0000

    fixed some DataAdapter problems
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@971 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/data/JRSDataAdapterStorage.java
+++ com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/data/JRSDataAdapterStorage.java
@@ -1,29 +1,23 @@
 package com.jaspersoft.studio.server.data;
 
-import java.util.List;
-
-import net.sf.jasperreports.data.DataAdapter;
-
+import com.jaspersoft.studio.data.DataAdapterDescriptor;
 import com.jaspersoft.studio.data.storage.ADataAdapterStorage;
 
 public class JRSDataAdapterStorage extends ADataAdapterStorage {
 
 	@Override
-	public void write(DataAdapter dataAdapter) {
+	public void findAll() {
 		// TODO Auto-generated method stub
-		
 	}
 
 	@Override
-	public DataAdapter read() {
+	public void save(String url, DataAdapterDescriptor adapter) {
 		// TODO Auto-generated method stub
-		return null;
 	}
 
 	@Override
-	public List<DataAdapter> getDataAdapterDescriptors() {
+	public void delete(String url) {
 		// TODO Auto-generated method stub
-		return null;
 	}
 
 }
