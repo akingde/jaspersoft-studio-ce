f3c7adfd430d0368b0e1973c5c320f37e1f4125d
mrabbi <mrabbi@tibco.com>
2012-05-11 12:40:27 +0000

    Move project to trunk
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@1163 6833c83e-4fd0-4c68-a3df-283c6148db30

---
+++ com.jaspersoft.studio.editor.jrexpressions.ui/src/com/jaspersoft/studio/editor/jrexpressions/ui/support/ObjectsNavigatorContentProvider.java
@@ -0,0 +1,63 @@
+package com.jaspersoft.studio.editor.jrexpressions.ui.support;
+
+import org.eclipse.jface.viewers.ITreeContentProvider;
+import org.eclipse.jface.viewers.Viewer;
+
+/**
+ * Content provider for the navigator tree containing the categories of object
+ * items.
+ * 
+ * @author Massimo Rabbi (mrabbi@users.sourceforge.net)
+ * 
+ * @see DefaultExpressionEditorComposite Example of usage of the content
+ *      provider
+ * 
+ */
+public class ObjectsNavigatorContentProvider implements ITreeContentProvider {
+
+	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
+	}
+
+	public void dispose() {
+	}
+
+	public boolean hasChildren(Object element) {
+		return getChildren(element).length > 0;
+	}
+
+	public Object getParent(Object element) {
+		return null;
+	}
+
+	public Object[] getElements(Object inputElement) {
+		// The passed element will be NameIconItem representing the objects root
+		// categories
+		// Usually these are the root categories (most of the times a subset of
+		// them)
+		// - Parameters
+		// - Field
+		// - Variables
+		// - CrossTabs
+		// - Built-in functions
+		// - User Defined Expressions
+		// - Recent Expressions
+		if (inputElement != null
+				&& inputElement instanceof ObjectCategoryItem[]) {
+			return (Object[]) inputElement;
+		}
+		return new Object[0];
+	}
+
+	public Object[] getChildren(Object parentElement) {
+
+		if (parentElement instanceof ObjectCategoryItem) {
+			Object data = ((ObjectCategoryItem) parentElement).getData();
+			if (data != null && data instanceof ObjectCategoryItem[]) {
+				return (ObjectCategoryItem[]) data;
+			}
+		}
+
+		return new Object[0];
+	}
+
+}
