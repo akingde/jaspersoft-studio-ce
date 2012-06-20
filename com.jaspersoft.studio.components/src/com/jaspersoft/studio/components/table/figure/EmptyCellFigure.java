cd8050cc2afd1e01a3c99d5e0ded1dc2c8827367
chicuslavic <chicuslavic@tibco.com>
2012-06-20 18:11:16 +0000

    table empty cell figure like in iReport
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@1298 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/table/figure/EmptyCellFigure.java
+++ com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/table/figure/EmptyCellFigure.java
@@ -19,25 +19,35 @@
  */
 package com.jaspersoft.studio.components.table.figure;
 
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.TexturePaint;
+import java.awt.image.BufferedImage;
+
 import net.sf.jasperreports.components.table.StandardBaseColumn;
 import net.sf.jasperreports.engine.export.draw.DrawVisitor;
 
 import org.eclipse.draw2d.ColorConstants;
 import org.eclipse.draw2d.Graphics;
-import org.eclipse.draw2d.LineBorder;
 import org.eclipse.draw2d.geometry.Rectangle;
 import org.eclipse.gef.handles.HandleBounds;
 
 import com.jaspersoft.studio.editor.gef.figures.FrameFigure;
+import com.jaspersoft.studio.editor.java2d.J2DGraphics;
 
 public class EmptyCellFigure extends FrameFigure {
+	private static final Color COLOR1 = new Color(224, 224, 224);
+	private static final Color COLOR2 = new Color(255, 255, 255);
 	private StandardBaseColumn column;
 
 	public EmptyCellFigure() {
 		super();
 		setOpaque(true);
-		setBackgroundColor(ColorConstants.lightGray);
-		setBorder(new LineBorder(1));
+		setAlpha(10);
+		setBackgroundColor(ColorConstants.white);
+		setBorder(null);
+		createTexture();
 	}
 
 	public void setJRElement(StandardBaseColumn column,
@@ -65,9 +75,37 @@ public class EmptyCellFigure extends FrameFigure {
 		Rectangle b = (this instanceof HandleBounds) ? ((HandleBounds) this)
 				.getHandleBounds() : this.getBounds();
 
-		graphics.setBackgroundColor(getBackgroundColor());
-		graphics.fillRectangle(b.x, b.y, b.width, b.height);
+		Graphics2D g = ((J2DGraphics) graphics).getGraphics2D();
+		g.setPaint(tp);
+		g.fillRect(b.x, b.y, b.width - 1, b.height - 1);
 
 		paintBorder(graphics);
+	}
+
+	private TexturePaint tp;
+
+	public TexturePaint createTexture() {
+		if (tp == null) {
+			int gridSize = 10;
+			BufferedImage img = new BufferedImage(gridSize * 2, gridSize * 2,
+					BufferedImage.TYPE_INT_ARGB);
+			Graphics2D g2 = img.createGraphics();
+
+			AlphaComposite ac = AlphaComposite.getInstance(
+					AlphaComposite.SRC_OVER, 0.2f);
+			g2.setComposite(ac);
+
+			g2.setColor(COLOR1);
+			g2.fillRect(0, 0, 10, 10);
+			g2.fillRect(10, 10, 10, 10);
+
+			g2.setColor(COLOR2);
+			g2.fillRect(10, 0, 10, 10);
+			g2.fillRect(0, 10, 10, 10);
+
+			tp = new TexturePaint(img, new java.awt.Rectangle(0, 0, gridSize,
+					gridSize));
+		}
+		return tp;
 	}
 }
