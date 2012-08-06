2b983324c1156b2cd4b461ccb5f7a8eec29942aa
morlandin <morlandin@tibco.com>
2012-08-06 12:47:26 +0000

    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@1415 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/editor/gef/decorator/error/PositionErrorDecorator.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/editor/gef/decorator/error/PositionErrorDecorator.java
@@ -25,6 +25,9 @@ package com.jaspersoft.studio.editor.gef.decorator.error;
 
 import java.util.List;
 
+import net.sf.jasperreports.engine.JRBand;
+import net.sf.jasperreports.engine.design.JRDesignElement;
+
 import org.eclipse.gef.EditPartViewer;
 import org.eclipse.gef.GraphicalViewer;
 import org.eclipse.gef.ui.actions.ActionRegistry;
@@ -34,28 +37,64 @@ import org.eclipse.jface.action.MenuManager;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.actions.RetargetAction;
 
-import com.jaspersoft.studio.editor.gef.decorator.ErrorDecorator;
 import com.jaspersoft.studio.editor.gef.decorator.IDecorator;
 import com.jaspersoft.studio.editor.gef.decorator.IElementDecorator;
 import com.jaspersoft.studio.editor.gef.decorator.pdf.ShowPDFTagsAction;
 import com.jaspersoft.studio.editor.gef.figures.ComponentFigure;
 import com.jaspersoft.studio.editor.gef.parts.FigureEditPart;
 import com.jaspersoft.studio.messages.Messages;
+import com.jaspersoft.studio.model.APropertyNode;
+import com.jaspersoft.studio.model.MGraphicElement;
 
 public class PositionErrorDecorator implements IElementDecorator {
 
-	private IDecorator decorator;
+	private IDecorator decorator = null;
 
 	@Override
 	public void setupFigure(ComponentFigure fig, FigureEditPart editPart) {
-		decorator = new ErrorDecorator(editPart.getModel());
+		
 		//It will remove the type ErrorDecorator from the list because equals is override in ErrorDecorator
 		fig.removeDecorator(decorator);
 		// check if we should show this decorator
 		if ((Boolean)editPart.getViewer().getProperty(ShowErrorsAction.ID)) {
-			fig.addDecorator(decorator);
+			if (decorator == null){
+				decorator = new ErrorDecorator();
+			}
+			if (!isValidPosition(fig, (MGraphicElement)editPart.getModel())){
+				fig.addDecorator(decorator);
+			}		
 		}
 	}
+	
+	/**
+	 * Evaluate if an element is between the bounds of the father
+	 * @param fig The moved graphical element.
+	 * @param itemModel the model associated to the moved item
+	 * @return true if the element position is valid (so no warning should be shown), false otherwise
+	 */
+	private boolean isValidPosition(ComponentFigure fig, MGraphicElement itemModel)
+  {
+      if (itemModel.getParent() instanceof APropertyNode){
+      	APropertyNode fatherModel = (APropertyNode) itemModel.getParent();
+	      Integer height = (Integer)itemModel.getPropertyValue(JRDesignElement.PROPERTY_HEIGHT);
+	      Integer width = (Integer)itemModel.getPropertyValue(JRDesignElement.PROPERTY_WIDTH);
+	      Integer x = (Integer)itemModel.getPropertyValue(JRDesignElement.PROPERTY_X);
+	      Integer y = (Integer)itemModel.getPropertyValue(JRDesignElement.PROPERTY_Y);
+	      Integer father_height = (Integer)fatherModel.getPropertyValue(JRDesignElement.PROPERTY_HEIGHT);	      
+	      if (fig.getJrElement().getElementGroup() instanceof JRBand)
+        {
+	      		Integer father_width = itemModel.getRoot().getJasperDesign().getColumnWidth();
+            return y + height<=father_height;
+        } else if (fig.getJrElement().getElementGroup() != null){
+        	Integer father_width = (Integer)fatherModel.getPropertyValue(JRDesignElement.PROPERTY_WIDTH);
+			    return father_height >= height + y &&
+			           x >= 0 &&
+			           y >= 0 &&
+			           father_width >= x + width;
+	      }
+      }
+      return true; 
+  }
 
 	@Override
 	public void registerActions(ActionRegistry registry, List<String> selectionActions, GraphicalViewer gviewer,
