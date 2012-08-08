c9b06a48ddc6d518b720bcba6b261de1164bdc56
chicuslavic <chicuslavic@tibco.com>
2012-08-08 12:33:19 +0000

    fixed handling inside container elements
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@1449 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/editor/gef/decorator/error/PositionErrorDecorator.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/editor/gef/decorator/error/PositionErrorDecorator.java
@@ -17,6 +17,7 @@ package com.jaspersoft.studio.editor.gef.decorator.error;
 import java.util.List;
 
 import net.sf.jasperreports.engine.JRBand;
+import net.sf.jasperreports.engine.design.JRDesignBand;
 import net.sf.jasperreports.engine.design.JRDesignElement;
 
 import org.eclipse.gef.EditPartViewer;
@@ -35,6 +36,7 @@ import com.jaspersoft.studio.editor.gef.parts.FigureEditPart;
 import com.jaspersoft.studio.messages.Messages;
 import com.jaspersoft.studio.model.APropertyNode;
 import com.jaspersoft.studio.model.MGraphicElement;
+import com.jaspersoft.studio.model.band.MBand;
 
 public class PositionErrorDecorator implements IElementDecorator {
 
@@ -42,49 +44,60 @@ public class PositionErrorDecorator implements IElementDecorator {
 
 	@Override
 	public void setupFigure(ComponentFigure fig, FigureEditPart editPart) {
-		
-		//It will remove the type ErrorDecorator from the list because equals is override in ErrorDecorator
+
+		// It will remove the type ErrorDecorator from the list because equals is override in ErrorDecorator
 		fig.removeDecorator(decorator);
 		// check if we should show this decorator
-		if ((Boolean)editPart.getViewer().getProperty(ShowErrorsAction.ID)) {
-			if (decorator == null){
+		if ((Boolean) editPart.getViewer().getProperty(ShowErrorsAction.ID)) {
+			if (decorator == null) {
 				decorator = new ErrorDecorator();
 			}
-			if (!isValidPosition(fig, (MGraphicElement)editPart.getModel())){
+			if (!isValidPosition(fig, (MGraphicElement) editPart.getModel())) {
 				fig.addDecorator(decorator);
-			}		
+			}
 		}
 	}
-	
+
 	/**
 	 * Evaluate if an element is between the bounds of the father
-	 * @param fig The moved graphical element.
-	 * @param itemModel the model associated to the moved item
+	 * 
+	 * @param fig
+	 *          The moved graphical element.
+	 * @param itemModel
+	 *          the model associated to the moved item
 	 * @return true if the element position is valid (so no warning should be shown), false otherwise
 	 */
-	private boolean isValidPosition(ComponentFigure fig, MGraphicElement itemModel)
-  {
-      if (itemModel.getParent() instanceof APropertyNode){
-      	APropertyNode fatherModel = (APropertyNode) itemModel.getParent();
-	      Integer height = (Integer)itemModel.getPropertyValue(JRDesignElement.PROPERTY_HEIGHT);
-	      Integer width = (Integer)itemModel.getPropertyValue(JRDesignElement.PROPERTY_WIDTH);
-	      Integer x = (Integer)itemModel.getPropertyValue(JRDesignElement.PROPERTY_X);
-	      Integer y = (Integer)itemModel.getPropertyValue(JRDesignElement.PROPERTY_Y);
-	      Integer father_height = (Integer)fatherModel.getPropertyValue(JRDesignElement.PROPERTY_HEIGHT);	      
-	      if (fig.getJrElement().getElementGroup() instanceof JRBand)
-        {
-	      		//Integer father_width = itemModel.getRoot().getJasperDesign().getColumnWidth();
-            return y + height<=father_height;
-        } else if (fig.getJrElement().getElementGroup() != null){
-        	Integer father_width = (Integer)fatherModel.getPropertyValue(JRDesignElement.PROPERTY_WIDTH);
-			    return father_height >= height + y &&
-			           x >= 0 &&
-			           y >= 0 &&
-			           father_width >= x + width;
-	      }
-      }
-      return true; 
-  }
+	private boolean isValidPosition(ComponentFigure fig, MGraphicElement itemModel) {
+		if (itemModel.getParent() instanceof APropertyNode) {
+			JRDesignElement item = itemModel.getValue();
+			int x = item.getX();
+			int y = item.getY();
+			int w = item.getWidth();
+			int h = item.getHeight();
+
+			int fh = Integer.MAX_VALUE;
+			int fw = Integer.MAX_VALUE;
+
+			if (itemModel.getParent() instanceof MGraphicElement) {
+				MGraphicElement fatherModel = (MGraphicElement) itemModel.getParent();
+				JRDesignElement fitem = fatherModel.getValue();
+
+				fh = fitem.getHeight();
+				fw = fitem.getWidth();
+			} else if (itemModel.getParent() instanceof MBand) {
+				JRDesignBand band = ((MBand) itemModel.getParent()).getValue();
+				fh = band.getHeight();
+				fw = itemModel.getJasperDesign().getPageWidth();
+			}
+			if (fig.getJrElement().getElementGroup() instanceof JRBand) {
+				// Integer father_width = itemModel.getRoot().getJasperDesign().getColumnWidth();
+				return y + h <= fh;
+			} else if (fig.getJrElement().getElementGroup() != null) {
+				return fh >= h + y && x >= 0 && y >= 0 && fw >= x + w;
+			}
+		}
+		return true;
+	}
 
 	@Override
 	public void registerActions(ActionRegistry registry, List<String> selectionActions, GraphicalViewer gviewer,
