5e2c85ff247fd1515be8e4a856f59f0f18383c03
chicuslavic <chicuslavic@tibco.com>
2012-08-22 14:49:45 +0000

    Bug 28975
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@1482 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/model/subreport/command/wizard/NewSubreportPage.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/model/subreport/command/wizard/NewSubreportPage.java
@@ -1,25 +1,21 @@
 /*
- * Jaspersoft Open Studio - Eclipse-based JasperReports Designer.
- * Copyright (C) 2005 - 2010 Jaspersoft Corporation. All rights reserved.
- * http://www.jaspersoft.com
- *
- * Unless you have purchased a commercial license agreement from Jaspersoft,
- * the following license terms apply:
- *
+ * Jaspersoft Open Studio - Eclipse-based JasperReports Designer. Copyright (C) 2005 - 2010 Jaspersoft Corporation. All
+ * rights reserved. http://www.jaspersoft.com
+ * 
+ * Unless you have purchased a commercial license agreement from Jaspersoft, the following license terms apply:
+ * 
  * This program is part of Jaspersoft Open Studio.
- *
- * Jaspersoft Open Studio is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Jaspersoft Open Studio is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with Jaspersoft Open Studio. If not, see <http://www.gnu.org/licenses/>.
+ * 
+ * Jaspersoft Open Studio is free software: you can redistribute it and/or modify it under the terms of the GNU Affero
+ * General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ * 
+ * Jaspersoft Open Studio is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
+ * implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License
+ * for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License along with Jaspersoft Open Studio. If not,
+ * see <http://www.gnu.org/licenses/>.
  */
 package com.jaspersoft.studio.model.subreport.command.wizard;
 
@@ -76,7 +72,7 @@ import com.jaspersoft.studio.wizards.AWizardNode;
 import com.jaspersoft.studio.wizards.JSSWizardSelectionPage;
 import com.jaspersoft.studio.wizards.ReportNewWizard;
 
-public class NewSubreportPage extends JSSWizardSelectionPage implements IExpressionContextSetter{
+public class NewSubreportPage extends JSSWizardSelectionPage implements IExpressionContextSetter {
 
 	private Button useReport;
 	private WTextExpression useReportPath;
@@ -128,7 +124,7 @@ public class NewSubreportPage extends JSSWizardSelectionPage implements IExpress
 				setPageComplete(!(useReportPath.getExpression() == null || useReportPath.getExpression().getText().isEmpty()));
 			}
 		});
-		if(expContext!=null){
+		if (expContext != null) {
 			useReportPath.setExpressionContext(expContext);
 		}
 
@@ -207,7 +203,7 @@ public class NewSubreportPage extends JSSWizardSelectionPage implements IExpress
 		IFile contextfile = (IFile) subreport.getJasperConfiguration().get(IEditorContributor.KEY_FILE);
 		String filepath = null;
 		if (contextfile != null && file.getProject().equals(contextfile.getProject())) {
-			filepath = file.getProjectRelativePath().toPortableString();
+			filepath = file.getProjectRelativePath().toPortableString().replaceAll(file.getProject().getName() + "/", "");
 		} else {
 			filepath = file.getRawLocationURI().toASCIIString();
 		}
@@ -267,8 +263,8 @@ public class NewSubreportPage extends JSSWizardSelectionPage implements IExpress
 	}
 
 	public void setExpressionContext(ExpressionContext expContext) {
-		this.expContext=expContext;
-		if(useReportPath!=null){
+		this.expContext = expContext;
+		if (useReportPath != null) {
 			useReportPath.setExpressionContext(this.expContext);
 		}
 	}
