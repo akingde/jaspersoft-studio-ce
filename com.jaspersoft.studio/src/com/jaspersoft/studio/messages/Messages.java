643d8b0f39d83e32f0a3dda5fb877663cd42c43f
morlandin <morlandin@tibco.com>
2012-09-13 17:19:56 +0000

    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@1621 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/messages/Messages.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/messages/Messages.java
@@ -1186,6 +1186,7 @@ public class Messages extends NLS {
 	public static String SortFieldSection_sort_field;
 	public static String SortFieldSection_sort_fields;
 	public static String SortFieldSection_sort_order;
+	public static String StylesListSection_Inherited_From_Default_Style;
 	public static String StylesListSection_removeAttribure_tooltip;
 	public static String StylesSectionList_Element_Attributes;
 	public static String StylesSectionList_Inherited_From_Style;
