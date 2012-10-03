4f613179b8ef741a259d1297cf56740529d0ea6c
chicuslavic <chicuslavic@tibco.com>
2012-10-03 08:28:25 +0000

    fixed java 1.6 compatibility
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@1723 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.data.hive/src/com/jaspersoft/hadoop/hive/connection/HiveConnection.java
+++ com.jaspersoft.studio.data.hive/src/com/jaspersoft/hadoop/hive/connection/HiveConnection.java
@@ -483,35 +483,5 @@ public class HiveConnection implements Connection {
 	public Struct createStruct(String typeName, Object[] attributes) throws SQLException {
 		return null;
 	}
-
-	@Override
-	public void setSchema(String schema) throws SQLException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public String getSchema() throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
-	public void abort(Executor executor) throws SQLException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void setNetworkTimeout(Executor executor, int milliseconds)
-			throws SQLException {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public int getNetworkTimeout() throws SQLException {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+ 
 }
