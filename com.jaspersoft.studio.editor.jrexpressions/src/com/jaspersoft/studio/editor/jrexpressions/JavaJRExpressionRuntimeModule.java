07ff1cb7f67b716a8ccd2a62c5dcd18dc94ee9da
mrabbi <mrabbi@tibco.com>
2012-10-31 16:50:16 +0000

    Support for some implicit imports related to JasperReports.
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@1880 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.editor.jrexpressions/src/com/jaspersoft/studio/editor/jrexpressions/JavaJRExpressionRuntimeModule.java
+++ com.jaspersoft.studio.editor.jrexpressions/src/com/jaspersoft/studio/editor/jrexpressions/JavaJRExpressionRuntimeModule.java
@@ -3,9 +3,16 @@
  */
 package com.jaspersoft.studio.editor.jrexpressions;
 
+import com.jaspersoft.studio.editor.jrexpressions.scoping.JRImportedNamespaceAwareLocalScopeProvider;
+
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
 public class JavaJRExpressionRuntimeModule extends com.jaspersoft.studio.editor.jrexpressions.AbstractJavaJRExpressionRuntimeModule {
 
+	@Override
+	public void configureIScopeProviderDelegate(com.google.inject.Binder binder) {
+		binder.bind(org.eclipse.xtext.scoping.IScopeProvider.class).annotatedWith(com.google.inject.name.Names.named(org.eclipse.xtext.scoping.impl.AbstractDeclarativeScopeProvider.NAMED_DELEGATE)).to(JRImportedNamespaceAwareLocalScopeProvider.class);
+	}
+	
 }
