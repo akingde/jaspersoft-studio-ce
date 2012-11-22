3c1aaff7d4ce484b1e1a6cdb3086cbb00fe76545
mrabbi <mrabbi@tibco.com>
2012-11-22 15:39:34 +0000

    Source headers updated with EPL license info.
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@1956 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.properties/src/com/jaspersoft/studio/properties/view/TabContents.java
+++ com.jaspersoft.studio.properties/src/com/jaspersoft/studio/properties/view/TabContents.java
@@ -1,13 +1,18 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2008 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
+ * Copyright (C) 2010 - 2012 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ * 
+ * Unless you have purchased a commercial license agreement from Jaspersoft, 
+ * the following license terms apply:
+ * 
+ * This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- *
+ * 
  * Contributors:
- *     IBM Corporation - initial API and implementation
- *******************************************************************************/
+ *     Jaspersoft Studio Team - initial API and implementation
+ ******************************************************************************/
 package com.jaspersoft.studio.properties.view;
 
 import org.eclipse.core.runtime.ISafeRunnable;
