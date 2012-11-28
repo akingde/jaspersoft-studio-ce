05a8f47ff1bc70e5298fc8e16c8657aad1aa32e8
mrabbi <mrabbi@tibco.com>
2012-11-28 17:25:45 +0000

    Changed hierarchy for Activator. Now using AbstractJRUIPlugin as superclass.
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@2010 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.data.drivers/src/com/jaspersoft/studio/data/drivers/Activator.java
+++ com.jaspersoft.studio.data.drivers/src/com/jaspersoft/studio/data/drivers/Activator.java
@@ -15,14 +15,14 @@
  ******************************************************************************/
 package com.jaspersoft.studio.data.drivers;
 
-import org.eclipse.jface.resource.ImageDescriptor;
-import org.eclipse.ui.plugin.AbstractUIPlugin;
+import net.sf.jasperreports.eclipse.AbstractJRUIPlugin;
+
 import org.osgi.framework.BundleContext;
 
 /**
  * The activator class controls the plug-in life cycle
  */
-public class Activator extends AbstractUIPlugin {
+public class Activator extends AbstractJRUIPlugin {
 
 	// The plug-in ID
 	public static final String PLUGIN_ID = "com.jaspersoft.studio.data.drivers"; //$NON-NLS-1$
@@ -63,14 +63,8 @@ public class Activator extends AbstractUIPlugin {
 		return plugin;
 	}
 
-	/**
-	 * Returns an image descriptor for the image file at the given
-	 * plug-in relative path
-	 *
-	 * @param path the path
-	 * @return the image descriptor
-	 */
-	public static ImageDescriptor getImageDescriptor(String path) {
-		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	@Override
+	public String getPluginID() {
+		return PLUGIN_ID;
 	}
 }
