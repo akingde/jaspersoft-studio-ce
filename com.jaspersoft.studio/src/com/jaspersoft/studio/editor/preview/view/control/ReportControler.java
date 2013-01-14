d524c61679441d3d1428fc30bdea5fce6458ba27
chicuslavic <chicuslavic@tibco.com>
2013-01-14 10:42:07 +0000

    fixed cleaning of parameters between report executions
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@2116 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/editor/preview/view/control/ReportControler.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/editor/preview/view/control/ReportControler.java
@@ -1,17 +1,12 @@
 /*******************************************************************************
- * Copyright (C) 2010 - 2013 Jaspersoft Corporation. All rights reserved.
- * http://www.jaspersoft.com
+ * Copyright (C) 2010 - 2013 Jaspersoft Corporation. All rights reserved. http://www.jaspersoft.com
  * 
- * Unless you have purchased a commercial license agreement from Jaspersoft, 
- * the following license terms apply:
+ * Unless you have purchased a commercial license agreement from Jaspersoft, the following license terms apply:
  * 
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors:
- *     Jaspersoft Studio Team - initial API and implementation
+ * Contributors: Jaspersoft Studio Team - initial API and implementation
  ******************************************************************************/
 package com.jaspersoft.studio.editor.preview.view.control;
 
@@ -153,6 +148,11 @@ public class ReportControler {
 				if (p.getName().equals(JRParameter.REPORT_DATA_SOURCE))
 					continue;
 				if (p.getName().equals(JRParameter.REPORT_CONNECTION))
+					continue;
+				if (p.getName().startsWith("XML_") || p.getName().startsWith("MONDRIAN_") || p.getName().startsWith("XLSX_")
+						|| p.getName().startsWith("XLS_") || p.getName().startsWith("JSON_")
+						|| p.getName().startsWith("HIBERNATE_") || p.getName().startsWith("JPA_") || p.getName().startsWith("CSV_")
+						|| p.getName().contains("csv.source") || p.getName().startsWith("XMLA_"))
 					continue;
 				try {
 					if (obj != null && p.getValueClass().isAssignableFrom(obj.getClass()))
