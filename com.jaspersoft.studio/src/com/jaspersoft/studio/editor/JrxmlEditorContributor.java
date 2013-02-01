1565003c2b0dfd526f33ac02b448733abfc36cf4
chicuslavic <chicuslavic@tibco.com>
2013-02-01 10:20:21 +0000

    fixed a npe
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@2226 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/editor/JrxmlEditorContributor.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/editor/JrxmlEditorContributor.java
@@ -1,17 +1,12 @@
 /*******************************************************************************
- * Copyright (C) 2010 - 2013 Jaspersoft Corporation. All rights reserved.
- * http://www.jaspersoft.com
+ * Copyright (C) 2010 - 2013 Jaspersoft Corporation. All rights reserved. http://www.jaspersoft.com
  * 
- * Unless you have purchased a commercial license agreement from Jaspersoft, 
- * the following license terms apply:
+ * Unless you have purchased a commercial license agreement from Jaspersoft, the following license terms apply:
  * 
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors:
- *     Jaspersoft Studio Team - initial API and implementation
+ * Contributors: Jaspersoft Studio Team - initial API and implementation
  ******************************************************************************/
 package com.jaspersoft.studio.editor;
 
@@ -202,7 +197,9 @@ public class JrxmlEditorContributor extends MultiPageEditorActionBarContributor 
 	 * @return the action
 	 */
 	protected IAction getAction(String id) {
-		return getActionRegistry().getAction(id);
+		if (getActionRegistry() != null)
+			return getActionRegistry().getAction(id);
+		return null;
 	}
 
 	private IEditorPart lastEditor;
