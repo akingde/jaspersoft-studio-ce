c61f8ee40ceea776c9e13e49e3969a7f9a601200
chicuslavic <chicuslavic@tibco.com>
2013-02-11 12:17:40 +0000

    fixed the selection
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@2299 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/wizards/JSSWizardHandler.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/wizards/JSSWizardHandler.java
@@ -1,17 +1,12 @@
 /*******************************************************************************
- * Copyright (C) 2010 - 2013 Jaspersoft Corporation. All rights reserved.
- * http://www.jaspersoft.com
+ * Copyright (C) 2010 - 2013 Jaspersoft Corporation. All rights reserved. http://www.jaspersoft.com
  * 
- * Unless you have purchased a commercial license agreement from Jaspersoft, 
- * the following license terms apply:
+ * Unless you have purchased a commercial license agreement from Jaspersoft, the following license terms apply:
  * 
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors:
- *     Jaspersoft Studio Team - initial API and implementation
+ * Contributors: Jaspersoft Studio Team - initial API and implementation
  ******************************************************************************/
 package com.jaspersoft.studio.wizards;
 
@@ -20,6 +15,8 @@ import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
 import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.wizard.IWizard;
 import org.eclipse.jface.wizard.WizardDialog;
@@ -46,8 +43,7 @@ public abstract class JSSWizardHandler extends AbstractHandler {
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		String wizardId = getWizardIdParameterId();
 
-		IWorkbenchWindow activeWindow = HandlerUtil
-				.getActiveWorkbenchWindowChecked(event);
+		IWorkbenchWindow activeWindow = HandlerUtil.getActiveWorkbenchWindowChecked(event);
 
 		if (wizardId == null) {
 			IAction wizardAction = createWizardChooserDialogAction(activeWindow);
@@ -55,26 +51,26 @@ public abstract class JSSWizardHandler extends AbstractHandler {
 		} else {
 
 			IWizardRegistry wizardRegistry = getWizardRegistry();
-			IWizardDescriptor wizardDescriptor = wizardRegistry
-					.findWizard(wizardId);
+			IWizardDescriptor wizardDescriptor = wizardRegistry.findWizard(wizardId);
 			if (wizardDescriptor == null) {
 				throw new ExecutionException("unknown wizard: " + wizardId); //$NON-NLS-1$
 			}
 
 			try {
 				IWorkbenchWizard wizard = wizardDescriptor.createWizard();
-				wizard.init(PlatformUI.getWorkbench(),
-						StructuredSelection.EMPTY);
+				ISelection selection = activeWindow.getSelectionService().getSelection();
+				IStructuredSelection ss = selection instanceof IStructuredSelection ? (IStructuredSelection) selection
+						: StructuredSelection.EMPTY;
+				wizard.init(PlatformUI.getWorkbench(), ss);
 
-				if (wizardDescriptor.canFinishEarly()
-						&& !wizardDescriptor.hasPages()) {
+				if (wizardDescriptor.canFinishEarly() && !wizardDescriptor.hasPages()) {
 					wizard.performFinish();
 					return null;
 				}
 
 				Shell parent = activeWindow.getShell();
 				createAndOpenWizardDialog(parent, wizard);
-				
+
 			} catch (CoreException ex) {
 				throw new ExecutionException("error creating wizard", ex); //$NON-NLS-1$
 			}
@@ -83,52 +79,46 @@ public abstract class JSSWizardHandler extends AbstractHandler {
 
 		return null;
 	}
-	
+
 	/**
 	 * Creates and opens the wizard dialog.
 	 * <p>
 	 * 
 	 * Default implementation uses the {@link WizardDialog} class.<br>
-	 * Subclasses can for example modify the behavior contributing of 
-	 * the standard buttonbar contributing a new one.
-	 * They can easily do this by using their one {@link WizardDialog} implementation. 
+	 * Subclasses can for example modify the behavior contributing of the standard buttonbar contributing a new one. They
+	 * can easily do this by using their one {@link WizardDialog} implementation.
 	 * 
-	 * @param parent the parent shell
-	 * @param wizard the wizard to be created in the dialog
+	 * @param parent
+	 *          the parent shell
+	 * @param wizard
+	 *          the wizard to be created in the dialog
 	 */
-	protected void createAndOpenWizardDialog(Shell parent, IWizard wizard){
+	protected void createAndOpenWizardDialog(Shell parent, IWizard wizard) {
 		WizardDialog dialog = new WizardDialog(parent, wizard);
 		dialog.create();
 		dialog.open();
 	}
-	
+
 	/**
-	 * Returns an <code>IAction</code> that opens a dialog to allow the user
-	 * to choose a wizard.
+	 * Returns an <code>IAction</code> that opens a dialog to allow the user to choose a wizard.
 	 * 
 	 * @param window
-	 *            The workbench window to use when constructing the action.
-	 * @return An <code>IAction</code> that opens a dialog to allow the user
-	 *         to choose a wizard.
+	 *          The workbench window to use when constructing the action.
+	 * @return An <code>IAction</code> that opens a dialog to allow the user to choose a wizard.
 	 */
-	protected abstract IAction createWizardChooserDialogAction(
-			IWorkbenchWindow window);
-	
+	protected abstract IAction createWizardChooserDialogAction(IWorkbenchWindow window);
+
 	/**
-	 * Returns the id of the parameter used to indicate which wizard this
-	 * command should launch.
+	 * Returns the id of the parameter used to indicate which wizard this command should launch.
 	 * 
-	 * @return The id of the parameter used to indicate which wizard this
-	 *         command should launch.
+	 * @return The id of the parameter used to indicate which wizard this command should launch.
 	 */
 	protected abstract String getWizardIdParameterId();
 
 	/**
-	 * Returns the wizard registry for the concrete <code>WizardHandler</code>
-	 * implementation class.
+	 * Returns the wizard registry for the concrete <code>WizardHandler</code> implementation class.
 	 * 
-	 * @return The wizard registry for the concrete <code>WizardHandler</code>
-	 *         implementation class.
+	 * @return The wizard registry for the concrete <code>WizardHandler</code> implementation class.
 	 */
 	protected abstract IWizardRegistry getWizardRegistry();
 }
