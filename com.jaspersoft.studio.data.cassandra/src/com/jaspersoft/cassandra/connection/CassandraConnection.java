d4867a0e3c19207104712d4c30733f9e8dc16404
chicuslavic <chicuslavic@tibco.com>
2013-02-21 14:54:15 +0000

    fixed a compatibility with java 7
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@2372 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.data.cassandra/src/com/jaspersoft/cassandra/connection/CassandraConnection.java
+++ com.jaspersoft.studio.data.cassandra/src/com/jaspersoft/cassandra/connection/CassandraConnection.java
@@ -37,6 +37,7 @@ import java.sql.Statement;
 import java.sql.Struct;
 import java.util.Map;
 import java.util.Properties;
+import java.util.concurrent.Executor;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -473,4 +474,35 @@ public class CassandraConnection implements Connection {
     public Struct createStruct(String typeName, Object[] attributes) throws SQLException {
         return null;
     }
+
+	@Override
+	public void setSchema(String schema) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getSchema() throws SQLException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void abort(Executor executor) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setNetworkTimeout(Executor executor, int milliseconds)
+			throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public int getNetworkTimeout() throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 }
