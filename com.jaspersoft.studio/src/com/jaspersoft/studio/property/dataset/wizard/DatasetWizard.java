5d45e5f32ec3058ec539b9e91bce4539fe39fd8c
chicuslavic <chicuslavic@tibco.com>
2013-03-08 08:51:13 +0000

    [#2792] - NPE when on the table wizard
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@2447 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/property/dataset/wizard/DatasetWizard.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/property/dataset/wizard/DatasetWizard.java
@@ -1,17 +1,12 @@
 /*******************************************************************************
- * Copyright (C) 2010 - 2013 Jaspersoft Corporation. All rights reserved.
- * http://www.jaspersoft.com
+ * Copyright (C) 2010 - 2013 Jaspersoft Corporation. All rights reserved. http://www.jaspersoft.com
  * 
- * Unless you have purchased a commercial license agreement from Jaspersoft, 
- * the following license terms apply:
+ * Unless you have purchased a commercial license agreement from Jaspersoft, the following license terms apply:
  * 
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors:
- *     Jaspersoft Studio Team - initial API and implementation
+ * Contributors: Jaspersoft Studio Team - initial API and implementation
  ******************************************************************************/
 package com.jaspersoft.studio.property.dataset.wizard;
 
@@ -28,30 +23,27 @@ import com.jaspersoft.studio.wizards.JSSWizard;
 import com.jaspersoft.studio.wizards.JSSWizardPageChangeEvent;
 import com.jaspersoft.studio.wizards.WizardUtils;
 
-
 /**
- * A Wizard to create an Dataset. The dataset can be get from the wizard using {@link #getDataset() getDataset}
- * which returns a {@link com.jaspersoft.studio.model.dataset.MDataset}.
+ * A Wizard to create an Dataset. The dataset can be get from the wizard using {@link #getDataset() getDataset} which
+ * returns a {@link com.jaspersoft.studio.model.dataset.MDataset}.
  * 
  * 
  * @author gtoffoli
- *
+ * 
  */
 public class DatasetWizard extends JSSWizard {
-	
-	
+
 	/**
 	 * Key used to store the wizard (MDataset) inside the wizard settings...
 	 */
 	public static final String DATASET = "datasetnamewizard"; //$NON-NLS-1$
-	
+
 	// Steps involved in the creation of a simple dataset
 	private WizardDatasetNewPage step1;
 	private WizardDataSourcePage step2;
 	private WizardFieldsPage step3;
 	private WizardFieldsGroupByPage step4;
 
-	
 	public DatasetWizard() {
 		super();
 		setWindowTitle(Messages.common_dataset);
@@ -66,12 +58,12 @@ public class DatasetWizard extends JSSWizard {
 
 	@Override
 	public void addPages() {
-		
+
 		step1 = new WizardDatasetNewPage();
 		addPage(step1);
 
-		//FIXME: wizard dataset broken after wizard changes
-		step2 = new WizardDataSourcePage(); //getConfig());
+		// FIXME: wizard dataset broken after wizard changes
+		step2 = new WizardDataSourcePage(); // getConfig());
 		addPage(step2);
 
 		step3 = new WizardFieldsPage();
@@ -81,105 +73,83 @@ public class DatasetWizard extends JSSWizard {
 		addPage(step4);
 	}
 
-	
 	@Override
 	public IWizardPage getNextPage(IWizardPage page) {
 		if (page == step1) {
-			
+
 			// Go directly to the end if the user asks to create an empty dataste...
 			if (step1.isEmptyDataset()) {
 				page = step4;
 			}
 		}
-		
+
 		return super.getNextPage(page);
 	}
 
-	
 	/**
-	 * This is the method that builds a JRDesignDataset from the settings get during the wizard steps.
-	 * This method returns a new dataset instance each time, there is no cached object.
-	 *
+	 * This is the method that builds a JRDesignDataset from the settings get during the wizard steps. This method returns
+	 * a new dataset instance each time, there is no cached object.
+	 * 
 	 * @return
-	 * @throws JRException 
+	 * @throws JRException
 	 */
 	public JRDesignDataset getDataset() {
 		JRDesignDataset ds = WizardUtils.createDataset(false, getSettings());
-		if(step2.getDataAdapter()!=null){
+		if (step2 != null && step2.getDataAdapter() != null) {
 			// Save the information on the default data adapter
 			// to propose selected for this specific dataset
-			ds.getPropertiesMap().setProperty(
-					DataQueryAdapters.DEFAULT_DATAADAPTER, 
-					step2.getDataAdapter().getName());
+			ds.getPropertiesMap().setProperty(DataQueryAdapters.DEFAULT_DATAADAPTER, step2.getDataAdapter().getName());
 		}
 		return ds;
 	}
 
-	
-	
-	
 	/**
-	 * This method force the user to go next and not finish if the selection
-	 * in the first step is not an empty dataset...
+	 * This method force the user to go next and not finish if the selection in the first step is not an empty dataset...
 	 */
 	@Override
 	public boolean canFinish() {
-		
-		if (getContainer().getCurrentPage() == step1)
-		{
-			if (step1.isPageComplete() &&
-					step1.isEmptyDataset())
-			{
+
+		if (getContainer().getCurrentPage() == step1) {
+			if (step1.isPageComplete() && step1.isEmptyDataset()) {
 				return true;
 			}
 			return false;
 		}
-		
+
 		return super.canFinish();
 	}
 
-	
 	/**
-	 * Get the dataset, and add the command to add the dataset to the model
-	 * to set stack of commands of this wizard.
+	 * Get the dataset, and add the command to add the dataset to the model to set stack of commands of this wizard.
 	 * 
-	 * The user will need to invoke the commands queue in order to actually
-	 * add the dataset to the document.
+	 * The user will need to invoke the commands queue in order to actually add the dataset to the document.
 	 * 
 	 */
 	@Override
 	public boolean performFinish() {
-		addCommand(new CreateDatasetCommand(getConfig(),getDataset()));
+		addCommand(new CreateDatasetCommand(getConfig(), getDataset()));
 		return super.performFinish();
 	}
 
 	@Override
 	public void pageChanged(JSSWizardPageChangeEvent event) {
-		
-		if (event.getPage() == step2)
-		{
-			
+
+		if (event.getPage() == step2) {
+
 			// If there are no fields, remove next step...
-			if (step2.requireElaboration() == false)
-			{
+			if (step2.requireElaboration() == false) {
 				removePage(step3);
 				removePage(step4);
-			}
-			else
-			{
-				if (!getPageList().contains(step3))
-				{
+			} else {
+				if (!getPageList().contains(step3)) {
 					addPage(step3);
 					addPage(step4);
 				}
 			}
-			
+
 			event.getPage().setPageComplete(event.getPage().isPageComplete());
-			
+
 		}
 	}
 
-	
-	
-	
 }
