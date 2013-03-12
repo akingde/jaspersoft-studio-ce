61e624506bd68901719cd94c66ca2f5041a0c387
chicuslavic <chicuslavic@tibco.com>
2013-03-12 12:57:40 +0000

    [#2808] - Create Conditional Style available in the styles root context menu
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@2464 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/editor/outline/OutlineTreeEditPartFactory.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/editor/outline/OutlineTreeEditPartFactory.java
@@ -1,17 +1,12 @@
 /*******************************************************************************
- * Copyright (C) 2010 - 2013 Jaspersoft Corporation. All rights reserved.
- * http://www.jaspersoft.com
+ * Copyright (C) 2010 - 2013 Jaspersoft Corporation. All rights reserved. http://www.jaspersoft.com
  * 
- * Unless you have purchased a commercial license agreement from Jaspersoft, 
- * the following license terms apply:
+ * Unless you have purchased a commercial license agreement from Jaspersoft, the following license terms apply:
  * 
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors:
- *     Jaspersoft Studio Team - initial API and implementation
+ * Contributors: Jaspersoft Studio Team - initial API and implementation
  ******************************************************************************/
 package com.jaspersoft.studio.editor.outline;
 
@@ -422,7 +417,11 @@ public class OutlineTreeEditPartFactory implements EditPartFactory {
 				return new CreateE4ObjectCommand(child, parent, location, newIndex);
 			}
 		} else {
-			if (child instanceof MStyle) {
+			if (child instanceof MConditionalStyle) {
+				if (parent instanceof MStyle)
+					return new CreateConditionalStyleCommand((MStyle) parent, (MConditionalStyle) child, newIndex);
+				return null;
+			} else if (child instanceof MStyle) {
 				if (parent instanceof MStyles)
 					return new CreateStyleCommand((MStyles) parent, (MStyle) child, newIndex);
 				if (parent instanceof MGraphicElement && child.getValue() != null && !(parent instanceof IContainer)) {
