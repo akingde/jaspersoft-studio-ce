d34b35aaf9d973bf8d8892e772b716bdaf5eb1be
morlandin <morlandin@tibco.com>
2013-05-14 16:17:00 +0000

    removed the custom template load for the table based template
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@2753 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/engine/ExtendedTemplateProvider.java
+++ com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/engine/ExtendedTemplateProvider.java
@@ -81,6 +81,7 @@ public class ExtendedTemplateProvider implements TemplateProvider {
 	 * Look for other templates inside the specified directories in the preferences.
 	 */
 	private void loadAdditionalTemplateBundles(List<TemplateBundle> templates) {
+		/*
 		String paths = JaspersoftStudioPlugin.getInstance().getPreferenceStore()
 				.getString(TemplateLocationsPreferencePage.TPP_TEMPLATES_LOCATIONS_LIST);
 		StringTokenizer st = new StringTokenizer(paths, File.pathSeparator + "\n\r");
@@ -88,7 +89,7 @@ public class ExtendedTemplateProvider implements TemplateProvider {
 		while (st.hasMoreTokens()) {
 			pathsList.add(st.nextToken());
 		}
-
+		
 		for (String dir : pathsList) {
 			File[] files = new File(dir).listFiles(new FileFilter() {
 				@Override
@@ -112,6 +113,6 @@ public class ExtendedTemplateProvider implements TemplateProvider {
 					}
 				}
 			}
-		}
+		}*/
 	}
 }
