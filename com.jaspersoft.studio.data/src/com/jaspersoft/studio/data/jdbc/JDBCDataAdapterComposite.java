409a4e6a6aebe84d1bed40da8bfb610c0c52e601
chicuslavic <chicuslavic@tibco.com>
2013-05-29 10:03:23 +0000

    Bug 32470 - Not shipping JDBC drivers that we ship with JRS Pro
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@2810 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.data/src/com/jaspersoft/studio/data/jdbc/JDBCDataAdapterComposite.java
+++ com.jaspersoft.studio.data/src/com/jaspersoft/studio/data/jdbc/JDBCDataAdapterComposite.java
@@ -48,30 +48,38 @@ public class JDBCDataAdapterComposite extends ADataAdapterComposite {
 			"org.hsqldb.jdbcDriver", "jdbc:hsqldb:hsql://{0}"), //$NON-NLS-1$ //$NON-NLS-2$
 			new JDBCDriverDefinition("HSQLDB (file)", "org.hsqldb.jdbcDriver", //$NON-NLS-1$ //$NON-NLS-2$
 					"jdbc:hsqldb:[PATH_TO_DB_FILES]/{1}"), //$NON-NLS-1$
-			new JDBCDriverDefinition("SQLite (file)", "org.sqlite.JDBC", //$NON-NLS-1$ //$NON-NLS-2$
-					"jdbc:sqlite:[PATH_TO_DB_FILES]/{1}"), //$NON-NLS-1$
-			new JDBCDriverDefinition("DerbyDB (Embedded)", "org.apache.derby.jdbc.EmbeddedDriver", //$NON-NLS-1$ //$NON-NLS-2$
-					"jdbc:derby:/{1}"), //$NON-NLS-1$	
-			new JDBCDriverDefinition("DerbyDB (Client)", "org.apache.derby.jdbc.ClientDriver", //$NON-NLS-1$ //$NON-NLS-2$
-					"jdbc:derby://{0}[:1527]/{1}"), //$NON-NLS-1$	
+
 			new JDBCDriverDefinition("H2 (file)", "org.h2.Driver", //$NON-NLS-1$ //$NON-NLS-2$
 					"jdbc:h2:file:{1}"), //$NON-NLS-1$
 			new JDBCDriverDefinition("H2 (tco)", "org.h2.Driver", //$NON-NLS-1$ //$NON-NLS-2$
 					"jdbc:h2:tcp://{0}:9101/~/{1}"), //$NON-NLS-1$
+
+			new JDBCDriverDefinition("SQLite (file)", "org.sqlite.JDBC", //$NON-NLS-1$ //$NON-NLS-2$
+					"jdbc:sqlite:[PATH_TO_DB_FILES]/{1}"), //$NON-NLS-1$
+
+			new JDBCDriverDefinition("DerbyDB (Embedded)", "org.apache.derby.jdbc.EmbeddedDriver", //$NON-NLS-1$ //$NON-NLS-2$
+					"jdbc:derby:/{1}"), //$NON-NLS-1$	
+			new JDBCDriverDefinition("DerbyDB (Client)", "org.apache.derby.jdbc.ClientDriver", //$NON-NLS-1$ //$NON-NLS-2$
+					"jdbc:derby://{0}[:1527]/{1}"), //$NON-NLS-1$	
+
 			new JDBCDriverDefinition("Cloudscape", "COM.cloudscape.JDBCDriver", //$NON-NLS-1$ //$NON-NLS-2$
 					"jdbc:cloudscape:/{1}"), //$NON-NLS-1$
+
 			new JDBCDriverDefinition("IBM DB2 (Type 4)", //$NON-NLS-1$
 					"com.ibm.db2.jcc.DB2Driver", "jdbc:db2://{0}/{1}"), //$NON-NLS-1$ //$NON-NLS-2$
 			new JDBCDriverDefinition("IBM DB2 (IBM Cloudscape® server)", //$NON-NLS-1$
 					"com.ibm.db2.jcc.DB2Driver", "jdbc:db2j:net://{0}/{1}"), //$NON-NLS-1$ //$NON-NLS-2$
 			new JDBCDriverDefinition("IBM DB2 (IDS data source)", //$NON-NLS-1$
 					"com.ibm.db2.jcc.DB2Driver", "jdbc:ids://{0}/{1}"), //$NON-NLS-1$ //$NON-NLS-2$
+
 			new JDBCDriverDefinition("inetdae7", "com.inet.tds.TdsDriver", //$NON-NLS-1$ //$NON-NLS-2$
 					"jdbc:inetdae7:{0}:1433/{1}"), //$NON-NLS-1$
 			new JDBCDriverDefinition("Informix", "com.informix.jdbc.IfxDriver", //$NON-NLS-1$ //$NON-NLS-2$
+
 					"jdbc:informix-sqli://{0}:informixserver={1}"), //$NON-NLS-1$
 			new JDBCDriverDefinition("Ingres", "com.ingres.jdbc.IngresDriver", //$NON-NLS-1$ //$NON-NLS-2$
 					"jdbc:ingres://{0}:II7/{1}"), //$NON-NLS-1$
+
 			new JDBCDriverDefinition("JDBC-ODBC Bridge", //$NON-NLS-1$
 					"sun.jdbc.odbc.JdbcOdbcDriver", "jdbc:odbc:{1}", "DSNAME"), //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
 			new JDBCDriverDefinition("JDBC-ODBC Bridge", //$NON-NLS-1$
@@ -93,28 +101,36 @@ public class JDBCDataAdapterComposite extends ADataAdapterComposite {
 			new JDBCDriverDefinition("MS SQLServer", //$NON-NLS-1$
 					"com.internetcds.jdbc.tds.Driver", //$NON-NLS-1$
 					"jdbc:freetds:sqlserver://{0}/{1}"), //$NON-NLS-1$
-			new JDBCDriverDefinition("Sybase", //$NON-NLS-1$
-					"net.sourceforge.jtds.jdbc.Driver", //$NON-NLS-1$
-					"jdbc:jtds:sybase://{0}/{1}"), //$NON-NLS-1$		
 
 			new JDBCDriverDefinition("MySQL", "org.gjt.mm.mysql.Driver", //$NON-NLS-1$ //$NON-NLS-2$
 					"jdbc:mysql://{0}/{1}"), //$NON-NLS-1$
 			new JDBCDriverDefinition("MySQL", "com.mysql.jdbc.Driver", //$NON-NLS-1$ //$NON-NLS-2$
 					"jdbc:mysql://{0}/{1}"), //$NON-NLS-1$
+
 			new JDBCDriverDefinition("Oracle", //$NON-NLS-1$
 					"oracle.jdbc.driver.OracleDriver", //$NON-NLS-1$
 					"jdbc:oracle:thin:@{0}:1521:{1}"), //$NON-NLS-1$
 			new JDBCDriverDefinition("PostgreSQL", "org.postgresql.Driver", //$NON-NLS-1$ //$NON-NLS-2$
 					"jdbc:postgresql://{0}:5432/{1}"), //$NON-NLS-1$
+
+			new JDBCDriverDefinition("Sybase", //$NON-NLS-1$
+					"net.sourceforge.jtds.jdbc.Driver", //$NON-NLS-1$
+					"jdbc:jtds:sybase://{0}/{1}"), //$NON-NLS-1$		
 			new JDBCDriverDefinition("Sybase", //$NON-NLS-1$
 					"com.sybase.jdbc2.jdbc.SybDriver", //$NON-NLS-1$
 					"jdbc:sybase:Tds:{0}:2638/{1}"), //$NON-NLS-1$
-			new JDBCDriverDefinition("Vertica", "com.vertica.Driver", //$NON-NLS-1$ //$NON-NLS-2$
-					"jdbc:vertica://{0}:5433/{1}"), //$NON-NLS-1$
+
 			new JDBCDriverDefinition("Hadoop Hive", "org.apache.hadoop.hive.jdbc.HiveDriver", //$NON-NLS-1$ //$NON-NLS-2$
 					"jdbc:hive://{0}:10000/default"),//$NON-NLS-1$
+			new JDBCDriverDefinition("Vertica", "com.vertica.Driver", //$NON-NLS-1$ //$NON-NLS-2$
+					"jdbc:vertica://{0}:5433/{1}"), //$NON-NLS-1$
 			new JDBCDriverDefinition("Vertica", "com.vertica.jdbc.Driver", //$NON-NLS-1$ //$NON-NLS-2$
-					"jdbc:vertica://{0}:5433/{1}") }; //$NON-NLS-1$ 
+					"jdbc:vertica://{0}:5433/{1}"), //$NON-NLS-1$ 
+
+			new JDBCDriverDefinition("Mondrian", "mondrian.olap4j.MondrianOlap4jDriver", //$NON-NLS-1$ //$NON-NLS-2$
+					"jdbc:mondrian:"), //$NON-NLS-1$ 
+			new JDBCDriverDefinition("OLAP4J", "org.olap4j.driver.xmla.XmlaOlap4jDriver", //$NON-NLS-1$ //$NON-NLS-2$
+					"jdbc:xmla:") }; //$NON-NLS-1$ 
 
 	public JDBCDriverDefinition[] getDefinitions() {
 		return jdbcDefinitions;
