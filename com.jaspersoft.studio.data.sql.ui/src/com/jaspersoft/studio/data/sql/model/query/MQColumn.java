634d23ccbddc6734970d2a8d955ddcca8fa2525f
chicuslavic <chicuslavic@tibco.com>
2013-06-17 06:22:52 +0000

    sql query designer
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@2915 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.data.sql.ui/src/com/jaspersoft/studio/data/sql/model/query/MQColumn.java
+++ com.jaspersoft.studio.data.sql.ui/src/com/jaspersoft/studio/data/sql/model/query/MQColumn.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright (C) 2010 - 2013 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ * 
+ * Unless you have purchased a commercial license agreement from Jaspersoft, 
+ * the following license terms apply:
+ * 
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Jaspersoft Studio Team - initial API and implementation
+ ******************************************************************************/
 package com.jaspersoft.studio.data.sql.model.query;
 
 public class MQColumn {
