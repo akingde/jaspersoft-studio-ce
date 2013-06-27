48c45d9a4c74b6747191f500cd7f84fed499a4fe
chicuslavic <chicuslavic@tibco.com>
2013-06-27 19:03:08 +0000

    bugs 3017, 3016, 3003, 3015
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@2981 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.doc/src/com/jaspersoft/studio/doc/handlers/UploadReportCheatAction.java
+++ com.jaspersoft.studio.doc/src/com/jaspersoft/studio/doc/handlers/UploadReportCheatAction.java
@@ -9,46 +9,35 @@ import org.eclipse.jface.wizard.WizardDialog;
 
 import com.jaspersoft.studio.doc.messages.Messages;
 import com.jaspersoft.studio.model.ANode;
-import com.jaspersoft.studio.model.INode;
-import com.jaspersoft.studio.model.MRoot;
-import com.jaspersoft.studio.server.model.server.MServerProfile;
-import com.jaspersoft.studio.server.model.server.ServerProfile;
 import com.jaspersoft.studio.server.publish.wizard.Publish2ServerWizard;
 import com.jaspersoft.studio.utils.jasper.JasperReportsConfiguration;
 
 /**
- * This class start the upload of a report into an instance of JasperReports Server
+ * This class start the upload of a report into an instance of JasperReports
+ * Server
  * 
  * @author Orlandin Marco
- *
+ * 
  */
 public class UploadReportCheatAction extends Action {
-	
-	private ANode cloneServer(ANode server, ANode parent){
-		MServerProfile fakeServer = new MServerProfile(parent, (ServerProfile)server.getValue());
-		for(INode element : server.getChildren())
-			fakeServer.addChild((ANode)element);
-		return fakeServer;
-	}
-	
+
 	@Override
 	public void run() {
 		ANode rootElement = HandlersUtil.getRootElement();
-		if (rootElement != null){
+		if (rootElement != null) {
 			JasperDesign design = rootElement.getJasperDesign();
 			JasperReportsConfiguration config = rootElement.getJasperConfiguration();
 			ANode servers = HandlersUtil.getServers();
-			if (servers != null && servers.getChildren().size()>0){
-				//Create a fake root to show only the server connection in the list
-				MRoot fakeRoot = new MRoot(null,design);
-				for(INode server : servers.getChildren())
-					cloneServer((ANode)server, fakeRoot);
-				Publish2ServerWizard wizard = new Publish2ServerWizard(fakeRoot , design, config,1);
+			if (servers != null && servers.getChildren().size() > 0) {
+				// Create a fake root to show only the server connection in the list
+				Publish2ServerWizard wizard = new Publish2ServerWizard(design, config, 1);
 				WizardDialog dialogToOpen = new WizardDialog(UIUtils.getShell(), wizard);
 				dialogToOpen.create();
 				dialogToOpen.open();
-			} else MessageDialog.openWarning(UIUtils.getShell(), Messages.UploadReportCheatAction_no_server_title, Messages.UploadReportCheatAction_no_server_warning);
-		} else MessageDialog.openWarning(UIUtils.getShell(), Messages.UploadReportCheatAction_warning_title, Messages.UploadReportCheatAction_warning_message);
+			} else
+				MessageDialog.openWarning(UIUtils.getShell(), Messages.UploadReportCheatAction_no_server_title, Messages.UploadReportCheatAction_no_server_warning);
+		} else
+			MessageDialog.openWarning(UIUtils.getShell(), Messages.UploadReportCheatAction_warning_title, Messages.UploadReportCheatAction_warning_message);
 	}
-	
+
 }
