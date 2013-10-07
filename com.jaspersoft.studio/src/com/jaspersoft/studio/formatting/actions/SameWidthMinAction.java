4f9853938a9e0e6b9259f6b39e32c7115f64a3a2
chicuslavic <chicuslavic@tibco.com>
2013-10-07 16:02:30 +0000

    fixed a ClassCastException
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@3380 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/formatting/actions/SameWidthMinAction.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/formatting/actions/SameWidthMinAction.java
@@ -1,22 +1,18 @@
 /*******************************************************************************
- * Copyright (C) 2010 - 2013 Jaspersoft Corporation. All rights reserved.
- * http://www.jaspersoft.com
+ * Copyright (C) 2010 - 2013 Jaspersoft Corporation. All rights reserved. http://www.jaspersoft.com
  * 
- * Unless you have purchased a commercial license agreement from Jaspersoft, 
- * the following license terms apply:
+ * Unless you have purchased a commercial license agreement from Jaspersoft, the following license terms apply:
  * 
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors:
- *     Jaspersoft Studio Team - initial API and implementation
+ * Contributors: Jaspersoft Studio Team - initial API and implementation
  ******************************************************************************/
 package com.jaspersoft.studio.formatting.actions;
 
 import java.util.List;
 
+import net.sf.jasperreports.components.table.StandardColumn;
 import net.sf.jasperreports.engine.design.JRDesignElement;
 
 import org.eclipse.gef.commands.Command;
@@ -24,65 +20,67 @@ import org.eclipse.gef.commands.CompoundCommand;
 import org.eclipse.ui.IWorkbenchPart;
 
 import com.jaspersoft.studio.JaspersoftStudioPlugin;
+import com.jaspersoft.studio.messages.Messages;
 import com.jaspersoft.studio.model.APropertyNode;
 import com.jaspersoft.studio.property.SetValueCommand;
-import com.jaspersoft.studio.messages.Messages;
 
-public class SameWidthMinAction extends AbstractFormattingAction{
+public class SameWidthMinAction extends AbstractFormattingAction {
 
 	/** The Constant ID. */
 	public static final String ID = "matchwidthmin"; //$NON-NLS-1$
-	
+
 	public SameWidthMinAction(IWorkbenchPart part) {
 		super(part);
 		setText(Messages.SameWidthMinAction_actionName);
 		setToolTipText(Messages.SameWidthMinAction_actionDescription);
 		setId(ID);
-		setImageDescriptor(JaspersoftStudioPlugin.getInstance().getImageDescriptor("icons/resources/matchwidthmin.png"));  //$NON-NLS-1$
+		setImageDescriptor(JaspersoftStudioPlugin.getInstance().getImageDescriptor("icons/resources/matchwidthmin.png")); //$NON-NLS-1$
 	}
 
 	@Override
 	protected boolean calculateEnabled() {
-		return getOperationSet().size()>1;
+		return getOperationSet().size() > 1;
 	}
-	
-	public static CompoundCommand generateCommand(List<APropertyNode> nodes){
+
+	public static CompoundCommand generateCommand(List<APropertyNode> nodes) {
 		CompoundCommand command = new CompoundCommand();
-		
+
 		int width = (Integer) nodes.get(0).getPropertyValue(JRDesignElement.PROPERTY_WIDTH);
-    
-    // Find the smallest one...
-    for (int i=1; i<nodes.size(); ++i)
-    {
-    		JRDesignElement element = (JRDesignElement)nodes.get(i).getValue();
-        if (width > element.getWidth())
-        {
-        	width = element.getWidth();
-        }
-    }
-    
-    for (APropertyNode node : nodes)
-    {
-  			SetValueCommand setCommand = new SetValueCommand();
-  			setCommand.setTarget(node);
-  			setCommand.setPropertyId(JRDesignElement.PROPERTY_WIDTH);
-  			setCommand.setPropertyValue(width);
-	      command.add(setCommand);
-    }
-		
+
+		// Find the smallest one...
+		for (int i = 1; i < nodes.size(); ++i) {
+			if (nodes.get(i).getValue() instanceof JRDesignElement) {
+				JRDesignElement element = (JRDesignElement) nodes.get(i).getValue();
+				if (width > element.getWidth())
+					width = element.getWidth();
+			} else if (nodes.get(i).getValue() instanceof StandardColumn) {
+				StandardColumn element = (StandardColumn) nodes.get(i).getValue();
+				if (width > element.getWidth())
+					width = element.getWidth();
+			}
+		}
+
+		for (APropertyNode node : nodes) {
+			SetValueCommand setCommand = new SetValueCommand();
+			setCommand.setTarget(node);
+			setCommand.setPropertyId(JRDesignElement.PROPERTY_WIDTH);
+			setCommand.setPropertyValue(width);
+			command.add(setCommand);
+		}
+
 		return command;
 	}
 
 	protected Command createAlignmentCommand() {
-			List<APropertyNode> nodes = getOperationSet();
-			CompoundCommand command = null;
-			if (nodes.isEmpty()) 
-				command = new CompoundCommand();
-			else {
-				command = generateCommand(nodes);
-			}
-			command.setDebugLabel(getText());
-			return command;
+		List<APropertyNode> nodes = getOperationSet();
+		CompoundCommand command = null;
+		if (nodes.isEmpty())
+			command = new CompoundCommand();
+		else {
+			command = generateCommand(nodes);
+		}
+		command.setDebugLabel(getText());
+		return command;
 	}
-	
+
 }
