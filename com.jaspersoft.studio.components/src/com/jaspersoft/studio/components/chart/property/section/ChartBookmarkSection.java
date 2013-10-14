82e105b2f61339069d4214462a65e628deb55436
chicuslavic <chicuslavic@tibco.com>
2013-10-14 17:51:35 +0000

    fixed a npe
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@3433 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/chart/property/section/ChartBookmarkSection.java
+++ com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/chart/property/section/ChartBookmarkSection.java
@@ -15,32 +15,45 @@
  ******************************************************************************/
 package com.jaspersoft.studio.components.chart.property.section;
 
+import net.sf.jasperreports.charts.design.JRDesignChartAxis;
 import net.sf.jasperreports.components.spiderchart.StandardChartSettings;
 import net.sf.jasperreports.engine.design.JRDesignChart;
 
+import com.jaspersoft.studio.components.chart.model.chartAxis.MChartAxes;
 import com.jaspersoft.studio.components.chartspider.model.MSpiderChart;
+import com.jaspersoft.studio.model.APropertyNode;
 import com.jaspersoft.studio.property.section.graphic.BookmarkSection;
 
 /**
  * 
- * This class extends the bookmark section to provide the correct property ids 
+ * This class extends the bookmark section to provide the correct property ids
  * for the chart elements
  * 
  * @author Orlandin Marco
- *
+ * 
  */
 public class ChartBookmarkSection extends BookmarkSection {
-	
+
 	@Override
-	public String getAnchorNameProperty(){
-		if (getElement() instanceof MSpiderChart) return StandardChartSettings.PROPERTY_ANCHOR_NAME_EXPRESSION;
-		else return JRDesignChart.PROPERTY_ANCHOR_NAME_EXPRESSION;
+	public String getAnchorNameProperty() {
+		if (getElement() instanceof MSpiderChart)
+			return StandardChartSettings.PROPERTY_ANCHOR_NAME_EXPRESSION;
+		else
+			return JRDesignChart.PROPERTY_ANCHOR_NAME_EXPRESSION;
 	}
-	
+
 	@Override
-	public String getBookmarkLevelProperty(){
-		if (getElement() instanceof MSpiderChart) return StandardChartSettings.PROPERTY_BOOKMARK_LEVEL;
+	public String getBookmarkLevelProperty() {
+		if (getElement() instanceof MSpiderChart)
+			return StandardChartSettings.PROPERTY_BOOKMARK_LEVEL;
 		return JRDesignChart.PROPERTY_BOOKMARK_LEVEL;
 	}
-	
+
+	@Override
+	protected APropertyNode getModelFromEditPart(Object item) {
+		APropertyNode md = super.getModelFromEditPart(item);
+		if (md instanceof MChartAxes)
+			return (APropertyNode) md.getPropertyValue(JRDesignChartAxis.PROPERTY_CHART);
+		return md;
+	}
 }
