7039cf89e62b882c6ea67ab3be67a4a6e1e92690
morlandin <morlandin@tibco.com>
2013-11-07 15:19:37 +0000

    added comments
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@3521 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/property/color/chooser/SaturationBasedSelector.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/property/color/chooser/SaturationBasedSelector.java
@@ -1,7 +1,30 @@
+/*******************************************************************************
+ * Copyright (C) 2010 - 2013 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
+ * 
+ * Unless you have purchased a commercial license agreement from Jaspersoft, 
+ * the following license terms apply:
+ * 
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ * 
+ * Contributors:
+ *     Jaspersoft Studio Team - initial API and implementation
+ ******************************************************************************/
 package com.jaspersoft.studio.property.color.chooser;
 
 import org.eclipse.swt.graphics.RGB;
 
+/**
+ * Governor for the ColorSelectorWidget where the user clicking the rectangle
+ * slider area control the saturation of the color, and on the square area are shown
+ * the color variations of hue and brightness with the selected saturation
+ * 
+ * @author Orlandin Marco
+ *
+ */
 public class SaturationBasedSelector implements IWidgetGovernor{
 
 	@Override
