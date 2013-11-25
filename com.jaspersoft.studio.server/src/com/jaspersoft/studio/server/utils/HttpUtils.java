8dfa85aa8c4561a251c5a8539af798b2eb386015
chicuslavic <chicuslavic@tibco.com>
2013-11-25 09:49:17 +0000

    refactored http client for rest api
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@3564 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/utils/HttpUtils.java
+++ com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/utils/HttpUtils.java
@@ -1,23 +1,103 @@
 package com.jaspersoft.studio.server.utils;
 
 import java.io.IOException;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.Map;
 
-import org.apache.commons.httpclient.HttpClient;
-import org.apache.commons.httpclient.HttpException;
-import org.apache.commons.httpclient.HttpMethod;
-import org.apache.commons.httpclient.HttpStatus;
-import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.http.HttpException;
+import org.apache.http.HttpHost;
+import org.apache.http.auth.Credentials;
+import org.apache.http.auth.NTCredentials;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.eclipse.core.net.proxy.IProxyChangeEvent;
+import org.eclipse.core.net.proxy.IProxyChangeListener;
+import org.eclipse.core.net.proxy.IProxyData;
+import org.eclipse.core.net.proxy.IProxyService;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceReference;
+
+import com.jaspersoft.studio.server.Activator;
+import com.jaspersoft.studio.server.model.server.ServerProfile;
+import com.jaspersoft.studio.server.protocol.restv2.RestV2Connection;
 
 public class HttpUtils {
-	public static HttpMethod get(HttpClient client, String url)
-			throws HttpException, IOException {
-		HttpMethod method = new GetMethod(url);
-		method.setRequestHeader("Accept", "application/json");
 
-		System.out.println(method.getURI());
-		int statusCode = client.executeMethod(method);
-		if (statusCode != HttpStatus.SC_OK)
-			System.err.println("Method failed: " + method.getStatusLine());
-		return method;
+	public static Executor setupProxy(Executor exec, URI uri) {
+		for (IProxyData d : proxyService.select(uri)) {
+			exec.auth(new HttpHost(d.getHost(), d.getPort()), getCredentials(d));
+			exec.authPreemptiveProxy(new HttpHost(d.getHost(), d.getPort()));
+			break;
+		}
+		executors.put(exec, uri);
+		return exec;
+	}
+
+	public static Request setRequest(Request req, ServerProfile sp) {
+		req.connectTimeout(sp.getTimeout());
+		req.socketTimeout(sp.getTimeout());
+		if (sp.isChunked())
+			req.setHeader("Transfer-Encoding", "chunked");
+		else
+			req.removeHeaders("Transfer-Encoding");
+		req.setHeader("Accept", "application/" + RestV2Connection.FORMAT);
+		return req;
+	}
+
+	public static Request get(String url, ServerProfile sp) throws HttpException, IOException {
+		System.out.println(url);
+		return HttpUtils.setRequest(Request.Get(url), sp);
+	}
+
+	public static Request put(String url, ServerProfile sp) throws HttpException, IOException {
+		System.out.println(url);
+		return HttpUtils.setRequest(Request.Put(url), sp);
+	}
+
+	public static Request post(String url, ServerProfile sp) throws HttpException, IOException {
+		System.out.println(url);
+		return HttpUtils.setRequest(Request.Post(url), sp);
+	}
+
+	public static Request delete(String url, ServerProfile sp) throws HttpException, IOException {
+		System.out.println(url);
+		return HttpUtils.setRequest(Request.Delete(url), sp);
+	}
+
+	private static Map<Executor, URI> executors = new HashMap<Executor, URI>();
+
+	public static IProxyService getProxyService() {
+		BundleContext bc = Activator.getDefault().getBundle().getBundleContext();
+		ServiceReference serviceReference = bc.getServiceReference(IProxyService.class.getName());
+		IProxyService service = (IProxyService) bc.getService(serviceReference);
+		service.addProxyChangeListener(new IProxyChangeListener() {
+
+			@Override
+			public void proxyInfoChanged(IProxyChangeEvent event) {
+				for (Executor exe : executors.keySet())
+					setupProxy(exe, executors.get(exe));
+			}
+		});
+		return service;
+	}
+
+	private static IProxyService proxyService = getProxyService();
+
+	protected static Credentials getCredentials(IProxyData data) {
+		String userId = data.getUserId();
+		Credentials proxyCred = new UsernamePasswordCredentials(userId, data.getPassword());
+		// if the username is in the form "user\domain"
+		// then use NTCredentials instead.
+		int domainIndex = userId.indexOf("\\");
+		if (domainIndex > 0) {
+			String domain = userId.substring(0, domainIndex);
+			if (userId.length() > domainIndex + 1) {
+				String user = userId.substring(domainIndex + 1);
+				proxyCred = new NTCredentials(user, data.getPassword(), data.getHost(), domain);
+			}
+		}
+		return proxyCred;
 	}
 }
