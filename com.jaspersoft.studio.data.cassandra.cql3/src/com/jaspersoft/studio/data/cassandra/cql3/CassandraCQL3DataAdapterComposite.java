3a74fe864f1e4ee9a2ff2442d8147e801b908f70
chicuslavic <chicuslavic@tibco.com>
2013-12-09 18:02:22 +0000

    fixed a number format problem
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@3616 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.data.cassandra.cql3/src/com/jaspersoft/studio/data/cassandra/cql3/CassandraCQL3DataAdapterComposite.java
+++ com.jaspersoft.studio.data.cassandra.cql3/src/com/jaspersoft/studio/data/cassandra/cql3/CassandraCQL3DataAdapterComposite.java
@@ -3,7 +3,15 @@ package com.jaspersoft.studio.data.cassandra.cql3;
 import net.sf.jasperreports.data.DataAdapter;
 import net.sf.jasperreports.engine.JasperReportsContext;
 
+import org.eclipse.core.databinding.UpdateValueStrategy;
 import org.eclipse.core.databinding.beans.PojoObservables;
+import org.eclipse.core.databinding.conversion.IConverter;
+import org.eclipse.core.databinding.conversion.NumberToStringConverter;
+import org.eclipse.core.databinding.conversion.StringToNumberConverter;
+import org.eclipse.core.databinding.validation.IValidator;
+import org.eclipse.core.databinding.validation.ValidationStatus;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
 import org.eclipse.jface.databinding.swt.SWTObservables;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridData;
@@ -12,20 +20,21 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
 
+import com.ibm.icu.text.NumberFormat;
 import com.jaspersoft.cassandra.cql3.adapter.CassandraCQL3DataAdapter;
 import com.jaspersoft.studio.data.ADataAdapterComposite;
 import com.jaspersoft.studio.data.DataAdapterDescriptor;
 
 /**
  * 
- * @author Raul Gallegos 
+ * @author Raul Gallegos
  * 
  */
 public class CassandraCQL3DataAdapterComposite extends ADataAdapterComposite {
 	private Text hostname;
 	private Text port;
 	private Text keyspace;
-	private Text cassandraVersion;	
+	private Text cassandraVersion;
 	private Text clustername;
 
 	private CassandraCQL3DataAdapterDescriptor dataAdapterDescriptor;
@@ -53,15 +62,12 @@ public class CassandraCQL3DataAdapterComposite extends ADataAdapterComposite {
 	private void createLabel(String text) {
 		Label label = new Label(this, SWT.NONE);
 		label.setText(text);
-		label.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1,
-				1));
+		label.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, false, false, 1, 1));
 	}
 
 	private Text createTextField(boolean password) {
-		Text textField = new Text(this, !password ? SWT.BORDER : SWT.BORDER
-				| SWT.PASSWORD);
-		textField.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false,
-				1, 1));
+		Text textField = new Text(this, !password ? SWT.BORDER : SWT.BORDER | SWT.PASSWORD);
+		textField.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		return textField;
 	}
 
@@ -77,28 +83,33 @@ public class CassandraCQL3DataAdapterComposite extends ADataAdapterComposite {
 		super.setDataAdapter(dataAdapterDescriptor);
 
 		this.dataAdapterDescriptor = (CassandraCQL3DataAdapterDescriptor) dataAdapterDescriptor;
-		CassandraCQL3DataAdapter dataAdapter = (CassandraCQL3DataAdapter) dataAdapterDescriptor
-				.getDataAdapter();
+		CassandraCQL3DataAdapter dataAdapter = (CassandraCQL3DataAdapter) dataAdapterDescriptor.getDataAdapter();
 		bindWidgets(dataAdapter);
 	}
 
 	@Override
 	protected void bindWidgets(DataAdapter dataAdapter) {
-		bindingContext.bindValue(
-				SWTObservables.observeText(hostname, SWT.Modify),
-				PojoObservables.observeValue(dataAdapter, "hostname"));
-		bindingContext.bindValue(
-				SWTObservables.observeText(port, SWT.Modify),
-				PojoObservables.observeValue(dataAdapter, "port"));
-		bindingContext.bindValue(
-				SWTObservables.observeText(keyspace, SWT.Modify),
-				PojoObservables.observeValue(dataAdapter, "keyspace"));
-		bindingContext.bindValue(
-				SWTObservables.observeText(cassandraVersion, SWT.Modify),
-				PojoObservables.observeValue(dataAdapter, "cassandraVersion"));
-		bindingContext.bindValue(
-				SWTObservables.observeText(clustername, SWT.Modify),
-				PojoObservables.observeValue(dataAdapter, "clustername"));
+		bindingContext.bindValue(SWTObservables.observeText(hostname, SWT.Modify), PojoObservables.observeValue(dataAdapter, "hostname"));
+		IValidator validator = new IValidator() {
+			public IStatus validate(Object value) {
+				String stringValue = (String) value;
+				try {
+					new Integer(stringValue).intValue();
+					return Status.OK_STATUS;
+				} catch (NumberFormatException ex) {
+					return ValidationStatus.error("Not an integer");
+				}
+			}
+		};
+		NumberFormat numberFormat = NumberFormat.getIntegerInstance();
+		numberFormat.setGroupingUsed(false);
+		IConverter targetToModelConverter = StringToNumberConverter.toInteger(numberFormat, true);
+		IConverter modelToTargetConverter = NumberToStringConverter.fromInteger(numberFormat, true);
+		bindingContext.bindValue(SWTObservables.observeText(port, SWT.Modify), PojoObservables.observeValue(dataAdapter, "port"),
+				new UpdateValueStrategy().setAfterGetValidator(validator).setConverter(targetToModelConverter), new UpdateValueStrategy().setConverter(modelToTargetConverter));
+		bindingContext.bindValue(SWTObservables.observeText(keyspace, SWT.Modify), PojoObservables.observeValue(dataAdapter, "keyspace"));
+		bindingContext.bindValue(SWTObservables.observeText(cassandraVersion, SWT.Modify), PojoObservables.observeValue(dataAdapter, "cassandraVersion"));
+		bindingContext.bindValue(SWTObservables.observeText(clustername, SWT.Modify), PojoObservables.observeValue(dataAdapter, "clustername"));
 	}
 
 	@Override
