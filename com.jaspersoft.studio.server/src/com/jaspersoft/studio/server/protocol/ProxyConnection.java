547e953ce69ecf4ced213417b8f2e9c5abcd7f9a
chicuslavic <chicuslavic@tibco.com>
2014-01-28 16:07:07 +0000

    Bug 35932 - [case 44364] 1MB or larger JRXML files can't be published to Jasper Reports Server
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@3728 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/protocol/ProxyConnection.java
+++ com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/protocol/ProxyConnection.java
@@ -17,6 +17,7 @@ import com.jaspersoft.jasperserver.dto.serverinfo.ServerInfo;
 import com.jaspersoft.studio.server.AFinderUI;
 import com.jaspersoft.studio.server.Activator;
 import com.jaspersoft.studio.server.model.server.ServerProfile;
+import com.jaspersoft.studio.server.protocol.restv2.RestV2ConnectionJersey;
 
 public class ProxyConnection implements IConnection {
 	public Format getDateFormat() {
@@ -36,7 +37,7 @@ public class ProxyConnection implements IConnection {
 	private IConnection[] getConnections() {
 		List<IConnection> c = new ArrayList<IConnection>();
 		// c.add(new RestV2Connection());
-		// c.add(new RestV2ConnectionJersey());
+		c.add(new RestV2ConnectionJersey());
 
 		c.addAll(Activator.getExtManager().getProtocols());
 		for (IConnection con : c)
