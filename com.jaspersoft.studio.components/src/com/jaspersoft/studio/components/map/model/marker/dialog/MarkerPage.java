d10d40c558d58fa9272f222c09ad5533109591ba
mrabbi <mrabbi@tibco.com>
2014-03-03 17:03:40 +0000

    Introduced map widget to modify some component information.
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@3883 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/map/model/marker/dialog/MarkerPage.java
+++ com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/map/model/marker/dialog/MarkerPage.java
@@ -20,8 +20,14 @@ import java.util.List;
 
 import net.sf.jasperreports.components.map.Item;
 import net.sf.jasperreports.components.map.ItemProperty;
+import net.sf.jasperreports.components.map.MapComponent;
 import net.sf.jasperreports.components.map.StandardItem;
 import net.sf.jasperreports.components.map.StandardItemProperty;
+import net.sf.jasperreports.components.map.StandardMapComponent;
+import net.sf.jasperreports.components.map.type.MapTypeEnum;
+import net.sf.jasperreports.eclipse.ui.util.UIUtils;
+import net.sf.jasperreports.engine.design.JRDesignDataset;
+import net.sf.jasperreports.engine.design.JRDesignExpression;
 
 import org.eclipse.jface.viewers.ColumnWeightData;
 import org.eclipse.jface.viewers.TableLayout;
@@ -35,10 +41,18 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableColumn;
+import org.eclipse.ui.forms.events.HyperlinkAdapter;
+import org.eclipse.ui.forms.events.HyperlinkEvent;
+import org.eclipse.ui.forms.widgets.FormText;
 
+import com.buzzcoders.yasw.widgets.map.core.LatLng;
+import com.buzzcoders.yasw.widgets.map.core.MapType;
+import com.buzzcoders.yasw.widgets.map.ui.MarkersPickupDialog;
 import com.jaspersoft.studio.components.map.messages.Messages;
+import com.jaspersoft.studio.components.map.model.MMap;
 import com.jaspersoft.studio.components.map.model.marker.MarkersDTO;
 import com.jaspersoft.studio.swt.widgets.table.DeleteButton;
 import com.jaspersoft.studio.swt.widgets.table.EditButton;
@@ -47,6 +61,8 @@ import com.jaspersoft.studio.swt.widgets.table.INewElement;
 import com.jaspersoft.studio.swt.widgets.table.ListContentProvider;
 import com.jaspersoft.studio.swt.widgets.table.ListOrderButtons;
 import com.jaspersoft.studio.swt.widgets.table.NewButton;
+import com.jaspersoft.studio.utils.ExpressionInterpreter;
+import com.jaspersoft.studio.utils.Misc;
 import com.jaspersoft.studio.utils.ModelUtils;
 
 public class MarkerPage extends WizardPage {
@@ -58,7 +74,6 @@ public class MarkerPage extends WizardPage {
 				return;
 			v = (Item) v.clone();
 			MarkerDialog dialog = new MarkerDialog(Display.getDefault().getActiveShell());
-
 			dialog.setValue((StandardItem) v, ModelUtils.getElementExpressionContext(null, value.getPnode()));
 			if (dialog.open() == Window.OK)
 				input.set(pos, v);
@@ -69,6 +84,7 @@ public class MarkerPage extends WizardPage {
 	private Table table;
 	private TableViewer tableViewer;
 	private EditButton<Item> editButton;
+	private BasicMapInfo mapInfo;
 
 	public MarkersDTO getValue() {
 		return new MarkersDTO(value.getMarkers(), value.getPnode());
@@ -99,7 +115,47 @@ public class MarkerPage extends WizardPage {
 		GridLayout layout = new GridLayout(2, false);
 		composite.setLayout(layout);
 		setControl(composite);
-
+		
+		FormText mapPickSuggestion=new FormText(composite, SWT.NONE);
+		mapPickSuggestion.setText("<form><p><b>You can easily pickup static markers using the</b><a href=\"\">map preview</a></p></form>", true, false);
+		mapPickSuggestion.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false,2,1));
+		mapPickSuggestion.setWhitespaceNormalized(true);
+		mapPickSuggestion.addHyperlinkListener(new HyperlinkAdapter() {
+			@Override
+			public void linkActivated(HyperlinkEvent e) {
+				MarkersPickupDialog staticMarkersDialog = new MarkersPickupDialog(getShell()){
+					@Override
+					protected void configureShell(Shell newShell) {
+						super.configureShell(newShell);
+						UIUtils.resizeAndCenterShell(newShell, 800, 600);
+					}
+				};
+				if(mapInfo==null){
+					mapInfo=getBasicMapInformation();
+				}
+				if(mapInfo.getLatitude()!=null && mapInfo.getLongitude()!=null){
+					staticMarkersDialog.setInitialPosition(new LatLng(mapInfo.getLatitude(), mapInfo.getLongitude(), true));
+				}
+				if(mapInfo.getMapType()!=null) {
+					staticMarkersDialog.setType(
+							MapType.fromStringID(mapInfo.getMapType().getName()));
+				}
+				if(mapInfo.getZoom()!=0){
+					staticMarkersDialog.setZoom(mapInfo.getZoom());
+				}
+				if(staticMarkersDialog.open()==Window.OK) {
+					List<LatLng> markersList = staticMarkersDialog.getMarkersList();
+					for(LatLng m : markersList) {
+						StandardItem newMarker = new StandardItem();
+						newMarker.addItemProperty(new StandardItemProperty("latitude", null, new JRDesignExpression(m.getLat().floatValue()+"f"))); //$NON-NLS-1$ //$NON-NLS-2$
+						newMarker.addItemProperty(new StandardItemProperty("longitude", null, new JRDesignExpression(m.getLng().floatValue()+"f"))); //$NON-NLS-1$ //$NON-NLS-2$
+						value.getMarkers().add(newMarker);
+					}
+					tableViewer.setInput(value.getMarkers());
+				}
+			}
+		});
+		
 		buildTable(composite);
 
 		GridData gd = new GridData(GridData.FILL_BOTH);
@@ -114,7 +170,6 @@ public class MarkerPage extends WizardPage {
 
 			public Object newElement(List<?> input, int pos) {
 				ArrayList<ItemProperty> props = new ArrayList<ItemProperty>();
-
 				props.add(new StandardItemProperty("id", "CHANGE_ME_ID", null)); //$NON-NLS-1$ //$NON-NLS-2$
 				props.add(new StandardItemProperty("label", "CHANGE_ME_LABEL", null)); //$NON-NLS-1$ //$NON-NLS-2$
 				props.add(new StandardItemProperty("latitude", "0", null)); //$NON-NLS-1$ //$NON-NLS-2$
@@ -133,6 +188,8 @@ public class MarkerPage extends WizardPage {
 		editButton.createEditButtons(bGroup, tableViewer, new EditElement());
 		new DeleteButton().createDeleteButton(bGroup, tableViewer);
 		new ListOrderButtons().createOrderButtons(bGroup, tableViewer);
+
+		table.setFocus();
 	}
 
 	private void buildTable(Composite composite) {
@@ -191,4 +248,86 @@ public class MarkerPage extends WizardPage {
 		tableViewer.setInput(props);
 	}
 
+	/*
+	 * Gets the basic information: map center, zoom and type.
+	 */
+	private BasicMapInfo getBasicMapInformation() {
+		MMap mapRef = (MMap) value.getPnode();
+		BasicMapInfo info = new BasicMapInfo();
+		JRDesignDataset dataset = ModelUtils.getDataset(mapRef);
+		if(dataset==null){
+			dataset = (JRDesignDataset) mapRef.getJasperDesign().getMainDataset();
+		}
+		ExpressionInterpreter expIntr = 
+				new ExpressionInterpreter(dataset, mapRef.getJasperDesign(), mapRef.getJasperConfiguration());
+		// Center
+		JRDesignExpression latitudeExpr = (JRDesignExpression) mapRef.getPropertyValue(StandardMapComponent.PROPERTY_LATITUDE_EXPRESSION);
+		JRDesignExpression longitudeExpr = (JRDesignExpression) mapRef.getPropertyValue(StandardMapComponent.PROPERTY_LONGITUDE_EXPRESSION);
+		if(latitudeExpr!=null && longitudeExpr!=null) {
+			Object latObj = expIntr.interpretExpression(Misc.nvl(latitudeExpr.getText()));
+			Object lngObj = expIntr.interpretExpression(Misc.nvl(longitudeExpr.getText()));
+			if(latObj instanceof Number && lngObj instanceof Number) {
+				info.setLatitude(((Number)latObj).doubleValue());
+				info.setLongitude(((Number)lngObj).doubleValue());
+			}
+		}
+		// Zoom
+		JRDesignExpression zoomExpr = (JRDesignExpression) mapRef.getPropertyValue(StandardMapComponent.PROPERTY_ZOOM_EXPRESSION);
+		if(zoomExpr!=null) {
+			Object zoomObj = expIntr.interpretExpression(zoomExpr.getText());
+			if(zoomObj instanceof Number) {
+				info.setZoom(((Number) zoomObj).intValue());
+			}
+			else {
+				info.setZoom(MapComponent.DEFAULT_ZOOM);
+			}
+		}		
+		// Map Type
+		Integer type = (Integer) mapRef.getPropertyValue(StandardMapComponent.PROPERTY_MAP_TYPE);
+		if(type!=null){
+			MapTypeEnum typeVal = MapTypeEnum.getByValue(type.byteValue());
+			info.setMapType(typeVal);
+		}
+		return info;
+	}
+	
+	class BasicMapInfo {
+
+		private Double latitude;
+		private Double longitude;
+		private MapTypeEnum mapType;
+		private int zoom;
+
+		public Double getLatitude() {
+			return latitude;
+		}
+
+		public Double getLongitude() {
+			return longitude;
+		}
+
+		public MapTypeEnum getMapType() {
+			return mapType;
+		}
+
+		public int getZoom() {
+			return zoom;
+		}
+
+		public void setLatitude(Double latitude) {
+			this.latitude = latitude;
+		}
+
+		public void setLongitude(Double longitude) {
+			this.longitude = longitude;
+		}
+
+		public void setMapType(MapTypeEnum mapType) {
+			this.mapType = mapType;
+		}
+
+		public void setZoom(int zoom) {
+			this.zoom = zoom;
+		}
+	}
 }
--- com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/map/property/MapSection.java
+++ com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/map/property/MapSection.java
@@ -16,11 +16,22 @@
 package com.jaspersoft.studio.components.map.property;
 
 import net.sf.jasperreports.components.map.StandardMapComponent;
+import net.sf.jasperreports.eclipse.ui.util.UIUtils;
+import net.sf.jasperreports.engine.design.JRDesignExpression;
 
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.forms.events.HyperlinkAdapter;
+import org.eclipse.ui.forms.events.HyperlinkEvent;
+import org.eclipse.ui.forms.widgets.FormText;
 import org.eclipse.ui.views.properties.IPropertyDescriptor;
 
+import com.buzzcoders.yasw.widgets.map.core.LatLng;
+import com.buzzcoders.yasw.widgets.map.ui.BasicInfoMapDialog;
 import com.jaspersoft.studio.components.map.messages.Messages;
 import com.jaspersoft.studio.properties.view.TabbedPropertySheetPage;
 import com.jaspersoft.studio.property.section.AbstractSection;
@@ -40,32 +51,48 @@ public class MapSection extends AbstractSection {
 	public void createControls(Composite parent,
 			TabbedPropertySheetPage tabbedPropertySheetPage) {
 		super.createControls(parent, tabbedPropertySheetPage);
-
 		parent.setLayout(new GridLayout(2, false));
 
-		IPropertyDescriptor pd = getPropertyDesriptor(StandardMapComponent.PROPERTY_EVALUATION_TIME);
-		IPropertyDescriptor gpd = getPropertyDesriptor(StandardMapComponent.PROPERTY_EVALUATION_GROUP);
-		getWidgetFactory().createCLabel(parent, pd.getDisplayName());
-		widgets.put(pd.getId(), new SPEvaluationTime(parent, this, pd, gpd));
-
+		FormText mapPickSuggestion=new FormText(parent, SWT.NONE);
+		mapPickSuggestion.setText("<form><p><b>You can pickup the map center, zooom and type by</b><a href=\"\">clicking here</a></p></form>", true, false);
+		mapPickSuggestion.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false,2,1));
+		mapPickSuggestion.setWhitespaceNormalized(true);
+		mapPickSuggestion.addHyperlinkListener(new HyperlinkAdapter() {
+			@Override
+			public void linkActivated(HyperlinkEvent e) {
+				BasicInfoMapDialog pickmapDialog = new BasicInfoMapDialog(UIUtils.getShell()) {
+					@Override
+					protected void configureShell(Shell newShell) {
+						super.configureShell(newShell);
+						UIUtils.resizeAndCenterShell(newShell, 800, 600);
+					}
+				};
+				if(pickmapDialog.open()==Dialog.OK){
+					LatLng center=pickmapDialog.getMapCenter();
+					int zoom = pickmapDialog.getZoomLevel();
+					getElement().setPropertyValue(StandardMapComponent.PROPERTY_LATITUDE_EXPRESSION, new JRDesignExpression(center.getLat()+"f"));
+					getElement().setPropertyValue(StandardMapComponent.PROPERTY_LONGITUDE_EXPRESSION, new JRDesignExpression(center.getLng()+"f"));
+					getElement().setPropertyValue(StandardMapComponent.PROPERTY_ZOOM_EXPRESSION, new JRDesignExpression(String.valueOf(zoom)));
+					getElement().setPropertyValue(StandardMapComponent.PROPERTY_MAP_TYPE, pickmapDialog.getMapType().ordinal());
+				}
+			}
+		});
+		createWidget4Property(parent, StandardMapComponent.PROPERTY_MAP_TYPE);
 		createWidget4Property(parent,
 				StandardMapComponent.PROPERTY_LATITUDE_EXPRESSION);
-
 		createWidget4Property(parent,
 				StandardMapComponent.PROPERTY_LONGITUDE_EXPRESSION);
-
 		createWidget4Property(parent,
 				StandardMapComponent.PROPERTY_ZOOM_EXPRESSION);
 
 		createWidget4Property(parent,
 				StandardMapComponent.PROPERTY_LANGUAGE_EXPRESSION);
-
-		createWidget4Property(parent, StandardMapComponent.PROPERTY_MAP_TYPE);
-
 		createWidget4Property(parent, StandardMapComponent.PROPERTY_MAP_SCALE);
-
+		IPropertyDescriptor pd = getPropertyDesriptor(StandardMapComponent.PROPERTY_EVALUATION_TIME);
+		IPropertyDescriptor gpd = getPropertyDesriptor(StandardMapComponent.PROPERTY_EVALUATION_GROUP);
+		getWidgetFactory().createCLabel(parent, pd.getDisplayName());
+		widgets.put(pd.getId(), new SPEvaluationTime(parent, this, pd, gpd));
 		createWidget4Property(parent, StandardMapComponent.PROPERTY_IMAGE_TYPE);
-
 		createWidget4Property(parent, StandardMapComponent.PROPERTY_ON_ERROR_TYPE);
 	}
 	
