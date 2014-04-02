3e927d1fec510c595a32dc1a12c164b2bb005939
chicuslavic <chicuslavic@tibco.com>
2014-04-02 10:13:52 +0000

    revert action, this was commited by error
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@4031 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/editor/action/snap/SnapToGeometryAction.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/editor/action/snap/SnapToGeometryAction.java
@@ -1,29 +1,37 @@
 /*******************************************************************************
- * Copyright (C) 2010 - 2013 Jaspersoft Corporation. All rights reserved. http://www.jaspersoft.com
+ * Copyright (C) 2010 - 2013 Jaspersoft Corporation. All rights reserved.
+ * http://www.jaspersoft.com
  * 
- * Unless you have purchased a commercial license agreement from Jaspersoft, the following license terms apply:
+ * Unless you have purchased a commercial license agreement from Jaspersoft, 
+ * the following license terms apply:
  * 
- * This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
  * 
- * Contributors: Jaspersoft Studio Team - initial API and implementation
+ * Contributors:
+ *     Jaspersoft Studio Team - initial API and implementation
  ******************************************************************************/
 package com.jaspersoft.studio.editor.action.snap;
 
+import org.eclipse.gef.ui.actions.GEFActionConstants;
+
+import com.jaspersoft.studio.messages.Messages;
+import com.jaspersoft.studio.preferences.RulersGridPreferencePage;
 import com.jaspersoft.studio.utils.jasper.JasperReportsConfiguration;
 
 public class SnapToGeometryAction extends ACheckResourcePrefAction {
-	public static final String ID = "showPublishDialogOnSave";
 
 	public SnapToGeometryAction(JasperReportsConfiguration jrConfig) {
-		super("Show Publish Dialog On Save", jrConfig);
-		setText("Show Publish Dialog On Save");
-		setToolTipText("Show Publish Dialog On Save");
-		setId(ID);
+		super(Messages.common_snap_to_guides, jrConfig);
+		setText(Messages.SnapToGeometryAction_label);
+		setToolTipText(Messages.SnapToGeometryAction_toolTip);
+		setId(GEFActionConstants.TOGGLE_SNAP_TO_GEOMETRY);
 	}
 
 	@Override
 	protected String getProperty() {
-		return ID;
+		return RulersGridPreferencePage.P_PAGE_RULERGRID_SNAPTOGEOMETRY;
 	}
 }
