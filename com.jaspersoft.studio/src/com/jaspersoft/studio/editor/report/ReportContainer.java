c4c5fd3f8bdd753ab5ef1e8eef995721a05875ab
morlandin <morlandin@tibco.com>
2014-04-14 10:04:57 +0000

    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@4109 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/editor/report/ReportContainer.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/editor/report/ReportContainer.java
@@ -14,8 +14,6 @@ import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
