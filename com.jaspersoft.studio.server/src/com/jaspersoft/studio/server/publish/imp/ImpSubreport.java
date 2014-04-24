095151cc3d8d110a7705ac9c149e628cb43bca0b
chicuslavic <chicuslavic@tibco.com>
2014-04-24 16:34:26 +0000

    Bug 37286 - publishing report with subreport links subreportname.jrxml instead of subreportname.jasper
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@4172 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/publish/imp/ImpSubreport.java
+++ com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/publish/imp/ImpSubreport.java
@@ -16,16 +16,21 @@
 package com.jaspersoft.studio.server.publish.imp;
 
 import java.io.File;
+import java.util.Set;
 
 import net.sf.jasperreports.engine.design.JRDesignElement;
 import net.sf.jasperreports.engine.design.JRDesignExpression;
 import net.sf.jasperreports.engine.design.JRDesignSubreport;
 
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IProgressMonitor;
 
 import com.jaspersoft.jasperserver.api.metadata.xml.domain.impl.ResourceDescriptor;
+import com.jaspersoft.studio.server.model.AFileResource;
 import com.jaspersoft.studio.server.model.MJrxml;
 import com.jaspersoft.studio.server.model.MReportUnit;
+import com.jaspersoft.studio.server.publish.PublishOptions;
+import com.jaspersoft.studio.utils.Misc;
 import com.jaspersoft.studio.utils.jasper.JasperReportsConfiguration;
 
 public class ImpSubreport extends AImpObject {
@@ -40,6 +45,14 @@ public class ImpSubreport extends AImpObject {
 		if (f == null)
 			f = super.findFile(file, str);
 		return f;
+	}
+
+	@Override
+	protected AFileResource addResource(IProgressMonitor monitor, MReportUnit mrunit, Set<String> fileset, File f, PublishOptions popt) {
+		String exp = popt.getExpression();
+		if (!Misc.isNullOrEmpty(exp))
+			popt.setExpression(exp.replaceAll(".jasper", ".jrxml"));
+		return super.addResource(monitor, mrunit, fileset, f, popt);
 	}
 
 	protected ResourceDescriptor createResource(MReportUnit mrunit) {
