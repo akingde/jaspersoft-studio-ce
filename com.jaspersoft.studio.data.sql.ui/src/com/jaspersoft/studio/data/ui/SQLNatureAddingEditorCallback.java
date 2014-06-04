c11d1fd8713fc50624e57cdb7648772ce8cf5fcb
chicuslavic <chicuslavic@tibco.com>
2014-06-04 07:45:16 +0000

    DDL generation
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@4308 6833c83e-4fd0-4c68-a3df-283c6148db30

---
+++ com.jaspersoft.studio.data.sql.ui/src/com/jaspersoft/studio/data/ui/SQLNatureAddingEditorCallback.java
@@ -0,0 +1,14 @@
+package com.jaspersoft.studio.data.ui;
+
+import org.eclipse.xtext.ui.editor.AbstractDirtyStateAwareEditorCallback;
+import org.eclipse.xtext.ui.editor.XtextEditor;
+
+public class SQLNatureAddingEditorCallback extends AbstractDirtyStateAwareEditorCallback {
+
+	@Override
+	public void afterCreatePartControl(XtextEditor editor) {
+		super.afterCreatePartControl(editor);
+
+	}
+
+}
