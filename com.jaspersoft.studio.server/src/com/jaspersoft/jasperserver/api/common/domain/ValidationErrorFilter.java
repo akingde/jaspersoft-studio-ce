c88599795194d1fc1405888bfddda07d7aab36e4
mrabbi <mrabbi@tibco.com>
2014-07-24 13:49:42 +0000

    Updated copyright header.
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@4423 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.server/src/com/jaspersoft/jasperserver/api/common/domain/ValidationErrorFilter.java
+++ com.jaspersoft.studio.server/src/com/jaspersoft/jasperserver/api/common/domain/ValidationErrorFilter.java
@@ -1,23 +1,16 @@
-/*
- * Copyright (C) 2005 - 2011 Jaspersoft Corporation. All rights reserved.
+/*******************************************************************************
+ * Copyright (C) 2005 - 2014 TIBCO Software Inc. All rights reserved.
  * http://www.jaspersoft.com.
- *
+ * 
  * Unless you have purchased  a commercial license agreement from Jaspersoft,
  * the following license terms  apply:
- *
- * This program is free software: you can redistribute it and/or  modify
- * it under the terms of the GNU Affero General Public License  as
- * published by the Free Software Foundation, either version 3 of  the
- * License, or (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Affero  General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public  License
- * along with this program. If not, see <http://www.gnu.org/licenses/>.
- */
+ * 
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+
 package com.jaspersoft.jasperserver.api.common.domain;
 
 import com.jaspersoft.jasperserver.api.JasperServerAPI;
