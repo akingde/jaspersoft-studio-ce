88e1d1723813c129a9d52aa34efa14c969cd084f
morlandin <morlandin@tibco.com>
2014-08-13 15:28:36 +0000

    fixed encoding problem
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@4478 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/property/descriptor/pattern/dialog/CurrencyPattern.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/property/descriptor/pattern/dialog/CurrencyPattern.java
@@ -21,20 +21,29 @@ import com.jaspersoft.studio.messages.Messages;
 
 public class CurrencyPattern extends NumericPattern {
 
+	private static String[] patterns = {
+		"\u00A4#,##0.##;\u00A4-##0.##",
+		"#,##0.##\u00A4;#,##0.##- \u00A4",
+		"#,##0.##\u00A4;(#,##0.##) \u00A4",
+		"\u00A4#,##0.###;\u00A4(-#,##0.###)",
+		"\u00A4#,##0.###;\u00A4(#,##0.###-)"
+	};
+	
 	public CurrencyPattern(Composite parent) {
 		super(parent, NumberFormat.getCurrencyInstance());
 		setDescription(Messages.CurrencyPattern_description);
 	}
-
+	
 	@Override
 	protected java.util.List<String> getDefaults() {
 		if (dList == null) {
 			dList = new ArrayList<String>();
-			dList.add("�#,##0.##;�-##0.##"); //$NON-NLS-1$
-			dList.add("#,##0.##�;#,##0.##- �"); //$NON-NLS-1$
-			dList.add("#,##0.##�;(#,##0.##) �"); //$NON-NLS-1$
-			dList.add("�#,##0.###;�(-#,##0.###)"); //$NON-NLS-1$
-			dList.add("�#,##0.###;�(#,##0.###-)"); //$NON-NLS-1$
+			
+			for(String pattern : patterns){
+				dList.add(pattern);
+				//pattern.replace("\u00A4", currencySymbol);
+			}
+			
 			setPattern(dList.get(0));
 		}
 		return dList;
