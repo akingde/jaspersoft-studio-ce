fe6216f6d5290f9466ec9c4175310f1c592ec86f
chicuslavic <chicuslavic@tibco.com>
2014-08-13 10:12:14 +0000

    now, it's possible to have .jrxml in subreport expression
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@4475 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/utils/jasper/JSSFileRepositoryService.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/utils/jasper/JSSFileRepositoryService.java
@@ -1,14 +1,10 @@
 /*******************************************************************************
- * Copyright (C) 2005 - 2014 TIBCO Software Inc. All rights reserved.
- * http://www.jaspersoft.com.
+ * Copyright (C) 2005 - 2014 TIBCO Software Inc. All rights reserved. http://www.jaspersoft.com.
  * 
- * Unless you have purchased  a commercial license agreement from Jaspersoft,
- * the following license terms  apply:
+ * Unless you have purchased a commercial license agreement from Jaspersoft, the following license terms apply:
  * 
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
  ******************************************************************************/
 package com.jaspersoft.studio.utils.jasper;
 
@@ -86,7 +82,10 @@ public class JSSFileRepositoryService implements RepositoryService {
 		} catch (JRRuntimeException e) {
 		}
 		try {
-			if (ReportResource.class.equals(resourceType) && uri.endsWith(FileExtension.PointJASPER)) {
+			if (ReportResource.class.equals(resourceType) && uri.endsWith(FileExtension.PointJRXML)) {
+				return doGetResource(uri.replaceAll(FileExtension.PointJRXML + "$", FileExtension.PointJASPER), resourceType,
+						rs);
+			} else if (ReportResource.class.equals(resourceType) && uri.endsWith(FileExtension.PointJASPER)) {
 				String nuri = uri.replaceAll(FileExtension.PointJASPER + "$", FileExtension.PointJRXML);
 				InputStreamResource inr = rs.getResource(nuri, InputStreamResource.class);
 				if (inr == null)
