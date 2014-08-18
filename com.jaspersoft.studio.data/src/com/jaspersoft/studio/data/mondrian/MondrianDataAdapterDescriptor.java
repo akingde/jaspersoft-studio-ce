979459659fbe1b1cc179979210dc409f9ab9bbee
morlandin <morlandin@tibco.com>
2014-08-18 15:12:47 +0000

    fixed Bug 38700 - [Case #52039] - Unable to get metadata from Mondrian OLAP datasource
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@4490 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.data/src/com/jaspersoft/studio/data/mondrian/MondrianDataAdapterDescriptor.java
+++ com.jaspersoft.studio.data/src/com/jaspersoft/studio/data/mondrian/MondrianDataAdapterDescriptor.java
@@ -12,11 +12,16 @@
  ******************************************************************************/
 package com.jaspersoft.studio.data.mondrian;
 
+import org.eclipse.jface.wizard.WizardPage;
+import org.eclipse.swt.widgets.Composite;
+
 import net.sf.jasperreports.data.mondrian.MondrianDataAdapterImpl;
 import net.sf.jasperreports.engine.JRConstants;
 
+import com.jaspersoft.studio.data.AWizardDataEditorComposite;
 import com.jaspersoft.studio.data.DataAdapterEditor;
 import com.jaspersoft.studio.data.jdbc.JDBCDataAdapterDescriptor;
+import com.jaspersoft.studio.data.ui.WizardQueryEditorComposite;
 import com.jaspersoft.studio.utils.jasper.JasperReportsConfiguration;
 
 /*
@@ -45,6 +50,11 @@ public class MondrianDataAdapterDescriptor extends JDBCDataAdapterDescriptor {
 	public boolean supportsGetFieldsOperation(JasperReportsConfiguration jConfig) {
 		getFieldProvider();
 		return fprovider.supportsGetFieldsOperation(jConfig);
+	}
+	
+	@Override
+	public AWizardDataEditorComposite createDataEditorComposite(Composite parent, WizardPage page) {
+		return new WizardQueryEditorComposite(parent, page, this, "mdx");
 	}
 
 }
