5228e26099b9bc13703fb7514a453d43bcbef97e
morlandin <morlandin@tibco.com>
2014-08-21 14:57:35 +0000

    fixed Bug 38773 - [case 52023] Deleting value in Table Properties > Dataset Run > Data Source Expression doesn't save
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@4507 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/property/descriptor/expression/JRExpressionCellEditor.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/property/descriptor/expression/JRExpressionCellEditor.java
@@ -15,26 +15,26 @@ package com.jaspersoft.studio.property.descriptor.expression;
 import net.sf.jasperreports.engine.design.JRDesignExpression;
 
 import org.eclipse.jface.dialogs.Dialog;
-import org.eclipse.jface.viewers.DialogCellEditor;
 import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 
 import com.jaspersoft.studio.editor.expression.ExpressionContext;
 import com.jaspersoft.studio.editor.expression.ExpressionEditorSupportUtil;
+import com.jaspersoft.studio.property.descriptor.JSSDialogCellEditor;
 import com.jaspersoft.studio.property.descriptor.expression.dialog.JRExpressionEditor;
 
-public class JRExpressionCellEditor extends DialogCellEditor {
+public class JRExpressionCellEditor extends JSSDialogCellEditor {
 
 	private ExpressionContext expContext;
 
 	public JRExpressionCellEditor(Composite parent, ExpressionContext expContext) {
-		super(parent);
+		super(parent, true);
 		this.expContext = expContext;
 	}
 
 	public JRExpressionCellEditor(Composite parent, int style, ExpressionContext expContext) {
-		super(parent, style);
+		super(parent, style, true);
 		this.expContext = expContext;
 	}
 
