4d4ee2c9409ab021cacc2d40f81ee5e274acf2d8
morlandin <morlandin@tibco.com>
2014-10-03 15:07:09 +0000

    added comments for the custom jar adapter plugin
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@4659 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/custom/adapter/Pair.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/custom/adapter/Pair.java
@@ -11,27 +11,60 @@
  * http://www.eclipse.org/legal/epl-v10.html
  ******************************************************************************/
 package com.jaspersoft.studio.custom.adapter;
+
+/**
+ * Container to store the full name of a class, so in the form
+ * package.classname
+ * 
+ * @author Orlandin Marco
+ *
+ */
 public class Pair{
-		
-		private String packageName;
-		
-		private String className;
-		
-		public Pair(String packageName, String className){
-			this.packageName = packageName;
-			this.className = className;
-		}
-		
-		public String getPackageName() {
-			return packageName;
-		}
-		
-		public String getClassName() {
-			return className;
-		}
-		
-		@Override
-		public String toString() {
-			return packageName + "." + className;
-		}
+	
+	/**
+	 * The package value
+	 */
+	private String packageName;
+
+	/**
+	 * The simple name of the class
+	 */
+	private String className;
+
+	/**
+	 * Create the container 
+	 * 
+	 * @param packageName the package value
+	 * @param className the simple name of the class
+	 */
+	public Pair(String packageName, String className) {
+		this.packageName = packageName;
+		this.className = className;
 	}
+	
+	/**
+	 * Return the package name of the class
+	 * 
+	 * @return the package as string
+	 */
+	public String getPackageName() {
+		return packageName;
+	}
+	
+	/**
+	 * Return the simple class name
+	 * 
+	 * @return the class name, without the package
+	 */
+	public String getClassName() {
+		return className;
+	}
+
+	/**
+	 * Return the complete classname in the form package.classname
+	 */
+	@Override
+	public String toString() {
+		return packageName + "." + className;
+	}
+}
