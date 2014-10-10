59efbf52e29ac4a9de36b3eae5eed3acd9bcb806
chicuslavic <chicuslavic@tibco.com>
2014-10-10 13:11:18 +0000

    refactored Display.getDefault method
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@4689 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/editor/ReportUnitEditor.java
+++ com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/editor/ReportUnitEditor.java
@@ -26,7 +26,6 @@ import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.ToolBar;
 import org.eclipse.swt.widgets.ToolItem;
 import org.eclipse.ui.IEditorInput;
