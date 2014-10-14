e600dfb8893e1cda46bd52afa063e99096bd62af
chicuslavic <chicuslavic@tibco.com>
2014-10-14 08:00:34 +0000

    report unit options
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@4704 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/editor/ReportUnitViewsFactory.java
+++ com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/editor/ReportUnitViewsFactory.java
@@ -31,7 +31,7 @@ import com.jaspersoft.studio.editor.preview.view.report.system.XlsViewer;
 import com.jaspersoft.studio.editor.preview.view.report.system.XlsxViewer;
 
 public class ReportUnitViewsFactory extends AViewsFactory {
-	public static final String DEFAULT = Argument.RUN_OUTPUT_FORMAT_JRPRINT;
+	public static final String DEFAULT = Argument.RUN_OUTPUT_FORMAT_HTML;
 
 	private static LinkedHashMap<String, Class<? extends APreview>> pcmap = new LinkedHashMap<String, Class<? extends APreview>>();
 	static {
