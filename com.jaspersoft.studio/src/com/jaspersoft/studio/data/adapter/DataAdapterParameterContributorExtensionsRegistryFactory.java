88045a3e02dac44c8429aa2ef1b172cd4113bac4
mrabbi <mrabbi@tibco.com>
2014-12-03 11:40:37 +0000

    Fix for Community Bug #3911.
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@5026 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/data/adapter/DataAdapterParameterContributorExtensionsRegistryFactory.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/data/adapter/DataAdapterParameterContributorExtensionsRegistryFactory.java
@@ -12,21 +12,31 @@
  ******************************************************************************/
 package com.jaspersoft.studio.data.adapter;
 
+import net.sf.jasperreports.data.BuiltinDataFileServiceFactory;
+import net.sf.jasperreports.data.DataFileServiceFactory;
 import net.sf.jasperreports.engine.JRPropertiesMap;
 import net.sf.jasperreports.engine.ParameterContributorFactory;
 import net.sf.jasperreports.extensions.ExtensionsRegistry;
 import net.sf.jasperreports.extensions.ExtensionsRegistryFactory;
-import net.sf.jasperreports.extensions.SingletonExtensionRegistry;
+import net.sf.jasperreports.extensions.ListExtensionsRegistry;
 
 /**
  * @author Teodor Danciu (teodord@users.sourceforge.net)
  * @version $Id: DataAdapterParameterContributorExtensionsRegistryFactory.java 4595 2011-09-08 15:55:10Z teodord $
  */
 public class DataAdapterParameterContributorExtensionsRegistryFactory implements ExtensionsRegistryFactory {
-	private static final ExtensionsRegistry extensionsRegistry = new SingletonExtensionRegistry<ParameterContributorFactory>(
-			ParameterContributorFactory.class, DataAdapterParameterContributorFactory.getInstance());
-
-	public ExtensionsRegistry createRegistry(String registryId, JRPropertiesMap properties) {
+	private static final ExtensionsRegistry extensionsRegistry;
+	
+	static
+	{
+		ListExtensionsRegistry registry = new ListExtensionsRegistry();
+		registry.add(ParameterContributorFactory.class, DataAdapterParameterContributorFactory.getInstance());
+		registry.add(DataFileServiceFactory.class, BuiltinDataFileServiceFactory.instance());
+		extensionsRegistry = registry;
+	}
+	
+	public ExtensionsRegistry createRegistry(String registryId, JRPropertiesMap properties) 
+	{
 		return extensionsRegistry;
 	}
 }
