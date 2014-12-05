7428537d0645785e62dec9e701903d385cf5db83
chicuslavic <chicuslavic@tibco.com>
2014-12-05 15:37:29 +0000

    improved hbase adapter for compatibility with JRS
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@5039 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.data.hbase/src/com/jaspersoft/hadoop/hbase/query/HBaseDataAdapterServiceFactoryImpl.java
+++ com.jaspersoft.studio.data.hbase/src/com/jaspersoft/hadoop/hbase/query/HBaseDataAdapterServiceFactoryImpl.java
@@ -10,7 +10,7 @@
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
  ******************************************************************************/
-package com.jaspersoft.studio.data.hbase;
+package com.jaspersoft.hadoop.hbase.query;
 
 import net.sf.jasperreports.data.DataAdapter;
 import net.sf.jasperreports.data.DataAdapterService;
--- com.jaspersoft.studio.data.hbase/src/com/jaspersoft/hadoop/hbase/query/HBaseExtensionsRegistryFactory.java
+++ com.jaspersoft.studio.data.hbase/src/com/jaspersoft/hadoop/hbase/query/HBaseExtensionsRegistryFactory.java
@@ -12,47 +12,27 @@
  ******************************************************************************/
 package com.jaspersoft.hadoop.hbase.query;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import net.sf.jasperreports.data.DataAdapterServiceFactory;
 import net.sf.jasperreports.engine.JRPropertiesMap;
-import net.sf.jasperreports.engine.query.JRQueryExecuterFactoryBundle;
 import net.sf.jasperreports.extensions.ExtensionsRegistry;
 import net.sf.jasperreports.extensions.ExtensionsRegistryFactory;
-
-import com.jaspersoft.studio.data.hbase.HBaseDataAdapterServiceFactoryImpl;
+import net.sf.jasperreports.extensions.SingletonExtensionRegistry;
+ 
 
 /**
  * 
  * @author Eric Diaz
  * 
  */
-public class HBaseExtensionsRegistryFactory implements ExtensionsRegistryFactory {
-	private static List<DataAdapterServiceFactory> dataAdapterServiceFactories;
-	private static List<JRQueryExecuterFactoryBundle> queryExecuterFactories;
+public class HBaseExtensionsRegistryFactory implements
+		ExtensionsRegistryFactory {
 
-	private static final ExtensionsRegistry extensionsRegistry = new ExtensionsRegistry() {
-		@SuppressWarnings("unchecked")
-		public <T> List<T> getExtensions(Class<T> extensionType) {
-			if (DataAdapterServiceFactory.class.equals(extensionType)) {
-				if (dataAdapterServiceFactories == null) {
-					dataAdapterServiceFactories = new ArrayList<DataAdapterServiceFactory>();
-					dataAdapterServiceFactories.add(HBaseDataAdapterServiceFactoryImpl.getInstance());
-				}
-				return (List<T>) dataAdapterServiceFactories;
-			} else if (JRQueryExecuterFactoryBundle.class.equals(extensionType)) {
-				if (queryExecuterFactories == null) {
-					queryExecuterFactories = new ArrayList<JRQueryExecuterFactoryBundle>();
-					queryExecuterFactories.add(HBaseQueryExecuterFactoryBundle.getInstance());
-				}
-				return (List<T>) queryExecuterFactories;
-			}
-			return null;
-		}
-	};
+	private static final ExtensionsRegistry extensionsRegistry = new SingletonExtensionRegistry<DataAdapterServiceFactory>(
+			DataAdapterServiceFactory.class,
+			HBaseDataAdapterServiceFactoryImpl.getInstance());
 
-	public ExtensionsRegistry createRegistry(String registryId, JRPropertiesMap properties) {
+	public ExtensionsRegistry createRegistry(String registryId,
+			JRPropertiesMap properties) {
 		return extensionsRegistry;
 	}
 }
