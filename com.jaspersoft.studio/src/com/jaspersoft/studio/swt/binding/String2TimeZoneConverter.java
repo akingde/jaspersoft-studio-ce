49ff445c1c84d9541582c6ea1df6043f74efcb40
chicuslavic <chicuslavic@tibco.com>
2014-12-10 14:36:42 +0000

    fixed a classcastexception
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@5070 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/swt/binding/String2TimeZoneConverter.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/swt/binding/String2TimeZoneConverter.java
@@ -1,14 +1,10 @@
 /*******************************************************************************
- * Copyright (C) 2005 - 2014 TIBCO Software Inc. All rights reserved.
- * http://www.jaspersoft.com.
+ * Copyright (C) 2005 - 2014 TIBCO Software Inc. All rights reserved. http://www.jaspersoft.com.
  * 
- * Unless you have purchased  a commercial license agreement from Jaspersoft,
- * the following license terms  apply:
+ * Unless you have purchased a commercial license agreement from Jaspersoft, the following license terms apply:
  * 
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
  ******************************************************************************/
 package com.jaspersoft.studio.swt.binding;
 
@@ -22,9 +18,9 @@ public class String2TimeZoneConverter extends Converter {
 	}
 
 	public Object convert(Object source) {
-		if (source != null)
-			return ((TimeZone) source).getDisplayName();
-		return ""; //$NON-NLS-1$
+		if (source instanceof String)
+			return TimeZone.getTimeZone((String) source);
+		return null;
 	}
 
 }
