5977235cf1f02410163f439c63842d33fb8f1954
chicuslavic <chicuslavic@tibco.com>
2015-02-11 15:52:34 +0000

    fixed a npe
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@5259 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/dnd/NodeTransfer.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/dnd/NodeTransfer.java
@@ -1,14 +1,10 @@
 /*******************************************************************************
- * Copyright (C) 2005 - 2014 TIBCO Software Inc. All rights reserved.
- * http://www.jaspersoft.com.
+ * Copyright (C) 2005 - 2014 TIBCO Software Inc. All rights reserved. http://www.jaspersoft.com.
  * 
- * Unless you have purchased  a commercial license agreement from Jaspersoft,
- * the following license terms  apply:
+ * Unless you have purchased a commercial license agreement from Jaspersoft, the following license terms apply:
  * 
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
  ******************************************************************************/
 package com.jaspersoft.studio.dnd;
 
@@ -42,6 +38,8 @@ public class NodeTransfer extends ByteArrayTransfer {
 
 	protected ANode[] fromByteArray(byte[] bytes) {
 		try {
+			if (bytes == null)
+				return null;
 			ObjectInputStream in = new ObjectInputStream(new ByteArrayInputStream(bytes));
 
 			/* read number of gadgets */
