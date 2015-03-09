9b783948368ab8f63be4eedbf8e31b4b75c52a97
morlandin <morlandin@tibco.com>
2015-03-09 15:11:25 +0000

    fixed [#4159] - JSS copying format wrongly copies default attr. instead of element attr.
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@5372 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/editor/action/copy/PasteFormatCommand.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/editor/action/copy/PasteFormatCommand.java
@@ -14,10 +14,16 @@ package com.jaspersoft.studio.editor.action.copy;
 
 import java.util.List;
 
+import net.sf.jasperreports.engine.base.JRBasePen;
+
 import org.eclipse.gef.commands.Command;
 
 import com.jaspersoft.studio.JSSCompoundCommand;
 import com.jaspersoft.studio.model.APropertyNode;
+import com.jaspersoft.studio.model.MGraphicElementLineBox;
+import com.jaspersoft.studio.model.MGraphicElementLinePen;
+import com.jaspersoft.studio.model.MLineBox;
+import com.jaspersoft.studio.model.MLinePen;
 import com.jaspersoft.studio.property.SetValueCommand;
 
 public class PasteFormatCommand extends Command {
@@ -62,13 +68,48 @@ public class PasteFormatCommand extends Command {
 	 */
 	private void generateCommandsForNode(APropertyNode node, JSSCompoundCommand commands){
 		for(String propString : CopyFormatAction.propertyNames){
-			Object copiedValue = CopyFormatAction.getCopiedValues(propString);
+			Object copiedValue = CopyFormatAction.getCopiedValue(propString);
 			SetValueCommand setCommand = new SetValueCommand();
 			setCommand.setTarget(node);
 			setCommand.setPropertyId(propString);
 			setCommand.setPropertyValue(copiedValue);
 			commands.add(setCommand);
+			if (node instanceof MGraphicElementLinePen){
+				MGraphicElementLinePen penNode = (MGraphicElementLinePen) node;
+				MLinePen linePen = penNode.getLinePen();
+				generateCommandsForLinePen(MGraphicElementLinePen.LINE_PEN, linePen, commands);
+			}
+			if (node instanceof MGraphicElementLineBox){
+				MGraphicElementLineBox boxNode = (MGraphicElementLineBox) node;
+				MLineBox lineBox = boxNode.getLineBox();
+				generateCommandsForLinePen(MLineBox.LINE_PEN, (MLinePen)lineBox.getPropertyValue(MLineBox.LINE_PEN), commands);
+				generateCommandsForLinePen(MLineBox.LINE_PEN_BOTTOM, (MLinePen)lineBox.getPropertyValue(MLineBox.LINE_PEN_BOTTOM), commands);
+				generateCommandsForLinePen(MLineBox.LINE_PEN_LEFT, (MLinePen)lineBox.getPropertyValue(MLineBox.LINE_PEN_LEFT), commands);
+				generateCommandsForLinePen(MLineBox.LINE_PEN_RIGHT, (MLinePen)lineBox.getPropertyValue(MLineBox.LINE_PEN_RIGHT), commands);
+				generateCommandsForLinePen(MLineBox.LINE_PEN_TOP, (MLinePen)lineBox.getPropertyValue(MLineBox.LINE_PEN_TOP), commands);
+			}
 		}
+	}
+	
+	/**
+	 * Generate the commands to paste the attributed on a MLinePen
+	 */
+	private void generateCommandsForLinePen(String prefix, MLinePen node, JSSCompoundCommand commands){
+		SetValueCommand setCommand = new SetValueCommand();
+		setCommand.setTarget(node);
+		setCommand.setPropertyId(JRBasePen.PROPERTY_LINE_COLOR);
+		setCommand.setPropertyValue(CopyFormatAction.getCopiedValue(prefix+JRBasePen.PROPERTY_LINE_COLOR));
+		commands.add(setCommand);
+		setCommand = new SetValueCommand();
+		setCommand.setTarget(node);
+		setCommand.setPropertyId(JRBasePen.PROPERTY_LINE_WIDTH);
+		setCommand.setPropertyValue(CopyFormatAction.getCopiedValue(prefix+JRBasePen.PROPERTY_LINE_WIDTH));
+		commands.add(setCommand);
+		setCommand = new SetValueCommand();
+		setCommand.setTarget(node);
+		setCommand.setPropertyId(JRBasePen.PROPERTY_LINE_STYLE);
+		setCommand.setPropertyValue(CopyFormatAction.getCopiedValue(prefix+JRBasePen.PROPERTY_LINE_STYLE));
+		commands.add(setCommand);
 	}
 
 	@Override
