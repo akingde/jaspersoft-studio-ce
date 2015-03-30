0ed0514e131511877ab42b4e1ba9613241bb57d8
morlandin <morlandin@tibco.com>
2015-03-30 09:10:31 +0000

    fixed commit error
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@5449 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.rcp/src/com/jaspersoft/studio/rcp/Startup.java
+++ com.jaspersoft.studio.rcp/src/com/jaspersoft/studio/rcp/Startup.java
@@ -12,15 +12,9 @@
  ******************************************************************************/
 package com.jaspersoft.studio.rcp;
 
-import java.util.List;
-
 import net.sf.jasperreports.eclipse.util.FileUtils;
 import net.sf.jasperreports.eclipse.wizard.project.ProjectUtil;
 
-import org.eclipse.core.commands.ExecutionEvent;
-import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.commands.IExecutionListener;
-import org.eclipse.core.commands.NotHandledException;
 import org.eclipse.core.resources.IProject;
 import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.ResourcesPlugin;
@@ -30,53 +24,14 @@ import org.eclipse.core.runtime.NullProgressMonitor;
 import org.eclipse.ui.IEditorRegistry;
 import org.eclipse.ui.IStartup;
 import org.eclipse.ui.PlatformUI;
-import org.eclipse.ui.commands.ICommandService;
 
 import com.jaspersoft.studio.JaspersoftStudioPlugin;
-import com.jaspersoft.studio.editor.JrxmlEditor;
 import com.jaspersoft.studio.rcp.messages.Messages;
-import com.jaspersoft.studio.utils.SelectionHelper;
-import com.jaspersoft.studio.utils.jasper.JasperReportsConfiguration;
 
 /**
  *
  */
 public class Startup implements IStartup {
-
-	private void testRefreshCommand() {
-		ICommandService service = (ICommandService) PlatformUI.getWorkbench().getService(ICommandService.class);
-		service.addExecutionListener(new IExecutionListener() {
-
-			@Override
-			public void postExecuteSuccess(String commandId, Object returnValue) {
-				if (org.eclipse.ui.IWorkbenchCommandConstants.FILE_REFRESH.equals(commandId)) {
-					List<JrxmlEditor> editors = SelectionHelper.getOpenedEditors();
-					for(JrxmlEditor editor : editors){
-						JasperReportsConfiguration jConfig = editor.getJrContext(null);
-						if (jConfig != null) jConfig.refreshClasspath();
-					}
-				}
-			}
-
-			@Override
-			public void preExecute(String commandId, final ExecutionEvent event) {
-
-			}
-
-			@Override
-			public void notHandled(String commandId,
-					NotHandledException exception) {
-				
-			}
-
-			@Override
-			public void postExecuteFailure(String commandId,
-					ExecutionException exception) {
-				
-			}
-
-		});
-	}
 
 	public void earlyStartup() {
 		IProject project = ResourcesPlugin.getWorkspace().getRoot()
@@ -95,7 +50,6 @@ public class Startup implements IStartup {
 			JaspersoftStudioPlugin.getInstance().getPreferenceStore().setValue(FileUtils.DEFAULT_PROJECT_PROPERTY, true);
 			IEditorRegistry registry = PlatformUI.getWorkbench().getEditorRegistry();
 			registry.setDefaultEditor("*.properties", "com.essiembre.rbe.eclipse.editor.ResourceBundleEditor");
-			testRefreshCommand();
 		} catch (CoreException e) {
 			e.printStackTrace();
 		} finally {
