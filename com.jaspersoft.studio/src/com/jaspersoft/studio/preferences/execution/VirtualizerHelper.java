919886e99df6f7ee54f2f2d91a341f5a24351994
chicuslavic <chicuslavic@tibco.com>
2015-04-24 18:13:08 +0000

    [#4399] - Poor performance when Data about millions
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@5615 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/preferences/execution/VirtualizerHelper.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/preferences/execution/VirtualizerHelper.java
@@ -1,14 +1,10 @@
 /*******************************************************************************
- * Copyright (C) 2005 - 2014 TIBCO Software Inc. All rights reserved.
- * http://www.jaspersoft.com.
+ * Copyright (C) 2005 - 2014 TIBCO Software Inc. All rights reserved. http://www.jaspersoft.com.
  * 
- * Unless you have purchased  a commercial license agreement from Jaspersoft,
- * the following license terms  apply:
+ * Unless you have purchased a commercial license agreement from Jaspersoft, the following license terms apply:
  * 
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
  ******************************************************************************/
 package com.jaspersoft.studio.preferences.execution;
 
@@ -36,32 +32,30 @@ public class VirtualizerHelper {
 
 	private static JRVirtualizer createVirtualizer(JasperReportsConfiguration jContext) {
 		JRVirtualizer v = null;
-		String vtype = jContext.getProperty(ReportExecutionPreferencePage.JSS_VIRTUALIZER_TYPE);
+		String vtype = jContext.getProperty(ReportExecutionPreferencePage.JSS_VIRTUALIZER_TYPE,
+				JRFileVirtualizer.class.getName());
 		int maxSize = jContext.getPropertyInteger(ReportExecutionPreferencePage.JSS_VIRTUALIZER_MAX_SIZE, 100);
 
-		if (vtype != null) {
-			if (vtype.equals(JRFileVirtualizer.class.getName())) {
-				v = new JRFileVirtualizer(maxSize);
-			} else if (vtype.equals(JRGzipVirtualizer.class.getName())) {
-				v = new JRGzipVirtualizer(maxSize);
-			} else if (vtype.equals(JRSwapFileVirtualizer.class.getName())) {
-				int blockSize = jContext.getPropertyInteger(ReportExecutionPreferencePage.JSS_VIRTUALIZER_BLOCK_SIZE, 100);
-				int minGrowCount = jContext.getPropertyInteger(ReportExecutionPreferencePage.JSS_VIRTUALIZER_MIN_GROW_COUNT,
-						100);
-				String directory = jContext.getProperty(ReportExecutionPreferencePage.JSS_VIRTUALIZER_TMP);
-				if (directory != null && !directory.trim().equals("")) {
-					// check if exists
-					File f = new File(directory);
-					if (!f.exists())
-						directory = null;
-				}
-				if (directory == null || directory.trim().equals("")) {
-					IWorkspace workspace = ResourcesPlugin.getWorkspace();
-					directory = workspace.getRoot().getLocation().toOSString();
-				}
-
-				v = new JRSwapFileVirtualizer(maxSize, new JRSwapFile(directory, blockSize, minGrowCount), false);
+		if (vtype.equals(JRFileVirtualizer.class.getName())) {
+			v = new JRFileVirtualizer(maxSize);
+		} else if (vtype.equals(JRGzipVirtualizer.class.getName())) {
+			v = new JRGzipVirtualizer(maxSize);
+		} else if (vtype.equals(JRSwapFileVirtualizer.class.getName())) {
+			int blockSize = jContext.getPropertyInteger(ReportExecutionPreferencePage.JSS_VIRTUALIZER_BLOCK_SIZE, 100);
+			int minGrowCount = jContext.getPropertyInteger(ReportExecutionPreferencePage.JSS_VIRTUALIZER_MIN_GROW_COUNT, 100);
+			String directory = jContext.getProperty(ReportExecutionPreferencePage.JSS_VIRTUALIZER_TMP);
+			if (directory != null && !directory.trim().equals("")) {
+				// check if exists
+				File f = new File(directory);
+				if (!f.exists())
+					directory = null;
 			}
+			if (directory == null || directory.trim().equals("")) {
+				IWorkspace workspace = ResourcesPlugin.getWorkspace();
+				directory = workspace.getRoot().getLocation().toOSString();
+			}
+
+			v = new JRSwapFileVirtualizer(maxSize, new JRSwapFile(directory, blockSize, minGrowCount), false);
 		}
 		return v;
 	}
