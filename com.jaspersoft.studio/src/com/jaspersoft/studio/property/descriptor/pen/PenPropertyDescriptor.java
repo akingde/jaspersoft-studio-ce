59e9753d602642fff9fae0c65d7e193a4126e066
chicuslavic <chicuslavic@tibco.com>
2015-04-29 14:21:53 +0000

    Bug 41977 - JSS - properties - incorrect behavior of Line Pen and properties list
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@5635 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/property/descriptor/pen/PenPropertyDescriptor.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/property/descriptor/pen/PenPropertyDescriptor.java
@@ -1,14 +1,10 @@
 /*******************************************************************************
- * Copyright (C) 2005 - 2014 TIBCO Software Inc. All rights reserved.
- * http://www.jaspersoft.com.
+ * Copyright (C) 2005 - 2014 TIBCO Software Inc. All rights reserved. http://www.jaspersoft.com.
  * 
- * Unless you have purchased  a commercial license agreement from Jaspersoft,
- * the following license terms  apply:
+ * Unless you have purchased a commercial license agreement from Jaspersoft, the following license terms apply:
  * 
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
  ******************************************************************************/
 package com.jaspersoft.studio.property.descriptor.pen;
 
@@ -25,8 +21,9 @@ public class PenPropertyDescriptor extends NTextPropertyDescriptor {
 	}
 
 	public CellEditor createPropertyEditor(Composite parent) {
-		CellEditor editor = new PenCellEditor(parent);
-		return editor;
+		// CellEditor editor = new PenCellEditor(parent);
+		// return editor;
+		return null;
 	}
 
 	@Override
