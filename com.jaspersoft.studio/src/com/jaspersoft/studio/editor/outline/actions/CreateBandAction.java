8ff8dc9d620152a3f652f57172e75d5634678522
chicuslavic <chicuslavic@tibco.com>
2015-09-09 15:25:33 +0000

    Bug 43494 - JSS - Outline - unnecessary additional option "Add Band" for Detail band
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@6033 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/editor/outline/actions/CreateBandAction.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/editor/outline/actions/CreateBandAction.java
@@ -1,16 +1,16 @@
 /*******************************************************************************
- * Copyright (C) 2005 - 2014 TIBCO Software Inc. All rights reserved.
- * http://www.jaspersoft.com.
+ * Copyright (C) 2005 - 2014 TIBCO Software Inc. All rights reserved. http://www.jaspersoft.com.
  * 
- * Unless you have purchased  a commercial license agreement from Jaspersoft,
- * the following license terms  apply:
+ * Unless you have purchased a commercial license agreement from Jaspersoft, the following license terms apply:
  * 
- * This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
  ******************************************************************************/
 package com.jaspersoft.studio.editor.outline.actions;
+
+import java.util.List;
+
+import net.sf.jasperreports.engine.type.BandTypeEnum;
 
 import org.eclipse.gef.EditPart;
 import org.eclipse.jface.viewers.ISelection;
@@ -45,10 +45,18 @@ public class CreateBandAction extends ACreateAndSelectAction {
 
 	@Override
 	protected boolean calculateEnabled() {
-		if(!checkAllSelectedObjects(MBand.class)){
+		if (!checkAllSelectedObjects(MBand.class)) {
 			return false;
-		}
-		else {
+		} else {
+			List<Object> elements = editor.getSelectionCache().getSelectionModelForType(MBand.class);
+			for (Object obj : elements) {
+				if (!(obj instanceof MBand))
+					return false;
+				if (((MBand) obj).getValue() != null)
+					return false;
+				if (((MBand) obj).getBandType() == BandTypeEnum.DETAIL)
+					return false;
+			}
 			command = createCommand();
 			return command != null && command.canExecute();
 		}
