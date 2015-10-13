f794eb0789ee88e21989d8d3477845c5b5a2ef8b
morlandin <morlandin@tibco.com>
2015-10-13 12:48:54 +0000

    added utility method to convert a AWT image into an SWT one
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@6146 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/utils/ImageUtils.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/utils/ImageUtils.java
@@ -12,6 +12,10 @@
  ******************************************************************************/
 package com.jaspersoft.studio.utils;
 
+import java.awt.image.BufferedImage;
+import java.awt.image.DirectColorModel;
+import java.awt.image.IndexColorModel;
+import java.awt.image.WritableRaster;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,6 +24,7 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.graphics.ImageData;
+import org.eclipse.swt.graphics.PaletteData;
 import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.wb.swt.ResourceManager;
@@ -198,4 +203,57 @@ public class ImageUtils {
 	public static List<String> getAllowedImageFileExtensions() {
 		return IMG_FILE_EXTENSIONS;
 	}
+	
+
+	 /**
+	  * Convert an AWT BufferedImage to an swt imagedata
+	  * 
+	  * @param bufferedImage the awt buffered image
+	  * @return the converted image data
+	  */
+	public static ImageData convertToSWT(BufferedImage bufferedImage) {
+	  	if (bufferedImage.getColorModel() instanceof DirectColorModel) {
+	  		DirectColorModel colorModel = (DirectColorModel)bufferedImage.getColorModel();
+	  		PaletteData palette = new PaletteData(colorModel.getRedMask(), colorModel.getGreenMask(), colorModel.getBlueMask());
+	  		ImageData data = new ImageData(bufferedImage.getWidth(), bufferedImage.getHeight(), colorModel.getPixelSize(), palette);
+	  		for (int y = 0; y < data.height; y++) {
+	  			for (int x = 0; x < data.width; x++) {
+	  				int rgb = bufferedImage.getRGB(x, y);
+	  				int pixel = palette.getPixel(new RGB((rgb >> 16) & 0xFF, (rgb >> 8) & 0xFF, rgb & 0xFF)); 
+	  				data.setPixel(x, y, pixel);
+	  				if (colorModel.hasAlpha()) {
+	  					data.setAlpha(x, y, (rgb >> 24) & 0xFF);
+	  				}
+	  			}
+	  		}
+	  		return data;		
+	  	} else if (bufferedImage.getColorModel() instanceof IndexColorModel) {
+	  		IndexColorModel colorModel = (IndexColorModel)bufferedImage.getColorModel();
+	  		int size = colorModel.getMapSize();
+	  		byte[] reds = new byte[size];
+	  		byte[] greens = new byte[size];
+	  		byte[] blues = new byte[size];
+	  		colorModel.getReds(reds);
+	  		colorModel.getGreens(greens);
+	  		colorModel.getBlues(blues);
+	  		RGB[] rgbs = new RGB[size];
+	  		for (int i = 0; i < rgbs.length; i++) {
+	  			rgbs[i] = new RGB(reds[i] & 0xFF, greens[i] & 0xFF, blues[i] & 0xFF);
+	  		}
+	  		PaletteData palette = new PaletteData(rgbs);
+	  		ImageData data = new ImageData(bufferedImage.getWidth(), bufferedImage.getHeight(), colorModel.getPixelSize(), palette);
+	  		data.transparentPixel = colorModel.getTransparentPixel();
+	  		WritableRaster raster = bufferedImage.getRaster();
+	  		int[] pixelArray = new int[1];
+	  		for (int y = 0; y < data.height; y++) {
+	  			for (int x = 0; x < data.width; x++) {
+	  				raster.getPixel(x, y, pixelArray);
+	  				data.setPixel(x, y, pixelArray[0]);
+	  			}
+	  		}
+	  		return data;
+	  	}
+	  	return null;
+	  }
+
 }
