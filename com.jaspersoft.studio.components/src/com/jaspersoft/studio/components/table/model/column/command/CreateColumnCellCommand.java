a483dec92a94f3c7a99d9848fd12e46c2deb961e
morlandin <morlandin@tibco.com>
2015-10-20 08:55:04 +0000

    fixed Bug 44071 - JSS - Table Designer - Styles for new cells are not added automatically
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@6193 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/table/model/column/command/CreateColumnCellCommand.java
+++ com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/table/model/column/command/CreateColumnCellCommand.java
@@ -12,9 +12,15 @@
  ******************************************************************************/
 package com.jaspersoft.studio.components.table.model.column.command;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import net.sf.jasperreports.components.table.Cell;
 import net.sf.jasperreports.components.table.DesignCell;
 import net.sf.jasperreports.components.table.StandardBaseColumn;
+import net.sf.jasperreports.engine.JRStyle;
+import net.sf.jasperreports.engine.design.JasperDesign;
 
 import org.eclipse.gef.commands.Command;
 
@@ -25,7 +31,11 @@ import com.jaspersoft.studio.components.table.model.MTableGroupFooter;
 import com.jaspersoft.studio.components.table.model.MTableGroupHeader;
 import com.jaspersoft.studio.components.table.model.MTableHeader;
 import com.jaspersoft.studio.components.table.model.column.MColumn;
+import com.jaspersoft.studio.components.table.model.columngroup.MColumnGroup;
+import com.jaspersoft.studio.components.table.model.columngroup.MColumnGroupCell;
 import com.jaspersoft.studio.model.ANode;
+import com.jaspersoft.studio.model.APropertyNode;
+import com.jaspersoft.studio.model.INode;
 
 /*
  * link nodes & together.
@@ -39,6 +49,7 @@ public class CreateColumnCellCommand extends Command {
 	private String groupName;
 	private Cell jrCell;
 	private int height = 0;
+	private ANode column;
 
 	@SuppressWarnings("unchecked")
 	public CreateColumnCellCommand(ANode parent, MColumn srcNode) {
@@ -51,9 +62,86 @@ public class CreateColumnCellCommand extends Command {
 			groupName = ((MTableGroupFooter) parent).getJrDesignGroup()
 					.getName();
 		this.jrColumn = (StandardBaseColumn) srcNode.getValue();
+		this.column = srcNode;
 		height = srcNode.getBounds().height;
 		//don't create cell with zero height to provide a feedback on the graphical editor
 		if (height == 0) height = 30;
+	}
+	
+	/**
+	 * Search the style on the sibling of the current column and if found return it. If the sibling 
+	 * is a group then the column is searched inside. If the style can't be found in the current sibling
+	 * and the column is inside a group then search in the sibling of the group
+	 * 
+	 * @return the name of the style if found, otherwise null
+	 */
+	protected String getSiblingStyle(ANode parent, ANode column){
+		int index = parent.getChildren().indexOf(column);
+		int leftIndex = index - 1;
+		int rightIndex = index + 1;
+		int size = parent.getChildren().size();
+		while((leftIndex >= 0 || rightIndex < size)){
+			if (leftIndex >= 0){
+				INode leftNode = parent.getChildren().get(leftIndex);
+				String style = null;
+				if (leftNode instanceof MColumnGroup || leftNode instanceof MColumnGroupCell){
+					style = getStyleFromColumnGroup((MColumn)leftNode, true);
+				} else if (leftNode instanceof MColumn){
+					style = (String)((APropertyNode)leftNode).getPropertyValue(DesignCell.PROPERTY_STYLE);
+				}
+				if (style != null) return style;
+				else {
+					leftIndex --;
+				}
+			}
+			if (rightIndex < size){
+				INode rightNode = parent.getChildren().get(rightIndex);
+				String style = null;
+				if (rightNode instanceof MColumnGroup || rightNode instanceof MColumnGroupCell){
+					style = getStyleFromColumnGroup((MColumnGroup)rightNode, false);
+				} else if (rightNode instanceof MColumn){
+					style = (String)((APropertyNode)rightNode).getPropertyValue(DesignCell.PROPERTY_STYLE);
+				}
+				if (style != null) return style;
+				else {
+					rightIndex ++;
+				}
+			}
+		}
+		//style not found, if the current column is inside a group search in the sibling of the group
+		if (parent instanceof MColumnGroup || parent instanceof MColumnGroupCell){
+			String style = getSiblingStyle(parent.getParent(), parent);
+			if (style != null){
+				return style;
+			}
+		}
+		return null;
+	}
+	
+	
+	/**
+	 * Search for a style inside a group recursively
+	 * 
+	 * @param group the group where to search
+	 * @param startFromRight true if the search start from the list child of the group, 
+	 * false if it start from the first one
+	 * @return the name of a style or null if it can't be found
+	 */
+	protected String getStyleFromColumnGroup(MColumn group, boolean startFromRight){
+		List<INode> children = new ArrayList<INode>(group.getChildren());
+		if (startFromRight){
+			Collections.reverse(children);
+		}
+		for(INode node : group.getChildren()){
+			if (node instanceof MColumnGroup || node instanceof MColumnGroup){
+				String style = getStyleFromColumnGroup((MColumn)node, startFromRight);
+				if (style != null) return style;
+			} else if (node instanceof MColumn){
+				String style = (String)((APropertyNode)node).getPropertyValue(DesignCell.PROPERTY_STYLE);
+				if (style != null) return style;
+			}
+		}
+		return null;
 	}
 
 	/*
@@ -66,8 +154,9 @@ public class CreateColumnCellCommand extends Command {
 		if (jrCell == null) {
 			jrCell = createCell();
 		}
-		if (type.isAssignableFrom(MTableHeader.class))
+		if (type.isAssignableFrom(MTableHeader.class)){
 			jrColumn.setTableHeader(jrCell);
+		}
 		else if (type.isAssignableFrom(MTableFooter.class))
 			jrColumn.setTableFooter(jrCell);
 		else if (type.isAssignableFrom(MTableColumnHeader.class))
@@ -84,6 +173,16 @@ public class CreateColumnCellCommand extends Command {
 	protected Cell createCell() {
 		DesignCell cell = new DesignCell();
 		cell.setHeight(height);
+		String siblingStyle = getSiblingStyle(column.getParent(), column);
+		if (siblingStyle != null){
+			JasperDesign jd = column.getJasperDesign();
+			JRStyle internalStyle = jd.getStylesMap().get(siblingStyle);
+			if (internalStyle != null){
+				cell.setStyle(internalStyle);
+			} else {
+				cell.setStyleNameReference(siblingStyle);
+			}
+		}
 		return cell;
 	}
 
