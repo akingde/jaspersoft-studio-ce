b2cffadd8ef2c0f26883d0ef4747f763910e0c05
morlandin <morlandin@tibco.com>
2015-10-21 11:34:38 +0000

    fixed Bug 44241 - JSS - Canvas - Outline - unable to use main designer after adding Table, select all and delete
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@6210 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/editor/action/CustomDeleteAction.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/editor/action/CustomDeleteAction.java
@@ -12,6 +12,7 @@
  ******************************************************************************/
 package com.jaspersoft.studio.editor.action;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import net.sf.jasperreports.eclipse.ui.util.UIUtils;
@@ -56,9 +57,69 @@ public class CustomDeleteAction extends DeleteAction{
 		return null;
 	}
 	
+	/**
+	 * Check if child is a descendant of a possible parent
+	 * 
+	 * @param child the child to check
+	 * @param possibileParent the parent checked
+	 * @return true is possibleParent is an ancestor of child, false otherwise
+	 */
+	private boolean isPartDescendent(ANode child, ANode possibileParent){
+		ANode parent = child.getParent();
+		while(parent != null){
+			if (parent == possibileParent){
+				return true;
+			}
+			parent = parent.getParent();
+		}
+		return false;
+	}
+	
+	/**
+	 * Check if a single selected part in the list is a descendant of another part 
+	 * placed before it. If it so the second part is always placed immediately before the first
+	 * 
+	 * @param selectedParts the current list of part to delete
+	 * @return true if a part was moved by this method, doesn't assure there aren't other part to move.
+	 * False if now parts were moved so it is already in the correct order
+	 */
+	private boolean reorderPart(ArrayList<EditPart> selectedParts){
+		if (selectedParts.size() <= 1) return false;
+		for(int i = 0; i< selectedParts.size(); i++){
+			for(int j = i+ 1; j<selectedParts.size(); j++){
+				ANode model1 = (ANode)selectedParts.get(i).getModel();
+				ANode model2 = (ANode)selectedParts.get(j).getModel();	
+				if (isPartDescendent(model2, model1)){
+					EditPart part = selectedParts.remove(j);
+					selectedParts.add(i, part);
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+
+	/**
+	 * Get the set of part to create the delete commands.
+	 * The part are reordered to have the child of other elements
+	 * in the selection set delete first their parent
+	 * 
+	 * @return ordered list of part to delete
+	 */
+	protected List<EditPart> getOrderedSelectedObjects() {
+		List<?> selectedObject = super.getSelectedObjects();
+		ArrayList<EditPart> selectedParts = new ArrayList<EditPart>();
+		for(Object obj : selectedObject){
+			selectedParts.add((EditPart)obj);
+		}
+		//Reoder the parts until all the children are before the parent
+		while(reorderPart(selectedParts));
+		return selectedParts;
+	}
+	
 	@Override
 	public void run() {
-		CompoundCommand deleteCommandds = (CompoundCommand)createDeleteCommand(getSelectedObjects());
+		CompoundCommand deleteCommandds = (CompoundCommand)createDeleteCommand(getOrderedSelectedObjects());
 		JSSCompoundCommand compCommand = new JSSCompoundCommand(deleteCommandds, getModel());
 		StringBuilder messages = new StringBuilder();
 		messages.append(Messages.CustomDeleteAction_messageListStart+"\n"); //$NON-NLS-1$
