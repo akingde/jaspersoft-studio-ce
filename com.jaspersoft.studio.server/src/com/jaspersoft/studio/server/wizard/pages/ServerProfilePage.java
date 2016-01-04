a5697033d01d46460aa4e99e0defef88ddc79dc2
chicuslavic <chicuslavic@tibco.com>
2016-01-04 16:22:40 +0000

    Bug 44888 - JSS - repository explorer - exception in error log after Test Connection
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@6464 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/wizard/pages/ServerProfilePage.java
+++ com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/wizard/pages/ServerProfilePage.java
@@ -649,7 +649,9 @@ public class ServerProfilePage extends WizardPage implements WizardEndingStateLi
 			public void run() {
 				if (drvtab != null)
 					drvtab.dispose();
-				sprofile.getWsClient().getServerProfile().setClientUser(null);
+				if (sprofile != null && sprofile.getWsClient() != null
+						&& sprofile.getWsClient().getServerProfile() != null)
+					sprofile.getWsClient().getServerProfile().setClientUser(null);
 			}
 		});
 	}
