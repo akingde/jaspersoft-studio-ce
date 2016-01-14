921724d9abd682598089c40bf03f8355ab3a4861
morlandin <morlandin@tibco.com>
2016-01-14 13:14:20 +0000

    fixed Bug 44954 - [Case #65802] - Add config option to disable 'Studio Usage' popup
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@6481 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/statistics/EnableUsageStatisticsQuestion.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/statistics/EnableUsageStatisticsQuestion.java
@@ -14,11 +14,13 @@ package com.jaspersoft.studio.statistics;
 
 import java.io.File;
 
-import net.sf.jasperreports.eclipse.ui.util.UIUtils;
+import org.eclipse.core.runtime.Platform;
 
 import com.jaspersoft.studio.JaspersoftStudioPlugin;
 import com.jaspersoft.studio.messages.Messages;
 import com.jaspersoft.studio.preferences.StudioPreferencePage;
+
+import net.sf.jasperreports.eclipse.ui.util.UIUtils;
 
 /**
  * Action executed the first time eclipse is started to ask to the user if the
@@ -29,9 +31,36 @@ import com.jaspersoft.studio.preferences.StudioPreferencePage;
  */
 public class EnableUsageStatisticsQuestion implements IFirstStartupAction {
 	
+	/**
+	 * Property that can be used to skip the statistics question
+	 */
+	public final String skipQuestionProperty = "-com.jaspersoft.studio.skipUsageQuestion";
+	
+	/**
+	 * Return the value of the flag to skip the question, if the flag is not found the question
+	 * is not skipped
+	 * 
+	 * @return true if the question to enable statistics should be skipped, false otherwise
+	 */
+	protected boolean getFlagValue(){
+		String[] args = Platform.getApplicationArgs();
+		for(int i=0; i < args.length; i++){
+			String arg = args[i];
+			if (skipQuestionProperty.equals(arg) && i < (args.length - 1)){
+				return Boolean.parseBoolean(args[i + 1]);
+			}
+		}
+		return false;
+	}
+	
 	@Override
 	public void executeFirstStartupAction(File configurationDirectory) {
-		boolean enableUsageStatistic = UIUtils.showConfirmation(Messages.EnableUsageStatisticsQuestion_actionTitle, Messages.EnableUsageStatisticsQuestion_actionMessage);
-		JaspersoftStudioPlugin.getInstance().getPreferenceStore().setValue(StudioPreferencePage.JSS_SEND_USAGE_STATISTICS, enableUsageStatistic);
+		boolean value = getFlagValue();
+		if (!value){
+			boolean enableUsageStatistic = UIUtils.showConfirmation(Messages.EnableUsageStatisticsQuestion_actionTitle, Messages.EnableUsageStatisticsQuestion_actionMessage);
+			JaspersoftStudioPlugin.getInstance().getPreferenceStore().setValue(StudioPreferencePage.JSS_SEND_USAGE_STATISTICS, enableUsageStatistic);
+		} else {
+			JaspersoftStudioPlugin.getInstance().getPreferenceStore().setValue(StudioPreferencePage.JSS_SEND_USAGE_STATISTICS, false);
+		}
 	}
 }
