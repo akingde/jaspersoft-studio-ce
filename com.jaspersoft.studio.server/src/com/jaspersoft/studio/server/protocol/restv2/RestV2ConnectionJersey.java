6d6a85838348b2d6a240a2e7a1d1d11a8faeb086
chicuslavic <chicuslavic@tibco.com>
2016-02-15 09:19:18 +0000

    removed unnecessary files
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@6567 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/protocol/restv2/RestV2ConnectionJersey.java
+++ com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/protocol/restv2/RestV2ConnectionJersey.java
@@ -23,7 +23,6 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.logging.Logger;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
