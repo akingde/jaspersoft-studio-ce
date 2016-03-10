3b8f2758a26d03c52e90f2022f7cb4f60cf4a5bc
morlandin <morlandin@tibco.com>
2016-03-10 11:35:01 +0000

    fixed http://jira.jaspersoft.com/browse/JSS-41
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@6683 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/swt/widgets/ResizableToolItem.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/swt/widgets/ResizableToolItem.java
@@ -1,3 +1,15 @@
+/*******************************************************************************
+ * Copyright (C) 2005 - 2014 TIBCO Software Inc. All rights reserved.
+ * http://www.jaspersoft.com.
+ * 
+ * Unless you have purchased  a commercial license agreement from Jaspersoft,
+ * the following license terms  apply:
+ * 
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
 package com.jaspersoft.studio.swt.widgets;
 
 import java.util.HashMap;
@@ -17,20 +29,61 @@ import org.eclipse.wb.swt.ResourceManager;
 import com.jaspersoft.studio.utils.ImageUtils;
 import com.jaspersoft.studio.utils.ModelUtils;
 
+/**
+ * Extension of the toolitems to allow to set their size. Toolitems has no easy way
+ * to set the size, they inherit it both from the toolbar or the content, taking the maximum.
+ * The biggest toolitem so set the size of the toolbar, all the others adapt to it. On some
+ * SWT implementation this cause a stretch of the image . This new toolitem will allow to 
+ * set their height using a trick to pad the internal image with the background color of the toolbar
+ * 
+ * 
+ * @author Orlandin Marco
+ *
+ */
 public class ResizableToolItem extends ToolItem{
 
+		/**
+		 * Cache map of the padded icons generated. The key is the action id for which they were 
+		 * generated plus the background of the toolbar in the moment they were generated
+		 */
 		private static HashMap<String, ImageData> iconImagesDataMap = new HashMap<String, ImageData>();
 		
+		/**
+		 * Cache map of the swt images created for this toolitem, it is disposed when the toolitem is 
+		 * disposed also
+		 */
 		private ResourceCache imagesResource = new ResourceCache();
 	
+		/**
+		 * The action associated to this toolitem
+		 */
 		private IAction associatedAction;
 		
+		/**
+		 * The height of this toolitem, SWT.DEFAULT is the original height of the image
+		 */
 		private int suggestedHeight = SWT.DEFAULT;
 		
+		/**
+		 * Listener on the tollbar repaint. Used to check if the toolbar has
+		 * changed background and update the images
+		 */
 		private PaintListener paintListener = null;
 		
+		/**
+		 * Last background of the toolbar, used to update the images when the toolbar
+		 * background changes
+		 */
 		private RGB lastParentBackground = null;
 		
+		/**
+		 * Create the toolitem 
+		 * 
+		 * @param parent the parent toolbar
+		 * @param style the style, they are the same of the base ToolItem
+		 * @param associtatedAction the action associated to this toolitem, must be not null
+		 * @param suggestedHeight the height for the icon, SWT.DEFAULT is the original height of the image
+		 */
 		public ResizableToolItem(ToolBar parent, int style, IAction associtatedAction, int suggestedHeight) {
 			super(parent, style);
 			this.associatedAction = associtatedAction;
@@ -42,9 +95,14 @@ public class ResizableToolItem extends ToolItem{
 				};
 			};
 			parent.addPaintListener(paintListener);
+			//Need to set the image on the creation otherwise on some OS it will not able to display any image
 			updateImage();
 		}
 		
+		/**
+		 * Update the image inside the toolitem, but only if the background of the toolbar
+		 * changed since the last time it was updated
+		 */
 		private void updateImage(){
 			RGB currentParentBackground = getParent().getBackground().getRGB();
 			if (!ModelUtils.safeEquals(lastParentBackground, currentParentBackground)){
@@ -63,10 +121,11 @@ public class ResizableToolItem extends ToolItem{
 		
 		/**
 		 * Get the image from the action and if it is tool small return a 
-		 * resized version of that image to a fixed height
+		 * resized version of that image to a fixed height. The returned value
+		 * is an ImageData and first is checked in the cache.
 		 * 
 		 * @param action the action, it must have an image
-		 * @return an image if the one from the icon was too small or the image
+		 * @return an ImageData if the one from the icon was too small or the image
 		 * of the action itself
 		 */
 		private ImageData getResizedImage(IAction action){
