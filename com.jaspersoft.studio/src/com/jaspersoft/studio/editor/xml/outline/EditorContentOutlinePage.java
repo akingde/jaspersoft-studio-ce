5928caf7f4a5769c4798b4b40cd11dd1e4b938cd
morlandin <morlandin@tibco.com>
2016-04-29 09:14:27 +0000

    Added background loading of the outline to avoid application to hang with very big files
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@6813 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/editor/xml/outline/EditorContentOutlinePage.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/editor/xml/outline/EditorContentOutlinePage.java
@@ -12,6 +12,12 @@
  ******************************************************************************/
 package com.jaspersoft.studio.editor.xml.outline;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.jface.text.DefaultPositionUpdater;
+import org.eclipse.jface.text.Document;
+import org.eclipse.jface.text.IDocument;
+import org.eclipse.jface.text.IPositionUpdater;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.SelectionChangedEvent;
@@ -20,17 +26,131 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.ui.texteditor.ITextEditor;
 import org.eclipse.ui.views.contentoutline.ContentOutlinePage;
+import org.xml.sax.helpers.LocatorImpl;
 
 import com.jaspersoft.studio.editor.xml.xml.XMLElement;
+import com.jaspersoft.studio.editor.xml.xml.XMLParser;
+import com.jaspersoft.studio.utils.ModelUtils;
+
+import net.sf.jasperreports.eclipse.ui.util.UIUtils;
 
  
 public class EditorContentOutlinePage extends ContentOutlinePage
 {
+	
+	/**
+	 * This thread parse the XML in background, so it doesn't block
+	 * the UI to desierialize the XML. If at the end the thread is not
+	 * cancelled the method to update the tree is called
+	 * 
+	 * @author Orlandin Marco
+	 *
+	 */
+	private final class XMLLoaderJob implements Runnable {
+		
+		private IProgressMonitor monitor;
+		
+		public XMLLoaderJob() {
+			monitor = new NullProgressMonitor();
+		}
+
+		@Override
+		public void run() {
+    	IDocument document = getDocument(input);
+    	XMLElement newRoot = null;
+			if (document != null) {
+				document.addPositionCategory(TAG_POSITIONS);
+				document.addPositionUpdater(positionUpdater);
+				newRoot = parseRootElement(document);
+			}
+			if (!monitor.isCanceled()) {
+				root = newRoot;
+				setRootIntoTree();
+			}
+    }
+		
+		public void cancel(){
+			monitor.setCanceled(true);
+		}
+		
+		/**
+		 * Parse the current document to create the tree
+		 * 
+		 * @param document the document to parse
+		 * @return the created tree or null if something went wrong during the execution
+		 */
+		private XMLElement parseRootElement(IDocument document) {
+			try {
+				XMLParser xmlParser = new XMLParser();
+				OutlineContentHandler contentHandler = new OutlineContentHandler();
+				contentHandler.setDocument(document);
+				contentHandler.setPositionCategory(TAG_POSITIONS);
+				contentHandler.setDocumentLocator(new LocatorImpl());
+				xmlParser.setContentHandler(contentHandler);
+				xmlParser.doParse(document.get());
+				XMLElement root = contentHandler.getRootElement();
+				return root;
+			} catch (Exception e) {
+				return null;
+			}
+		}
+		
+		/**
+		 * Return the document by the type of the input. The document is
+		 * always a new instance since more thread can work on it
+		 * 
+		 * @param input the input must be a string or an IEditorInput
+		 * @return an IDocument build from the input
+		 */
+		private IDocument getDocument(Object input){
+			if (input == null) return null;
+			if (input instanceof String) return new Document((String)input);
+			else {
+				return getDocument(editor.getDocumentProvider().getDocument(input).get());
+			}
+		}
+	};
+	
+	/**
+	 * Fake input node used to show in the outline a loading message
+	 */
+	private static XMLElement loadingNode;
+	
+	/**
+	 * Fake input node used to show in the outline an error message
+	 */
+	private static XMLElement noInput;
+	
+	static{
+		//initialize the fake input nodes
+		loadingNode = new XMLElement("root");
+		loadingNode.addChildElement(new XMLElement("Loading the outline..."));
+
+		noInput = new XMLElement("root"); 
+		noInput.addChildElement(new XMLElement("No valid input"));
+	}
 
 	private ITextEditor editor;
+	
 	private Object input;
+	
 	private OutlineContentProvider outlineContentProvider;
+	
 	private OutlineLabelProvider outlineLabelProvider;
+
+	protected final static String TAG_POSITIONS = "__tag_positions";
+	
+	protected IPositionUpdater positionUpdater = new DefaultPositionUpdater(TAG_POSITIONS);
+	
+	/**
+	 * Reference to the current running tree
+	 */
+	private XMLLoaderJob xmlLoaderJob = null;
+	
+	/**
+	 * The current input
+	 */
+	private XMLElement root = null;
 
 	public EditorContentOutlinePage(ITextEditor editor)
 	{
@@ -42,15 +162,14 @@ public class EditorContentOutlinePage extends ContentOutlinePage
 	{
 		super.createControl(parent);
 		TreeViewer viewer = getTreeViewer();
-		outlineContentProvider = new OutlineContentProvider(editor.getDocumentProvider());
+		outlineContentProvider = new OutlineContentProvider();
 		viewer.setContentProvider(outlineContentProvider);
 		outlineLabelProvider = new OutlineLabelProvider();
 		viewer.setLabelProvider(outlineLabelProvider);
 		viewer.addSelectionChangedListener(this);
 
 		//control is created after input is set
-		if (input != null)
-			viewer.setInput(input);
+		setRootIntoTree();
 	}
 
 	/**
@@ -58,8 +177,10 @@ public class EditorContentOutlinePage extends ContentOutlinePage
 	 */
 	public void setInput(Object input)
 	{
-		this.input = input;
-		update();
+		if (!ModelUtils.safeEquals(input, this.input)) {
+			this.input = input;
+			update();
+		}
 	}
 
 	/*
@@ -76,41 +197,76 @@ public class EditorContentOutlinePage extends ContentOutlinePage
 		else
 		{
 			XMLElement element = (XMLElement) ((IStructuredSelection) selection).getFirstElement();		
-			
-			int start = element.getPosition().getOffset();
-			int length = element.getPosition().getLength();
-			try
-			{
-				editor.setHighlightRange(start, length, true);
-			}
-			catch (IllegalArgumentException x)
-			{
-				editor.resetHighlightRange();
+			if (element.getPosition() != null){
+				int start = element.getPosition().getOffset();
+				int length = element.getPosition().getLength();
+				try
+				{
+					editor.setHighlightRange(start, length, true);
+				}
+				catch (IllegalArgumentException x)
+				{
+					editor.resetHighlightRange();
+				}
 			}
 		}
 	}
 
+
 	/**
-	 * The editor is saved, so we should refresh representation
+	 * The editor is saved, so we should refresh representation. It will
+	 * start the thread to parse the XML, if there is another one in progress
+	 * it is marked for the cancellation
 	 * 
-	 * @param tableNamePositions
 	 */
 	public void update()
 	{
 		//set the input so that the outlines parse can be called
 		//update the tree viewer state
-		TreeViewer viewer = getTreeViewer();
+		final TreeViewer viewer = getTreeViewer();
 
 		if (viewer != null)
 		{
 			Control control = viewer.getControl();
-			if (control != null && !control.isDisposed())
-			{
-				control.setRedraw(false);
-				viewer.setInput(input);
-				control.setRedraw(true);
+			if (control != null && !control.isDisposed()){
+				//set the informative loading node
+				viewer.setInput(loadingNode);	
+				if (xmlLoaderJob != null){
+					//mark the old thread for the cancellation
+					xmlLoaderJob.cancel();
+					xmlLoaderJob = null;
+				}
+				//start the new thread
+				xmlLoaderJob = new XMLLoaderJob();
+				new Thread(xmlLoaderJob).start();
 			}
 		}
 	}
-
+	
+	/**
+	 * Set the input inside the tree, it is done with a UI thread
+	 * so it is a safe operation
+	 */
+	private synchronized void setRootIntoTree(){
+		UIUtils.getDisplay().syncExec(new Runnable() {
+			
+			@Override
+			public void run() {
+				TreeViewer viewer = getTreeViewer();
+				if (viewer != null){
+					Control control = viewer.getControl();
+					if (control != null && !control.isDisposed())
+					{
+						control.setRedraw(false);
+						if (root == null){
+							viewer.setInput(noInput);
+						} else {
+							viewer.setInput(root);
+						}
+						control.setRedraw(true);
+					}
+				}
+			}
+		});
+	}
 }
--- com.jaspersoft.studio/src/com/jaspersoft/studio/editor/xml/outline/OutlineContentProvider.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/editor/xml/outline/OutlineContentProvider.java
@@ -14,45 +14,22 @@ package com.jaspersoft.studio.editor.xml.outline;
 
 import java.util.List;
 
-import org.eclipse.jface.text.BadPositionCategoryException;
-import org.eclipse.jface.text.DefaultPositionUpdater;
-import org.eclipse.jface.text.Document;
-import org.eclipse.jface.text.IDocument;
-import org.eclipse.jface.text.IPositionUpdater;
 import org.eclipse.jface.viewers.ITreeContentProvider;
 import org.eclipse.jface.viewers.Viewer;
-import org.eclipse.ui.texteditor.IDocumentProvider;
-import org.xml.sax.helpers.LocatorImpl;
 
 import com.jaspersoft.studio.editor.xml.xml.XMLElement;
-import com.jaspersoft.studio.editor.xml.xml.XMLParser;
 
 public class OutlineContentProvider implements ITreeContentProvider {
 
 	private XMLElement root = null;
-	private Object input;
-	private IDocumentProvider documentProvider;
-
-	protected final static String TAG_POSITIONS = "__tag_positions";
-	protected IPositionUpdater positionUpdater = new DefaultPositionUpdater(TAG_POSITIONS);
-
-	public OutlineContentProvider(IDocumentProvider provider) {
-		super();
-		this.documentProvider = provider;
-	}
 
 	public Object[] getChildren(Object parentElement) {
-		if (parentElement == input) {
-			if (root == null)
-				return new Object[0];
-			List<XMLElement> childrenDTDElements = root.getChildrenDTDElements();
-			if (childrenDTDElements != null)
-				return childrenDTDElements.toArray();
-		} else {
-			XMLElement parent = (XMLElement) parentElement;
-			List<XMLElement> childrenDTDElements = parent.getChildrenDTDElements();
-			if (childrenDTDElements != null)
-				return childrenDTDElements.toArray();
+		if (root == null)
+			return new Object[0];
+		XMLElement parent = (XMLElement) parentElement;
+		List<XMLElement> childrenDTDElements = parent.getChildrenDTDElements();
+		if (childrenDTDElements != null){
+			return childrenDTDElements.toArray();
 		}
 		return new Object[0];
 	}
@@ -63,14 +40,12 @@ public class OutlineContentProvider implements ITreeContentProvider {
 		return null;
 	}
 
+	@Override
 	public boolean hasChildren(Object element) {
-		if (element == input)
-			return true;
-		else {
-			return ((XMLElement) element).getChildrenDTDElements().size() > 0;
-		}
+		return ((XMLElement) element).getChildrenDTDElements().size() > 0;
 	}
 
+	@Override
 	public Object[] getElements(Object inputElement) {
 		if (root == null)
 			return new Object[0];
@@ -83,66 +58,7 @@ public class OutlineContentProvider implements ITreeContentProvider {
 	public void dispose() {
 	}
 	
-	/**
-	 * Return the document by the type of the input
-	 * 
-	 * @param input the input must be a string or an IEditorInput
-	 * @return an IDocument build from the input
-	 */
-	private IDocument getDocument(Object input){
-		if (input instanceof String) return new Document((String)input);
-		else return documentProvider.getDocument(input);
-	}
-
 	public void inputChanged(Viewer viewer, Object oldInput, Object newInput) {
-
-		if (oldInput != null) {
-			IDocument document = getDocument(oldInput);
-			if (document != null) {
-				try {
-					document.removePositionCategory(TAG_POSITIONS);
-				} catch (BadPositionCategoryException x) {
-				}
-				document.removePositionUpdater(positionUpdater);
-			}
-		}
-
-		input = newInput;
-
-		if (newInput != null) {
-			IDocument document = getDocument(newInput);
-			if (document != null) {
-				document.addPositionCategory(TAG_POSITIONS);
-				document.addPositionUpdater(positionUpdater);
-
-				XMLElement rootElement = parseRootElement(document);
-				if (rootElement != null) {
-					root = rootElement;
-				}
-			}
-		}
+		root = (XMLElement)newInput;
 	}
-
-	private XMLElement parseRootElement(IDocument document) {
-		String text = document.get();
-		XMLElement tagPositions = parseRootElements(text, document);
-		return tagPositions;
-	}
-
-	private XMLElement parseRootElements(String text, IDocument document) {
-		try {
-			XMLParser xmlParser = new XMLParser();
-			OutlineContentHandler contentHandler = new OutlineContentHandler();
-			contentHandler.setDocument(document);
-			contentHandler.setPositionCategory(TAG_POSITIONS);
-			contentHandler.setDocumentLocator(new LocatorImpl());
-			xmlParser.setContentHandler(contentHandler);
-			xmlParser.doParse(text);
-			XMLElement root = contentHandler.getRootElement();
-			return root;
-		} catch (Exception e) {
-			return null;
-		}
-	}
-
 }
