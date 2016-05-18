dd8d36f844adcba15af3e54397f991f95a114e84
morlandin <morlandin@tibco.com>
2016-05-18 10:07:22 +0000

    fixed graphical elements refresh when a font is removed/edited
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@6887 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/model/MReportRoot.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/model/MReportRoot.java
@@ -17,6 +17,7 @@ import java.beans.PropertyChangeEvent;
 import org.eclipse.jface.util.IPropertyChangeListener;
 
 import com.jaspersoft.studio.JaspersoftStudioPlugin;
+import com.jaspersoft.studio.preferences.fonts.FontsPreferencePage;
 import com.jaspersoft.studio.utils.ModelUtils;
 import com.jaspersoft.studio.utils.jasper.JasperReportsConfiguration;
 
@@ -33,7 +34,8 @@ import net.sf.jasperreports.engine.design.JRDesignElement;
  * will get when a property is changed and fire and event to every element in the report that
  * force it to refresh. It is not necessary to use this on a subeditor node, because the event
  * is fired on every jr elements of the main report, so it will arrive to every node listening that
- * element
+ * element. The refresh is done also when something change in the font storage, since removing/editing
+ * a font could reflect on the graphical elements
  * 
  * @author Orlandin Marco
  *
@@ -48,8 +50,7 @@ public class MReportRoot extends MRoot {
 	private IPropertyChangeListener preferenceListener = new IPropertyChangeListener() {
 		
 		public void propertyChange(org.eclipse.jface.util.PropertyChangeEvent event) {
-				String property = event.getProperty();
-				if (getJasperDesign() != null && property.equals(FilePrefUtil.NET_SF_JASPERREPORTS_JRPROPERTIES)) {	
+				if (getJasperDesign() != null && needRefresh(event)) {	
 					final PropertyChangeEvent changeEvent = new PropertyChangeEvent(getJasperDesign(), MGraphicElement.FORCE_GRAPHICAL_REFRESH, false, true);
 					UIUtils.getDisplay().asyncExec(new Runnable() {
 						
@@ -76,5 +77,17 @@ public class MReportRoot extends MRoot {
 				JaspersoftStudioPlugin.getInstance().removePreferenceListener(preferenceListener);	
 			}
 		});
+	}
+	
+	/**
+	 * Check if the change on the preferences require a graphical refresh of the elements
+	 * 
+	 * @param event the event coming from the preferences
+	 * @return true if the visual elements should be refreshed, false otherwise
+	 */
+	protected boolean needRefresh(org.eclipse.jface.util.PropertyChangeEvent event){
+		String property = event.getProperty();
+		return property.equals(FilePrefUtil.NET_SF_JASPERREPORTS_JRPROPERTIES) || 
+							property.equals(FontsPreferencePage.FPP_FONT_LIST);
 	}
 }
