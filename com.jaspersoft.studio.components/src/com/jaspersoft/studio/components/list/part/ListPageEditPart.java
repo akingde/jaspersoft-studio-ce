1cd6217d6f9f10059fb14e7c4e58b01a454cdb5f
morlandin <morlandin@tibco.com>
2016-05-30 12:58:53 +0000

    Fixed subeditor elements when used inside a list
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@6917 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/list/part/ListPageEditPart.java
+++ com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/list/part/ListPageEditPart.java
@@ -14,13 +14,20 @@ package com.jaspersoft.studio.components.list.part;
 
 import net.sf.jasperreports.engine.design.JRDesignElement;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.draw2d.geometry.Dimension;
 
+import com.jaspersoft.studio.callout.MCallout;
+import com.jaspersoft.studio.callout.pin.MPinConnection;
 import com.jaspersoft.studio.components.list.model.MList;
 import com.jaspersoft.studio.editor.gef.figures.APageFigure;
 import com.jaspersoft.studio.editor.gef.figures.ContainerPageFigure;
 import com.jaspersoft.studio.editor.gef.parts.PageEditPart;
+import com.jaspersoft.studio.model.IGraphicElement;
 import com.jaspersoft.studio.model.INode;
+import com.jaspersoft.studio.model.util.ShowChildrenModelVisitor;
 import com.jaspersoft.studio.property.dataset.dialog.IDatasetDialogSupport;
 
 public class ListPageEditPart extends PageEditPart implements
@@ -56,5 +63,31 @@ public class ListPageEditPart extends PageEditPart implements
 			containerSize = d;
 		} else
 			containerSize = null;
+	}
+	
+	@Override
+	protected List<Object> getModelChildren() {
+		final List<Object> list = new ArrayList<Object>();
+		new ShowChildrenModelVisitor<Object>(getPage()) {
+
+			@Override
+			public boolean visit(INode n) {
+				if (n instanceof MCallout) {
+					list.add(n);
+					for (INode child : n.getChildren()){
+						//the connection must not be returned, since their edit part 
+						//must not be created trough the edit part factory but from the createConnection
+						//method of the Pin/Callout edit part
+						if (!(child instanceof MPinConnection)) {
+							list.add(child);
+						}
+					}
+				} else if (n instanceof IGraphicElement){
+					list.add(n);
+				}
+				return true;
+			}
+		};
+		return list;
 	}
 }
