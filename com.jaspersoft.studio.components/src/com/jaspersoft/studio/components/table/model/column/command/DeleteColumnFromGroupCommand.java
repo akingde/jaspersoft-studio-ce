f97fc4b8249ee44c2cfc6178bdd76a3d9c4375f5
morlandin <morlandin@tibco.com>
2016-06-07 11:49:51 +0000

    Fixed invalid table group when all the columns inside are deleted, now the group is removed after the last column is deleted
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@6959 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/table/model/column/command/DeleteColumnFromGroupCommand.java
+++ com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/table/model/column/command/DeleteColumnFromGroupCommand.java
@@ -12,34 +12,49 @@
  ******************************************************************************/
 package com.jaspersoft.studio.components.table.model.column.command;
 
-import net.sf.jasperreports.components.table.StandardBaseColumn;
-import net.sf.jasperreports.components.table.StandardColumnGroup;
-import net.sf.jasperreports.components.table.StandardTable;
+import java.util.List;
 
 import org.eclipse.gef.commands.Command;
 
+import com.jaspersoft.studio.components.table.TableComponentFactory;
 import com.jaspersoft.studio.components.table.TableManager;
 import com.jaspersoft.studio.components.table.model.column.MColumn;
 import com.jaspersoft.studio.components.table.model.columngroup.MColumnGroup;
 import com.jaspersoft.studio.components.table.model.columngroup.MColumnGroupCell;
 import com.jaspersoft.studio.components.table.util.TableColumnSize;
-/*
- * link nodes & together.
+import com.jaspersoft.studio.model.ANode;
+import com.jaspersoft.studio.model.INode;
+
+import net.sf.jasperreports.components.table.StandardBaseColumn;
+import net.sf.jasperreports.components.table.StandardColumnGroup;
+import net.sf.jasperreports.components.table.StandardTable;
+
+/**
+ * Delete a column from a parent column group. It also refresh the
+ * name after its execution. It at the end the group is empty the 
+ * group itself is removed 
  * 
  * @author Chicu Veaceslav
  */
 public class DeleteColumnFromGroupCommand extends Command {
 
 	private StandardColumnGroup jrGroup;
+	
 	private StandardBaseColumn jrColumn;
+	
 	private StandardTable jrTable;
 
 	/** The element position. */
 	private int elementPosition = 0;
+	
+	private Command deleteGroupCommand = null;
+	
+	private ANode groupNode;
 
 	public DeleteColumnFromGroupCommand(StandardColumnGroup destNode, MColumn srcNode) {
 		super();
 		this.jrGroup = destNode;
+		this.groupNode = getGroupNode(destNode, srcNode.getTable().getChildren());
 		this.jrColumn = (StandardBaseColumn) srcNode.getValue();
 		this.jrTable = TableManager.getTable(srcNode.getMTable());
 		elementPosition = jrGroup.getColumns().indexOf(jrColumn);
@@ -48,6 +63,7 @@ public class DeleteColumnFromGroupCommand extends Command {
 	public DeleteColumnFromGroupCommand(MColumnGroup destNode, MColumn srcNode) {
 		super();
 		this.jrGroup = (StandardColumnGroup) destNode.getValue();
+		this.groupNode = destNode;
 		this.jrColumn = (StandardBaseColumn) srcNode.getValue();
 		this.jrTable = TableManager.getTable(destNode.getMTable());
 		elementPosition = jrGroup.getColumns().indexOf(jrColumn);
@@ -56,6 +72,7 @@ public class DeleteColumnFromGroupCommand extends Command {
 	public DeleteColumnFromGroupCommand(MColumnGroupCell destNode, MColumn srcNode) {
 		super();
 		this.jrGroup = (StandardColumnGroup) destNode.getValue();
+		this.groupNode = destNode;
 		this.jrColumn = (StandardBaseColumn) srcNode.getValue();
 		this.jrTable = TableManager.getTable(destNode.getMTable());
 		elementPosition = jrGroup.getColumns().indexOf(jrColumn);
@@ -69,7 +86,18 @@ public class DeleteColumnFromGroupCommand extends Command {
 	@Override
 	public void execute() {
 		jrGroup.removeColumn(jrColumn);
-		TableColumnSize.setGroupWidth2Top(jrTable.getColumns(), jrGroup, -jrColumn.getWidth());
+		
+		if (jrGroup.getColumns().isEmpty()){
+			deleteGroupCommand = TableComponentFactory.getDeleteColumnCommand(groupNode.getParent(), groupNode);
+			if (deleteGroupCommand != null && deleteGroupCommand.canExecute()){
+				//Set the width to 0 since it is not necessary to compute a new width
+				//because we already know it is empty
+				jrGroup.setWidth(0);
+				deleteGroupCommand.execute();
+			}
+		} else {
+			TableColumnSize.setGroupWidth2Top(jrTable.getColumns(), jrGroup, -jrColumn.getWidth());	
+		}
 	}
 
 	/*
@@ -91,12 +119,29 @@ public class DeleteColumnFromGroupCommand extends Command {
 	 */
 	@Override
 	public void undo() {
-		if (elementPosition < 0 || elementPosition > jrGroup.getColumns().size())
+		if (deleteGroupCommand != null && deleteGroupCommand.canUndo()){
+			deleteGroupCommand.undo();
+		}
+		
+		if (elementPosition < 0 || elementPosition > jrGroup.getColumns().size()){
 			jrGroup.addColumn(jrColumn);
-		else
+		} else {
 			jrGroup.addColumn(elementPosition, jrColumn);
-		jrGroup.setWidth(jrGroup.getWidth() + jrColumn.getWidth());
+		}
 		TableColumnSize.setGroupWidth2Top(jrTable.getColumns(), jrGroup, jrColumn.getWidth());
+	}
+	
+	private ANode getGroupNode(StandardColumnGroup searchedGroup, List<INode> children){
+		for(INode child : children){
+			if (child.getValue() == searchedGroup){
+				return (ANode)child;
+			}
+			ANode childrenResult = getGroupNode(searchedGroup, child.getChildren());
+			if (childrenResult != null){
+				return childrenResult;
+			}
+		}
+		return null;
 	}
 
 }
