613cc7c9144742d028eca81f2aeb16e463e6cce4
chicuslavic <chicuslavic@tibco.com>
2016-07-06 09:30:09 +0000

    JSS-138: JSS - repository explorer - errors in error log, while editing URI field
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@7062 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/wizard/validator/URLValidator.java
+++ com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/wizard/validator/URLValidator.java
@@ -12,27 +12,18 @@
  ******************************************************************************/
 package com.jaspersoft.studio.server.wizard.validator;
 
-import java.net.MalformedURLException;
-import java.net.URL;
+import org.eclipse.core.databinding.validation.ValidationStatus;
+import org.eclipse.core.runtime.IStatus;
 
 import net.sf.jasperreports.eclipse.ui.validator.EmptyStringValidator;
 
-import org.eclipse.core.databinding.validation.ValidationStatus;
-import org.eclipse.core.runtime.IStatus;
-import org.eclipse.core.runtime.Status;
-
 public class URLValidator extends EmptyStringValidator {
+
 	@Override
 	public IStatus validate(Object value) {
 		IStatus st = super.validate(value);
-		if (st.isOK()) {
-			try {
-				new URL((String) value);
-			} catch (MalformedURLException e) {
-				return ValidationStatus.error("URL is not good", e);
-			}
-			return Status.OK_STATUS;
-		}
+		if (st.isOK() && !((String) value).matches("^(https?)://[-a-zA-Z0-9+&@#/%?=~_|!:,.;]*[-a-zA-Z0-9+&@#/%=~_|]"))
+			return ValidationStatus.error("URL is wrong");
 		return st;
 	}
 }
