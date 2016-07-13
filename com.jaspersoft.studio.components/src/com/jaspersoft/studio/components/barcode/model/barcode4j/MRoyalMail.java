fcc386f15f5833b1bf11fbb059c1804c4547c8cb
chicuslavic <chicuslavic@tibco.com>
2016-07-13 08:39:15 +0000

    JSS-241: JSS - Barcode - Text Position has no sense for some types of barcodes
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@7101 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/barcode/model/barcode4j/MRoyalMail.java
+++ com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/barcode/model/barcode4j/MRoyalMail.java
@@ -12,26 +12,146 @@
  ******************************************************************************/
 package com.jaspersoft.studio.components.barcode.model.barcode4j;
 
+import net.sf.jasperreports.components.barcode4j.FourStateBarcodeComponent;
 import net.sf.jasperreports.components.barcode4j.RoyalMailCustomerComponent;
 import net.sf.jasperreports.engine.JRConstants;
+import net.sf.jasperreports.engine.JRElement;
 import net.sf.jasperreports.engine.component.ComponentKey;
 import net.sf.jasperreports.engine.design.JRDesignComponentElement;
 import net.sf.jasperreports.engine.design.JRDesignExpression;
 import net.sf.jasperreports.engine.design.JasperDesign;
 
+import java.util.HashSet;
+import java.util.List;
+
+import org.eclipse.ui.views.properties.IPropertyDescriptor;
+
+import com.jaspersoft.studio.components.barcode.messages.Messages;
 import com.jaspersoft.studio.editor.defaults.DefaultManager;
 import com.jaspersoft.studio.model.ANode;
+import com.jaspersoft.studio.property.descriptors.DoublePropertyDescriptor;
+import com.jaspersoft.studio.property.descriptors.JSSComboPropertyDescriptor;
 
-public class MRoyalMail extends MFourStateBarcode {
+public class MRoyalMail extends MBarcode4j {
 	public static final long serialVersionUID = JRConstants.SERIAL_VERSION_UID;
+	private static IPropertyDescriptor[] descriptors;
 
 	public MRoyalMail() {
 		super();
 	}
 
-	public MRoyalMail(ANode parent, JRDesignComponentElement jrBarcode,
-			int newIndex) {
+	public MRoyalMail(ANode parent, JRDesignComponentElement jrBarcode, int newIndex) {
 		super(parent, jrBarcode, newIndex);
+	}
+
+	@Override
+	public IPropertyDescriptor[] getDescriptors() {
+		return descriptors;
+	}
+
+	@Override
+	public void setDescriptors(IPropertyDescriptor[] descriptors1) {
+		descriptors = descriptors1;
+	}
+
+	/**
+	 * Creates the property descriptors.
+	 * 
+	 * @param desc
+	 *            the desc
+	 */
+	@Override
+	public void createPropertyDescriptors(List<IPropertyDescriptor> desc) {
+		super.createPropertyDescriptors(desc);
+
+		JSSComboPropertyDescriptor checksumModeD = new JSSComboPropertyDescriptor(
+				FourStateBarcodeComponent.PROPERTY_CHECKSUM_MODE, Messages.common_checksum_mode,
+				ChecksumMode.getItems());
+		checksumModeD.setDescription(Messages.MFourStateBarcode_checksum_mode_description);
+		desc.add(checksumModeD);
+
+		DoublePropertyDescriptor intercharD = new DoublePropertyDescriptor(
+				FourStateBarcodeComponent.PROPERTY_INTERCHAR_GAP_WIDTH, Messages.common_interchar_gap_width);
+		intercharD.setDescription(Messages.MFourStateBarcode_interchar_gap_width_description);
+		desc.add(intercharD);
+
+		DoublePropertyDescriptor ascenderHeightD = new DoublePropertyDescriptor(
+				FourStateBarcodeComponent.PROPERTY_ASCENDER_HEIGHT, Messages.MFourStateBarcode_ascender_height);
+		ascenderHeightD.setDescription(Messages.MFourStateBarcode_ascender_height_description);
+		desc.add(ascenderHeightD);
+
+		DoublePropertyDescriptor trackHeightD = new DoublePropertyDescriptor(
+				FourStateBarcodeComponent.PROPERTY_TRACK_HEIGHT, Messages.MFourStateBarcode_track_height);
+		trackHeightD.setDescription(Messages.MFourStateBarcode_track_height_description);
+		desc.add(trackHeightD);
+
+		checksumModeD.setCategory(Messages.common_properties_category);
+		intercharD.setCategory(Messages.common_properties_category);
+		ascenderHeightD.setCategory(Messages.common_properties_category);
+		trackHeightD.setCategory(Messages.common_properties_category);
+	}
+
+	@Override
+	public Object getPropertyValue(Object id) {
+		JRDesignComponentElement jrElement = (JRDesignComponentElement) getValue();
+		FourStateBarcodeComponent jrList = (FourStateBarcodeComponent) jrElement.getComponent();
+
+		if (id.equals(FourStateBarcodeComponent.PROPERTY_INTERCHAR_GAP_WIDTH))
+			return jrList.getIntercharGapWidth();
+
+		if (id.equals(FourStateBarcodeComponent.PROPERTY_ASCENDER_HEIGHT))
+			return jrList.getAscenderHeight();
+		if (id.equals(FourStateBarcodeComponent.PROPERTY_TRACK_HEIGHT))
+			return jrList.getTrackHeight();
+
+		if (id.equals(FourStateBarcodeComponent.PROPERTY_CHECKSUM_MODE))
+			return ChecksumMode.getPos4ChecksumMode(jrList.getChecksumMode());
+
+		return super.getPropertyValue(id);
+	}
+
+	@Override
+	public void setPropertyValue(Object id, Object value) {
+		JRDesignComponentElement jrElement = (JRDesignComponentElement) getValue();
+		FourStateBarcodeComponent jrList = (FourStateBarcodeComponent) jrElement.getComponent();
+
+		if (id.equals(FourStateBarcodeComponent.PROPERTY_INTERCHAR_GAP_WIDTH))
+			jrList.setIntercharGapWidth((Double) value);
+		else if (id.equals(FourStateBarcodeComponent.PROPERTY_CHECKSUM_MODE))
+			jrList.setChecksumMode(ChecksumMode.getChecksumMode4Pos((Integer) value));
+
+		else if (id.equals(FourStateBarcodeComponent.PROPERTY_ASCENDER_HEIGHT))
+			jrList.setAscenderHeight((Double) value);
+		else if (id.equals(FourStateBarcodeComponent.PROPERTY_TRACK_HEIGHT))
+			jrList.setTrackHeight((Double) value);
+		else
+			super.setPropertyValue(id, value);
+	}
+
+	@Override
+	public HashSet<String> generateGraphicalProperties() {
+		HashSet<String> properties = super.generateGraphicalProperties();
+		properties.add(FourStateBarcodeComponent.PROPERTY_INTERCHAR_GAP_WIDTH);
+		properties.add(FourStateBarcodeComponent.PROPERTY_CHECKSUM_MODE);
+		properties.add(FourStateBarcodeComponent.PROPERTY_ASCENDER_HEIGHT);
+		properties.add(FourStateBarcodeComponent.PROPERTY_TRACK_HEIGHT);
+		return properties;
+	}
+
+	@Override
+	public void trasnferProperties(JRElement target) {
+		super.trasnferProperties(target);
+
+		JRDesignComponentElement jrSourceElement = (JRDesignComponentElement) getValue();
+		FourStateBarcodeComponent jrSourceBarcode = (FourStateBarcodeComponent) jrSourceElement.getComponent();
+
+		JRDesignComponentElement jrTargetElement = (JRDesignComponentElement) target;
+		FourStateBarcodeComponent jrTargetBarcode = (FourStateBarcodeComponent) jrTargetElement.getComponent();
+
+		jrTargetBarcode.setIntercharGapWidth(jrSourceBarcode.getIntercharGapWidth());
+		jrTargetBarcode.setChecksumMode(jrSourceBarcode.getChecksumMode());
+		jrTargetBarcode.setAscenderHeight(jrSourceBarcode.getAscenderHeight());
+		jrTargetBarcode.setTrackHeight(jrSourceBarcode.getTrackHeight());
 	}
 
 	@Override
@@ -42,10 +162,11 @@ public class MRoyalMail extends MFourStateBarcode {
 		exp.setText("\"123456789\""); //$NON-NLS-1$
 		component.setCodeExpression(exp);
 		el.setComponent(component);
-		el.setComponentKey(new ComponentKey("http://jasperreports.sourceforge.net/jasperreports/components", "jr", "RoyalMailCustomer")); //$NON-NLS-1$
-		
+		el.setComponentKey(new ComponentKey("http://jasperreports.sourceforge.net/jasperreports/components", "jr", //$NON-NLS-1$
+				"RoyalMailCustomer"));
+
 		DefaultManager.INSTANCE.applyDefault(this.getClass(), el);
-		
+
 		return el;
 	}
 }
--- com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/barcode/property/RoyalMailSection.java
+++ com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/barcode/property/RoyalMailSection.java
@@ -18,10 +18,9 @@ import com.jaspersoft.studio.components.barcode.messages.Messages;
 import com.jaspersoft.studio.properties.view.TabbedPropertySheetPage;
 import com.jaspersoft.studio.property.section.AbstractSection;
 
-import net.sf.jasperreports.components.barcode4j.Barcode4jComponent;
 import net.sf.jasperreports.components.barcode4j.FourStateBarcodeComponent;
 
-public class FourStateSection extends AbstractSection {
+public class RoyalMailSection extends AbstractSection {
 
 	/**
 	 * @see org.eclipse.ui.views.properties.tabbed.ITabbedPropertySection#createControls(org.eclipse.swt.widgets.Composite,
@@ -30,13 +29,12 @@ public class FourStateSection extends AbstractSection {
 	public void createControls(Composite parent, TabbedPropertySheetPage tabbedPropertySheetPage) {
 		super.createControls(parent, tabbedPropertySheetPage);
 
-		Composite group = getWidgetFactory().createSection(parent, "Four State", false, 2);
+		Composite group = getWidgetFactory().createSection(parent, "Royal Mail", false, 2);
 
 		createWidget4Property(group, FourStateBarcodeComponent.PROPERTY_CHECKSUM_MODE);
 		createWidget4Property(group, FourStateBarcodeComponent.PROPERTY_INTERCHAR_GAP_WIDTH);
 		createWidget4Property(group, FourStateBarcodeComponent.PROPERTY_ASCENDER_HEIGHT);
 		createWidget4Property(group, FourStateBarcodeComponent.PROPERTY_TRACK_HEIGHT);
-		createWidget4Property(group, Barcode4jComponent.PROPERTY_TEXT_POSITION);
 	}
 
 	@Override
@@ -48,6 +46,5 @@ public class FourStateSection extends AbstractSection {
 		addProvidedProperties(FourStateBarcodeComponent.PROPERTY_ASCENDER_HEIGHT,
 				Messages.MFourStateBarcode_ascender_height);
 		addProvidedProperties(FourStateBarcodeComponent.PROPERTY_TRACK_HEIGHT, Messages.MFourStateBarcode_track_height);
-		addProvidedProperties(Barcode4jComponent.PROPERTY_TEXT_POSITION, Messages.MBarcode4j_text_position);
 	}
 }
