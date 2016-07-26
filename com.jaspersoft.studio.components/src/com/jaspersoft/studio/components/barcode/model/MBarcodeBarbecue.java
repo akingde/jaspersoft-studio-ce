6f5beecadafd7c38127f0fad2e072c515ea39599
chicuslavic <chicuslavic@tibco.com>
2016-07-26 11:04:42 +0000

    JSS-272: JSS - Properties - Barcode - no default values for Bar Width and Height
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@7137 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/barcode/model/MBarcodeBarbecue.java
+++ com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/barcode/model/MBarcodeBarbecue.java
@@ -51,14 +51,14 @@ import com.jaspersoft.studio.utils.ModelUtils;
  * The Class MBarcode.
  */
 public class MBarcodeBarbecue extends MBarcode implements IRotatable {
-	
+
 	public static final long serialVersionUID = JRConstants.SERIAL_VERSION_UID;
-	
+
 	/** The icon descriptor. */
 	private static IIconDescriptor iconDescriptor;
-	
+
 	private static IPropertyDescriptor[] descriptors;
-	
+
 	private static NamedEnumPropertyDescriptor<RotationEnum> rotationD;
 
 	/**
@@ -89,8 +89,7 @@ public class MBarcodeBarbecue extends MBarcode implements IRotatable {
 	 * @param newIndex
 	 *            the new index
 	 */
-	public MBarcodeBarbecue(ANode parent, JRDesignComponentElement jrBarcode,
-			int newIndex) {
+	public MBarcodeBarbecue(ANode parent, JRDesignComponentElement jrBarcode, int newIndex) {
 		super(parent, newIndex);
 		setValue(jrBarcode);
 	}
@@ -103,8 +102,8 @@ public class MBarcodeBarbecue extends MBarcode implements IRotatable {
 		exp.setText("\"1234\""); //$NON-NLS-1$
 		component.setCodeExpression(exp);
 		el.setComponent(component);
-		el.setComponentKey(new ComponentKey(
-				"http://jasperreports.sourceforge.net/jasperreports/components", "jr", "barbecue")); //$NON-NLS-1$
+		el.setComponentKey(
+				new ComponentKey("http://jasperreports.sourceforge.net/jasperreports/components", "jr", "barbecue")); //$NON-NLS-1$
 
 		DefaultManager.INSTANCE.applyDefault(this.getClass(), el);
 
@@ -172,15 +171,13 @@ public class MBarcodeBarbecue extends MBarcode implements IRotatable {
 	public void createPropertyDescriptors(List<IPropertyDescriptor> desc) {
 		super.createPropertyDescriptors(desc);
 
-		PixelPropertyDescriptor widthD = new PixelPropertyDescriptor(
-				StandardBarbecueComponent.PROPERTY_BAR_WIDTH,
+		PixelPropertyDescriptor widthD = new PixelPropertyDescriptor(StandardBarbecueComponent.PROPERTY_BAR_WIDTH,
 				Messages.MBarcodeBarbecue_bar_width);
 		widthD.setValidator(new JSSPixelBarcodeSizeValidator());
 		widthD.setDescription(Messages.MBarcodeBarbecue_bar_width_description);
 		desc.add(widthD);
 
-		PixelPropertyDescriptor heightD = new PixelPropertyDescriptor(
-				StandardBarbecueComponent.PROPERTY_BAR_HEIGTH,
+		PixelPropertyDescriptor heightD = new PixelPropertyDescriptor(StandardBarbecueComponent.PROPERTY_BAR_HEIGTH,
 				Messages.MBarcodeBarbecue_bar_height);
 		heightD.setValidator(new JSSPixelBarcodeSizeValidator());
 		heightD.setDescription(Messages.MBarcodeBarbecue_bar_height_description);
@@ -189,41 +186,33 @@ public class MBarcodeBarbecue extends MBarcode implements IRotatable {
 		JRExpressionPropertyDescriptor appIDexprD = new JRExpressionPropertyDescriptor(
 				StandardBarbecueComponent.PROPERTY_APPLICATION_IDENTIFIER_EXPRESSION,
 				Messages.MBarcodeBarbecue_application_identifier_expression);
-		appIDexprD
-				.setDescription(Messages.MBarcodeBarbecue_application_identifier_expression_description);
+		appIDexprD.setDescription(Messages.MBarcodeBarbecue_application_identifier_expression_description);
 		desc.add(appIDexprD);
 
-		RComboBoxPropertyDescriptor typeD = new RComboBoxPropertyDescriptor(
-				StandardBarbecueComponent.PROPERTY_TYPE,
+		RComboBoxPropertyDescriptor typeD = new RComboBoxPropertyDescriptor(StandardBarbecueComponent.PROPERTY_TYPE,
 				Messages.MBarcodeBarbecue_type,
-				new String[] {
-						"2of7", "3of9", "Bookland", "Codabar", "Code128", "Code128A", "Code128B", "Code128C", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$
-						"Code39", "Code39 (Extended)", "EAN128", "EAN13", "GlobalTradeItemNumber", "Int2of5", "Monarch", "NW7", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$
-						"PDF417", "PostNet", "RandomWeightUPCA", "SCC14ShippingCode", "ShipmentIdentificationNumber", "SSCC18", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
+				new String[] { "2of7", "3of9", "Bookland", "Codabar", "Code128", "Code128A", "Code128B", "Code128C", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$ //$NON-NLS-8$
+						"Code39", "Code39 (Extended)", "EAN128", "EAN13", "GlobalTradeItemNumber", "Int2of5", "Monarch", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$ //$NON-NLS-7$
+						"NW7", //$NON-NLS-1$
+						"PDF417", "PostNet", "RandomWeightUPCA", "SCC14ShippingCode", "ShipmentIdentificationNumber", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
+						"SSCC18", //$NON-NLS-1$
 						"Std2of5", "UCC128", "UPCA", "USD3", "USD4", "USPS" }); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
 		typeD.setDescription(Messages.MBarcodeBarbecue_type_description);
 		desc.add(typeD);
 
 		CheckBoxPropertyDescriptor checksumRequiredD = new CheckBoxPropertyDescriptor(
-				StandardBarbecueComponent.PROPERTY_CHECKSUM_REQUIRED,
-				Messages.MBarcodeBarbecue_checksum_required);
-		checksumRequiredD
-				.setDescription(Messages.MBarcodeBarbecue_checksum_required_description);
+				StandardBarbecueComponent.PROPERTY_CHECKSUM_REQUIRED, Messages.MBarcodeBarbecue_checksum_required);
+		checksumRequiredD.setDescription(Messages.MBarcodeBarbecue_checksum_required_description);
 		desc.add(checksumRequiredD);
 
 		CheckBoxPropertyDescriptor drawTextD = new CheckBoxPropertyDescriptor(
-				StandardBarbecueComponent.PROPERTY_DRAW_TEXT,
-				Messages.MBarcodeBarbecue_draw_text);
-		drawTextD
-				.setDescription(Messages.MBarcodeBarbecue_draw_text_description);
+				StandardBarbecueComponent.PROPERTY_DRAW_TEXT, Messages.MBarcodeBarbecue_draw_text);
+		drawTextD.setDescription(Messages.MBarcodeBarbecue_draw_text_description);
 		desc.add(drawTextD);
 
-		rotationD = new NamedEnumPropertyDescriptor<RotationEnum>(
-				StandardBarbecueComponent.PROPERTY_ROTATION,
-				Messages.MBarcodeBarbecue_rotation, RotationEnum.LEFT,
-				NullEnum.INHERITED);
-		rotationD
-				.setDescription(Messages.MBarcodeBarbecue_rotation_description);
+		rotationD = new NamedEnumPropertyDescriptor<RotationEnum>(StandardBarbecueComponent.PROPERTY_ROTATION,
+				Messages.MBarcodeBarbecue_rotation, RotationEnum.LEFT, NullEnum.INHERITED);
+		rotationD.setDescription(Messages.MBarcodeBarbecue_rotation_description);
 		desc.add(rotationD);
 
 		rotationD.setCategory(Messages.common_properties_category);
@@ -234,22 +223,25 @@ public class MBarcodeBarbecue extends MBarcode implements IRotatable {
 		heightD.setCategory(Messages.common_properties_category);
 		appIDexprD.setCategory(Messages.common_properties_category);
 	}
-	
+
 	@Override
 	protected Map<String, DefaultValue> createDefaultsMap() {
 		Map<String, DefaultValue> defaultsMap = super.createDefaultsMap();
-		
-		defaultsMap.put(StandardBarbecueComponent.PROPERTY_EVALUATION_TIME, new DefaultValue(EvaluationTimeEnum.NOW, false));
+
+		defaultsMap.put(StandardBarbecueComponent.PROPERTY_EVALUATION_TIME,
+				new DefaultValue(EvaluationTimeEnum.NOW, false));
 		defaultsMap.put(StandardBarbecueComponent.PROPERTY_ROTATION, new DefaultValue(null, true));
-		
+
+		defaultsMap.put(StandardBarbecueComponent.PROPERTY_BAR_WIDTH, new DefaultValue(true));
+		defaultsMap.put(StandardBarbecueComponent.PROPERTY_BAR_HEIGTH, new DefaultValue(true));
+
 		return defaultsMap;
 	}
 
 	@Override
 	public Object getPropertyValue(Object id) {
 		JRDesignComponentElement jrElement = (JRDesignComponentElement) getValue();
-		StandardBarbecueComponent barbecueComponent = (StandardBarbecueComponent) jrElement
-				.getComponent();
+		StandardBarbecueComponent barbecueComponent = (StandardBarbecueComponent) jrElement.getComponent();
 
 		if (id.equals(StandardBarbecueComponent.PROPERTY_EVALUATION_TIME))
 			return barbecueComponent.getEvaluationTimeValue();
@@ -268,37 +260,29 @@ public class MBarcodeBarbecue extends MBarcode implements IRotatable {
 		if (id.equals(StandardBarbecueComponent.PROPERTY_BAR_WIDTH))
 			return barbecueComponent.getBarWidth();
 		if (id.equals(StandardBarbecueComponent.PROPERTY_CODE_EXPRESSION))
-			return ExprUtil
-					.getExpression(barbecueComponent.getCodeExpression());
+			return ExprUtil.getExpression(barbecueComponent.getCodeExpression());
 		if (id.equals(StandardBarbecueComponent.PROPERTY_ROTATION))
 			return rotationD.getIntValue(barbecueComponent.getOwnRotation());
 		if (id.equals(StandardBarbecueComponent.PROPERTY_APPLICATION_IDENTIFIER_EXPRESSION))
-			return ExprUtil.getExpression(barbecueComponent
-					.getApplicationIdentifierExpression());
+			return ExprUtil.getExpression(barbecueComponent.getApplicationIdentifierExpression());
 		return super.getPropertyValue(id);
 	}
 
 	@Override
 	public void setPropertyValue(Object id, Object value) {
 		JRDesignComponentElement jrElement = (JRDesignComponentElement) getValue();
-		StandardBarbecueComponent barbecueComponent = (StandardBarbecueComponent) jrElement
-				.getComponent();
+		StandardBarbecueComponent barbecueComponent = (StandardBarbecueComponent) jrElement.getComponent();
 
 		if (id.equals(StandardBarbecueComponent.PROPERTY_EVALUATION_TIME)) {
-			EvaluationTimeEnum evalTime = EnumHelper.getEnumByObjectValue(
-					EvaluationTimeEnum.values(), value);
+			EvaluationTimeEnum evalTime = EnumHelper.getEnumByObjectValue(EvaluationTimeEnum.values(), value);
 			barbecueComponent.setEvaluationTimeValue(evalTime);
 			if (evalTime != null && !evalTime.equals(EvaluationTimeEnum.GROUP)) {
 				barbecueComponent.setEvaluationGroup(null);
 			}
-		} else if (id
-				.equals(StandardBarbecueComponent.PROPERTY_EVALUATION_GROUP)) {
-			barbecueComponent.setEvaluationGroup(ModelUtils
-					.getGroupNameForProperty(value));
-		} else if (id
-				.equals(StandardBarbecueComponent.PROPERTY_CHECKSUM_REQUIRED))
-			barbecueComponent.setChecksumRequired(((Boolean) value)
-					.booleanValue());
+		} else if (id.equals(StandardBarbecueComponent.PROPERTY_EVALUATION_GROUP)) {
+			barbecueComponent.setEvaluationGroup(ModelUtils.getGroupNameForProperty(value));
+		} else if (id.equals(StandardBarbecueComponent.PROPERTY_CHECKSUM_REQUIRED))
+			barbecueComponent.setChecksumRequired(((Boolean) value).booleanValue());
 		else if (id.equals(StandardBarbecueComponent.PROPERTY_DRAW_TEXT))
 			barbecueComponent.setDrawText(((Boolean) value).booleanValue());
 		else if (id.equals(StandardBarbecueComponent.PROPERTY_TYPE))
@@ -316,17 +300,11 @@ public class MBarcodeBarbecue extends MBarcode implements IRotatable {
 			if (intv != null)
 				intv = Math.abs(intv.intValue());
 			barbecueComponent.setBarWidth((Integer) value);
-		} else if (id
-				.equals(StandardBarbecueComponent.PROPERTY_CODE_EXPRESSION)) {
-			barbecueComponent.setCodeExpression(ExprUtil.setValues(
-					barbecueComponent.getCodeExpression(), value, null));
-		} else if (id
-				.equals(StandardBarbecueComponent.PROPERTY_APPLICATION_IDENTIFIER_EXPRESSION)) {
-			barbecueComponent
-					.setApplicationIdentifierExpression(ExprUtil.setValues(
-							barbecueComponent
-									.getApplicationIdentifierExpression(),
-							value, null));
+		} else if (id.equals(StandardBarbecueComponent.PROPERTY_CODE_EXPRESSION)) {
+			barbecueComponent.setCodeExpression(ExprUtil.setValues(barbecueComponent.getCodeExpression(), value, null));
+		} else if (id.equals(StandardBarbecueComponent.PROPERTY_APPLICATION_IDENTIFIER_EXPRESSION)) {
+			barbecueComponent.setApplicationIdentifierExpression(
+					ExprUtil.setValues(barbecueComponent.getApplicationIdentifierExpression(), value, null));
 		} else
 			super.setPropertyValue(id, value);
 	}
@@ -336,12 +314,10 @@ public class MBarcodeBarbecue extends MBarcode implements IRotatable {
 		super.trasnferProperties(target);
 
 		JRDesignComponentElement jrSourceElement = (JRDesignComponentElement) getValue();
-		StandardBarbecueComponent jrSourceCode = (StandardBarbecueComponent) jrSourceElement
-				.getComponent();
+		StandardBarbecueComponent jrSourceCode = (StandardBarbecueComponent) jrSourceElement.getComponent();
 
 		JRDesignComponentElement jrTargetElement = (JRDesignComponentElement) target;
-		StandardBarbecueComponent jrTargetCode = (StandardBarbecueComponent) jrTargetElement
-				.getComponent();
+		StandardBarbecueComponent jrTargetCode = (StandardBarbecueComponent) jrTargetElement.getComponent();
 
 		jrTargetCode.setChecksumRequired(jrSourceCode.isChecksumRequired());
 		jrTargetCode.setDrawText(jrSourceCode.isDrawText());
