0a1530108d9c9a4771f7164241883cc784eec897
chicuslavic <chicuslavic@tibco.com>
2016-09-13 13:02:11 +0000

    added language to json data adapter ui
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@7234 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.data/src/com/jaspersoft/studio/data/json/JsonDataAdapterComposite.java
+++ com.jaspersoft.studio.data/src/com/jaspersoft/studio/data/json/JsonDataAdapterComposite.java
@@ -12,10 +12,6 @@
  ******************************************************************************/
 package com.jaspersoft.studio.data.json;
 
-import net.sf.jasperreports.data.DataAdapter;
-import net.sf.jasperreports.data.json.JsonDataAdapter;
-import net.sf.jasperreports.engine.JasperReportsContext;
-
 import org.eclipse.core.databinding.beans.PojoObservables;
 import org.eclipse.jface.databinding.swt.SWTObservables;
 import org.eclipse.swt.SWT;
@@ -25,6 +21,7 @@ import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Button;
+import org.eclipse.swt.widgets.Combo;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
@@ -33,6 +30,12 @@ import com.jaspersoft.studio.data.AFileDataAdapterComposite;
 import com.jaspersoft.studio.data.DataAdapterDescriptor;
 import com.jaspersoft.studio.data.DateNumberFormatWidget;
 import com.jaspersoft.studio.data.messages.Messages;
+import com.jaspersoft.studio.utils.Misc;
+
+import net.sf.jasperreports.data.DataAdapter;
+import net.sf.jasperreports.data.json.JsonDataAdapter;
+import net.sf.jasperreports.data.json.JsonExpressionLanguageEnum;
+import net.sf.jasperreports.engine.JasperReportsContext;
 
 public class JsonDataAdapterComposite extends AFileDataAdapterComposite {
 
@@ -43,6 +46,7 @@ public class JsonDataAdapterComposite extends AFileDataAdapterComposite {
 	private boolean useConnection = false;
 
 	private DateNumberFormatWidget dnf;
+	private Combo clang;
 
 	/**
 	 * Create the composite.
@@ -50,14 +54,12 @@ public class JsonDataAdapterComposite extends AFileDataAdapterComposite {
 	 * @param parent
 	 * @param style
 	 */
-	public JsonDataAdapterComposite(Composite parent, int style,
-			JasperReportsContext jrContext) {
+	public JsonDataAdapterComposite(Composite parent, int style, JasperReportsContext jrContext) {
 		super(parent, style, jrContext);
 		setLayout(new GridLayout(1, false));
 
 		Composite composite = new Composite(this, SWT.NONE);
-		composite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false,
-				1, 1));
+		composite.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		GridLayout gl_composite = new GridLayout(3, false);
 		gl_composite.marginWidth = 0;
 		gl_composite.marginHeight = 0;
@@ -66,16 +68,14 @@ public class JsonDataAdapterComposite extends AFileDataAdapterComposite {
 		createFileNameWidgets(composite);
 
 		Composite composite_1 = new Composite(this, SWT.NONE);
-		composite_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true,
-				false, 1, 1));
+		composite_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 		composite_1.setLayout(new FillLayout(SWT.VERTICAL));
 
 		btnRadioButtonUseXpath = new Button(composite_1, SWT.RADIO);
 		btnRadioButtonUseXpath.setText(Messages.JsonDataAdapterComposite_2);
 
 		btnRadioButtonCreateDataAdapter = new Button(composite_1, SWT.RADIO);
-		btnRadioButtonCreateDataAdapter
-				.setText(Messages.JsonDataAdapterComposite_3);
+		btnRadioButtonCreateDataAdapter.setText(Messages.JsonDataAdapterComposite_3);
 		btnRadioButtonCreateDataAdapter.setSelection(true);
 
 		Composite composite_2 = new Composite(composite_1, SWT.NONE);
@@ -84,14 +84,20 @@ public class JsonDataAdapterComposite extends AFileDataAdapterComposite {
 		gl_composite_2.marginHeight = 0;
 		composite_2.setLayout(gl_composite_2);
 
+		Label lbl = new Label(composite_2, SWT.NONE);
+		lbl.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
+		lbl.setText("Language");
+
+		clang = new Combo(composite_2, SWT.READ_ONLY);
+		clang.setItems(new String[] { JsonExpressionLanguageEnum.JSON.getName(),
+				JsonExpressionLanguageEnum.JSONQL.getName() });
+
 		Label lblNewLabel_1 = new Label(composite_2, SWT.NONE);
-		lblNewLabel_1.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false,
-				false, 1, 1));
+		lblNewLabel_1.setLayoutData(new GridData(SWT.RIGHT, SWT.CENTER, false, false, 1, 1));
 		lblNewLabel_1.setText(Messages.JsonDataAdapterComposite_4);
 
 		textSelectExpression = new Text(composite_2, SWT.BORDER);
-		textSelectExpression.setLayoutData(new GridData(SWT.FILL, SWT.CENTER,
-				true, false, 1, 1));
+		textSelectExpression.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 1, 1));
 
 		new Label(composite_1, SWT.SEPARATOR | SWT.HORIZONTAL);
 
@@ -102,22 +108,21 @@ public class JsonDataAdapterComposite extends AFileDataAdapterComposite {
 			@Override
 			public void widgetSelected(SelectionEvent e) {
 				textSelectExpression.setEnabled(false);
+				clang.setEnabled(false);
 				useConnection = true;
-				pchangesuport.firePropertyChange(
-						"createdataadapter", false, true); //$NON-NLS-1$
+				pchangesuport.firePropertyChange("createdataadapter", false, true); //$NON-NLS-1$
 			}
 		});
 
-		btnRadioButtonCreateDataAdapter
-				.addSelectionListener(new SelectionAdapter() {
-					@Override
-					public void widgetSelected(SelectionEvent e) {
-						textSelectExpression.setEnabled(true);
-						useConnection = false;
-						pchangesuport.firePropertyChange(
-								"createdataadapter", false, true); //$NON-NLS-1$
-					}
-				});
+		btnRadioButtonCreateDataAdapter.addSelectionListener(new SelectionAdapter() {
+			@Override
+			public void widgetSelected(SelectionEvent e) {
+				textSelectExpression.setEnabled(true);
+				clang.setEnabled(true);
+				useConnection = false;
+				pchangesuport.firePropertyChange("createdataadapter", false, true); //$NON-NLS-1$
+			}
+		});
 
 	}
 
@@ -126,15 +131,14 @@ public class JsonDataAdapterComposite extends AFileDataAdapterComposite {
 		JsonDataAdapter xmlDataAdapter = (JsonDataAdapter) dataAdapter;
 
 		doBindFileNameWidget(xmlDataAdapter);
-		bindingContext.bindValue(
-				SWTObservables.observeText(textSelectExpression, SWT.Modify),
+		bindingContext.bindValue(SWTObservables.observeText(textSelectExpression, SWT.Modify),
 				PojoObservables.observeValue(dataAdapter, "selectExpression")); //$NON-NLS-1$
+		bindingContext.bindValue(SWTObservables.observeText(clang),
+				PojoObservables.observeValue(new Proxy((JsonDataAdapter) dataAdapter), "language"));
 
-		dnf.bindWidgets(xmlDataAdapter, bindingContext,
-				xmlDataAdapter.getLocale(), xmlDataAdapter.getTimeZone());
+		dnf.bindWidgets(xmlDataAdapter, bindingContext, xmlDataAdapter.getLocale(), xmlDataAdapter.getTimeZone());
 
-		bindingContext.bindValue(SWTObservables
-				.observeSelection(btnRadioButtonCreateDataAdapter),
+		bindingContext.bindValue(SWTObservables.observeSelection(btnRadioButtonCreateDataAdapter),
 				PojoObservables.observeValue(dataAdapter, "useConnection")); //$NON-NLS-1$
 
 		if (xmlDataAdapter.isUseConnection()) {
@@ -142,11 +146,29 @@ public class JsonDataAdapterComposite extends AFileDataAdapterComposite {
 			btnRadioButtonCreateDataAdapter.setSelection(false);
 			textSelectExpression.setEnabled(false);
 			btnRadioButtonUseXpath.setSelection(true);
+			clang.setEnabled(false);
 		} else {
 			useConnection = false;
 			btnRadioButtonUseXpath.setSelection(false);
 			btnRadioButtonCreateDataAdapter.setSelection(true);
 			textSelectExpression.setEnabled(true);
+			clang.setEnabled(true);
+		}
+	}
+
+	class Proxy {
+		private JsonDataAdapter da;
+
+		public Proxy(JsonDataAdapter da) {
+			this.da = da;
+		}
+
+		public void setLanguage(String v) {
+			da.setLanguage(JsonExpressionLanguageEnum.getByName(v));
+		}
+
+		public String getLanguage() {
+			return Misc.nvl(da.getLanguage(), JsonExpressionLanguageEnum.JSON).getName();
 		}
 	}
 
@@ -154,8 +176,7 @@ public class JsonDataAdapterComposite extends AFileDataAdapterComposite {
 		if (dataAdapterDesc == null)
 			dataAdapterDesc = new JsonDataAdapterDescriptor();
 
-		JsonDataAdapter dataAdapter = (JsonDataAdapter) dataAdapterDesc
-				.getDataAdapter();
+		JsonDataAdapter dataAdapter = (JsonDataAdapter) dataAdapterDesc.getDataAdapter();
 
 		dataAdapter.setUseConnection(useConnection);
 		dataAdapter.setSelectExpression(textSelectExpression.getText());
@@ -164,6 +185,7 @@ public class JsonDataAdapterComposite extends AFileDataAdapterComposite {
 		dataAdapter.setNumberPattern(dnf.getTextNumberPattern());
 		dataAdapter.setLocale(dnf.getLocale());
 		dataAdapter.setTimeZone(dnf.getTimeZone());
+		dataAdapter.setLanguage(JsonExpressionLanguageEnum.getByName(clang.getText()));
 
 		return dataAdapterDesc;
 	}
