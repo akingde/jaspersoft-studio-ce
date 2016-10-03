a24ee6957c0c6ca3c1ad3ddaf11c9b296a05d72a
chicuslavic <chicuslavic@tibco.com>
2016-10-03 09:53:15 +0000

    JSS-1531: [case #71432] "Publish to Jasperreports Server" option does not work
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@7296 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/publish/action/ServerSettingsEditorMenuContributor.java
+++ com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/publish/action/ServerSettingsEditorMenuContributor.java
@@ -18,12 +18,15 @@ import java.util.List;
 import org.eclipse.gef.ui.actions.ActionRegistry;
 
 import com.jaspersoft.studio.editor.gef.ui.actions.IEditorSettingsMenuContributor;
+import com.jaspersoft.studio.server.preferences.JRSPreferencesPage;
 import com.jaspersoft.studio.utils.jasper.JasperReportsConfiguration;
 
 public class ServerSettingsEditorMenuContributor implements IEditorSettingsMenuContributor {
+	private JasperReportsConfiguration jConfig;
 
 	@Override
 	public void registerActions(ActionRegistry actionRegistry, JasperReportsConfiguration jConfig) {
+		this.jConfig = jConfig;
 		actionRegistry.registerAction(new ShowPublishDialogAction(jConfig));
 	}
 
@@ -34,6 +37,9 @@ public class ServerSettingsEditorMenuContributor implements IEditorSettingsMenuC
 
 	@Override
 	public List<String> getActionIds() {
-		return actions;
+		Boolean doSave = jConfig.getPropertyBoolean(JRSPreferencesPage.PUBLISH_REPORT_TOJRSONSAVE, Boolean.TRUE);
+		if (doSave)
+			return actions;
+		return new ArrayList<String>();
 	}
 }
