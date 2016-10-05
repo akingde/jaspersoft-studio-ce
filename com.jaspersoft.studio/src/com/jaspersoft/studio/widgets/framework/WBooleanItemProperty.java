8d5eedea67f909e489080539a22e0c37b7123c1e
mrabbi <mrabbi@tibco.com>
2016-10-05 15:58:48 +0000

    Fix for JIRA TIBCO #JSS-1524, #JSS-1532, #JSS-1519.
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@7309 6833c83e-4fd0-4c68-a3df-283c6148db30

---
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/widgets/framework/WBooleanItemProperty.java
@@ -0,0 +1,90 @@
+/*******************************************************************************
+ * Copyright (C) 2005 - 2016 TIBCO Software Inc. All rights reserved. http://www.jaspersoft.com.
+ * 
+ * Unless you have purchased a commercial license agreement from Jaspersoft, the following license terms apply:
+ * 
+ * This program and the accompanying materials are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at http://www.eclipse.org/legal/epl-v10.html
+ ******************************************************************************/
+package com.jaspersoft.studio.widgets.framework;
+
+import org.eclipse.swt.widgets.Composite;
+
+import com.jaspersoft.studio.widgets.framework.model.WidgetPropertyDescriptor;
+import com.jaspersoft.studio.widgets.framework.ui.ItemPropertyDescription;
+
+import net.sf.jasperreports.engine.JRExpression;
+import net.sf.jasperreports.engine.design.JRDesignExpression;
+
+/**
+ * This custom version of the standard {@link WItemProperty} is meant to be used
+ * with boolean widgets where it is not possible to rely on the standard value string
+ * for the property value.
+ * <p>
+ * 
+ * In this case a custom expression (true/false) is generated whenever the simple
+ * mode interface is used to set the property value itself.
+ * 
+ * @author Massimo Rabbi (mrabbi@users.sourceforge.net)
+ *
+ */
+public class WBooleanItemProperty extends WItemProperty {
+
+	public WBooleanItemProperty(Composite parent, int style, ItemPropertyDescription<?> widgetDescriptor,
+			IPropertyEditor editor) {
+		super(parent, style, widgetDescriptor, editor);
+	}
+
+	public WBooleanItemProperty(Composite parent, int style, WidgetPropertyDescriptor descriptor,
+			ItemPropertyDescription<?> widgetDescriptor, IPropertyEditor editor) {
+		super(parent, style, descriptor, widgetDescriptor, editor);
+	}
+	
+	@Override
+	public boolean isExpressionMode() {
+		if(isCustomSimpleMode()){
+			return false;
+		}
+		else {
+			return getPropertyEditor().getPropertyValueExpression(getPropertyName()) != null;			
+		}
+	}
+	
+	@Override
+	public String getStaticValue() {
+		if(isCustomSimpleMode()){
+			JRExpression expressionValue = getExpressionValue();
+			if(expressionValue!=null && "false".equals(expressionValue.getText())){
+				return "false";
+			}
+			if(expressionValue!=null && "true".equals(expressionValue.getText())){
+				return "true";
+			}
+		}
+		return super.getStaticValue();
+	}
+	
+	private boolean isCustomSimpleMode(){
+		String customSimpleMode = getPropertyEditor().getPropertyValue(getPropertyName()+".customSimpleMode");
+		return "true".equals(customSimpleMode);
+	}
+
+	@Override
+	public void setValue(String staticValue, JRExpression expressionValue) {
+		setRefresh(true);
+		try {
+			if(staticValue!=null){
+				String simpleMode = getPropertyName() + ".customSimpleMode";
+				getPropertyEditor().createUpdateProperty(simpleMode, "true", null);
+				expressionValue=new JRDesignExpression(staticValue);
+				staticValue=null;
+			}
+			getPropertyEditor().createUpdateProperty(getPropertyName(), staticValue, expressionValue);
+			updateWidget();
+			// Notifies the listeners of the new expression
+			fireModifyEvent(staticValue, expressionValue);
+		} finally {
+			setRefresh(false);
+		}
+	}
+}
