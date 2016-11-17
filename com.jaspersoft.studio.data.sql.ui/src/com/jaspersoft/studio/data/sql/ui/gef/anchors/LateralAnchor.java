42bf01d5baa18364ae09054ce44b2d0340937b43
mrabbi <mrabbi@tibco.com>
2016-11-17 09:38:36 +0000

    Updated copyright headers.
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@7371 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.data.sql.ui/src/com/jaspersoft/studio/data/sql/ui/gef/anchors/LateralAnchor.java
+++ com.jaspersoft.studio.data.sql.ui/src/com/jaspersoft/studio/data/sql/ui/gef/anchors/LateralAnchor.java
@@ -1,3 +1,7 @@
+/*******************************************************************************
+ * Copyright (C) 2010 - 2016. TIBCO Software Inc. 
+ * All Rights Reserved. Confidential & Proprietary.
+ ******************************************************************************/
 package com.jaspersoft.studio.data.sql.ui.gef.anchors;
 
 import org.eclipse.draw2d.AbstractConnectionAnchor;
