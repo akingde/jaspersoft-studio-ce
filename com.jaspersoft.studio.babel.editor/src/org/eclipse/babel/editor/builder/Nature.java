fc7d32bfaf301866120cef3edb6e425c63af22af
morlandin <morlandin@tibco.com>
2017-05-16 14:57:39 +0000

    fixed http://community.jaspersoft.com/jaspersoft-studio/issues/9471
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@7689 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.babel.editor/src/org/eclipse/babel/editor/builder/Nature.java
+++ com.jaspersoft.studio.babel.editor/src/org/eclipse/babel/editor/builder/Nature.java
@@ -10,9 +10,7 @@
  ******************************************************************************/
 package org.eclipse.babel.editor.builder;
 
-import org.eclipse.core.resources.ICommand;
 import org.eclipse.core.resources.IProject;
-import org.eclipse.core.resources.IProjectDescription;
 import org.eclipse.core.resources.IProjectNature;
 import org.eclipse.core.runtime.CoreException;
 
@@ -31,22 +29,7 @@ public class Nature implements IProjectNature {
      * @see org.eclipse.core.resources.IProjectNature#configure()
      */
     public void configure() throws CoreException {
-        IProjectDescription desc = project.getDescription();
-        ICommand[] commands = desc.getBuildSpec();
 
-        for (int i = 0; i < commands.length; ++i) {
-            if (commands[i].getBuilderName().equals(Builder.BUILDER_ID)) {
-                return;
-            }
-        }
-
-        ICommand[] newCommands = new ICommand[commands.length + 1];
-        System.arraycopy(commands, 0, newCommands, 0, commands.length);
-        ICommand command = desc.newCommand();
-        command.setBuilderName(Builder.BUILDER_ID);
-        newCommands[newCommands.length - 1] = command;
-        desc.setBuildSpec(newCommands);
-        project.setDescription(desc, null);
     }
 
     /*
@@ -55,18 +38,7 @@ public class Nature implements IProjectNature {
      * @see org.eclipse.core.resources.IProjectNature#deconfigure()
      */
     public void deconfigure() throws CoreException {
-        IProjectDescription description = getProject().getDescription();
-        ICommand[] commands = description.getBuildSpec();
-        for (int i = 0; i < commands.length; ++i) {
-            if (commands[i].getBuilderName().equals(Builder.BUILDER_ID)) {
-                ICommand[] newCommands = new ICommand[commands.length - 1];
-                System.arraycopy(commands, 0, newCommands, 0, i);
-                System.arraycopy(commands, i + 1, newCommands, i,
-                        commands.length - i - 1);
-                description.setBuildSpec(newCommands);
-                return;
-            }
-        }
+
     }
 
     /*
