3365d378c6c3233b134696948b851f4ae9345e25
chicuslavic <chicuslavic@tibco.com>
2017-06-06 09:20:42 +0000

    JSS-1586: [case #1477321] JSS compile button should save the current report and then compile it
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@7742 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/editor/action/CompileAction.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/editor/action/CompileAction.java
@@ -28,6 +28,7 @@ import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
 import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.ISaveablePart;
 import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.dialogs.PreferencesUtil;
 
@@ -59,7 +60,8 @@ public class CompileAction extends SelectionAction implements IMenuCreator {
 	public static final String ID = "compileAction"; //$NON-NLS-1$
 
 	/**
-	 * Drop down menu of the action, where the backward compatibility options are shown
+	 * Drop down menu of the action, where the backward compatibility options
+	 * are shown
 	 */
 	private Menu menu;
 
@@ -86,6 +88,13 @@ public class CompileAction extends SelectionAction implements IMenuCreator {
 			Job job = new Job(Messages.CompileAction_jobName) {
 				@Override
 				protected IStatus run(IProgressMonitor monitor) {
+					IWorkbenchPart part = getWorkbenchPart();
+					if (part != null && part.getSite() != null && part.getSite().getPage() != null
+							&& part.getSite().getPage().getActiveEditor() != null) {
+						IEditorPart editor = part.getSite().getPage().getActiveEditor();
+						if (editor instanceof ISaveablePart && ((ISaveablePart) editor).isDirty())
+							editor.doSave(monitor);
+					}
 					IStatus status = actionCompile(jConfig, monitor, true, console);
 					return status;
 				}
@@ -96,22 +105,25 @@ public class CompileAction extends SelectionAction implements IMenuCreator {
 	}
 
 	/**
-	 * Execute the action of compilation of the report and its subreports, adding the errors to the error view and various
-	 * information to the console.
+	 * Execute the action of compilation of the report and its subreports,
+	 * adding the errors to the error view and various information to the
+	 * console.
 	 * 
 	 * @param jConfig
-	 *          jasper configuration of the compiled report
+	 *            jasper configuration of the compiled report
 	 * @param monitor
-	 *          monitor for the execution
+	 *            monitor for the execution
 	 * @param compileMain
-	 *          true if the main file need to be compiled, false to compile only the subreports
+	 *            true if the main file need to be compiled, false to compile
+	 *            only the subreports
 	 * @param console
-	 *          console where print info and errors
-	 * @return Status.OK_STATUS if the compilation finished without exception (this dosen't means that the report hasen't
-	 *         Compilation error), Status.CANCEL_STATUS otherwise
+	 *            console where print info and errors
+	 * @return Status.OK_STATUS if the compilation finished without exception
+	 *         (this dosen't means that the report hasen't Compilation error),
+	 *         Status.CANCEL_STATUS otherwise
 	 */
-	public IStatus actionCompile(final JasperReportsConfiguration jConfig, IProgressMonitor monitor, boolean compileMain,
-			final Console console) {
+	public IStatus actionCompile(final JasperReportsConfiguration jConfig, IProgressMonitor monitor,
+			boolean compileMain, final Console console) {
 
 		IFile mfile = (IFile) jConfig.get(FileUtils.KEY_FILE);
 		if (mfile != null) {
@@ -120,7 +132,8 @@ public class CompileAction extends SelectionAction implements IMenuCreator {
 					console.addMessage(MessageFormat.format(Messages.CompileAction_consoleMessage1, mfile.getName()));
 				}
 
-				// Use a custom builder that redirect the output errors found during the compilation process to the console
+				// Use a custom builder that redirect the output errors found
+				// during the compilation process to the console
 				JasperReportsBuilder builder = new JasperReportsBuilder() {
 
 					@Override
@@ -131,15 +144,19 @@ public class CompileAction extends SelectionAction implements IMenuCreator {
 					};
 				};
 
-				// ATTENTION! this can generate possible errors, because we are not calling builders in the right order
-				// we are also not looking very good for for subreports, because expression evaluation is not good
-				// file.getProject().build(IncrementalProjectBuilder.INCREMENTAL_BUILD, monitor);
+				// ATTENTION! this can generate possible errors, because we are
+				// not calling builders in the right order
+				// we are also not looking very good for for subreports, because
+				// expression evaluation is not good
+				// file.getProject().build(IncrementalProjectBuilder.INCREMENTAL_BUILD,
+				// monitor);
 				if (compileMain) {
 					IFile destFIle = builder.compileJRXML(mfile, monitor, jConfig);
 					if (console != null && destFIle != null) {
 						File file = destFIle.getRawLocation().toFile();
 						if (file.exists()) {
-							console.addMessage(MessageFormat.format(Messages.CompileAction_consoleMessage2, file.toString()));
+							console.addMessage(
+									MessageFormat.format(Messages.CompileAction_consoleMessage2, file.toString()));
 						} else {
 							console.addMessage(Messages.CompileAction_consoleMessage3);
 						}
@@ -171,10 +188,12 @@ public class CompileAction extends SelectionAction implements IMenuCreator {
 	}
 
 	/**
-	 * Return the output console for the current opened editor, the console is cleaned before the return. If the console
-	 * can not be found the null is returned
+	 * Return the output console for the current opened editor, the console is
+	 * cleaned before the return. If the console can not be found the null is
+	 * returned
 	 * 
-	 * @return reference to a clean console of the current editor or null if it is not available
+	 * @return reference to a clean console of the current editor or null if it
+	 *         is not available
 	 */
 	protected Console getCleanConsole() {
 		AbstractJRXMLEditor editor = (AbstractJRXMLEditor) SelectionHelper.getActiveJRXMLEditor();
@@ -222,18 +241,23 @@ public class CompileAction extends SelectionAction implements IMenuCreator {
 		return true;
 	}
 
-	public static IStatus doRun(final JasperReportsConfiguration jConfig, IProgressMonitor monitor, boolean compileMain) {
+	public static IStatus doRun(final JasperReportsConfiguration jConfig, IProgressMonitor monitor,
+			boolean compileMain) {
 		IFile mfile = (IFile) jConfig.get(FileUtils.KEY_FILE);
 		if (mfile != null)
 			try {
-				// ATTENTION! this can generate possible errors, because we are not calling builders in the right order
-				// we are also not looking very good for for subreports, because expression evaluation is not good
-				// file.getProject().build(IncrementalProjectBuilder.INCREMENTAL_BUILD, monitor);
+				// ATTENTION! this can generate possible errors, because we are
+				// not calling builders in the right order
+				// we are also not looking very good for for subreports, because
+				// expression evaluation is not good
+				// file.getProject().build(IncrementalProjectBuilder.INCREMENTAL_BUILD,
+				// monitor);
 
 				JasperReportsBuilder builder = new JasperReportsBuilder();
 				if (compileMain)
 					builder.compileJRXML(mfile, monitor, jConfig);
-				Map<File, IFile> fmap = SubreportsUtil.getSubreportFiles(jConfig, mfile, jConfig.getJasperDesign(), monitor);
+				Map<File, IFile> fmap = SubreportsUtil.getSubreportFiles(jConfig, mfile, jConfig.getJasperDesign(),
+						monitor);
 				for (File f : fmap.keySet()) {
 					IFile file = fmap.get(f);
 					if (file != null) {
