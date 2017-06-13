e583abcd063c1057ce2478517d60952443865da9
chicuslavic <chicuslavic@tibco.com>
2017-06-13 13:07:06 +0000

    JSS-602: JSS - repository explorer - unable to paste jar inside a report unit till you open it
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@7762 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/action/resource/CutResourceAction.java
+++ com.jaspersoft.studio.server/src/com/jaspersoft/studio/server/action/resource/CutResourceAction.java
@@ -7,6 +7,10 @@ package com.jaspersoft.studio.server.action.resource;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.gef.ui.actions.Clipboard;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.viewers.TreePath;
@@ -16,9 +20,14 @@ import org.eclipse.ui.ISharedImages;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.actions.ActionFactory;
 
+import com.jaspersoft.jasperserver.api.metadata.xml.domain.impl.ResourceDescriptor;
 import com.jaspersoft.studio.messages.Messages;
+import com.jaspersoft.studio.model.ANode;
+import com.jaspersoft.studio.server.ResourceFactory;
 import com.jaspersoft.studio.server.model.AMResource;
 import com.jaspersoft.studio.server.model.MAdHocDataView;
+
+import net.sf.jasperreports.eclipse.ui.util.UIUtils;
 
 public class CutResourceAction extends Action {
 	private TreeViewer treeViewer;
@@ -53,17 +62,50 @@ public class CutResourceAction extends Action {
 	@Override
 	public void run() {
 		TreeSelection s = (TreeSelection) treeViewer.getSelection();
-		TreePath[] p = s.getPaths();
-		List<AMResource> rlist = new ArrayList<AMResource>();
-		for (int i = 0; i < p.length; i++) {
-			final Object obj = p[i].getLastSegment();
-			if (obj instanceof AMResource) {
-				System.out.println(((AMResource) obj).getToolTip());
-				((AMResource) obj).setCut(true);
-				rlist.add((AMResource) obj);
+		final TreePath[] p = s.getPaths();
+		final List<AMResource> rlist = new ArrayList<AMResource>();
+		Job job = new Job("Copy resources") {
+			@Override
+			protected IStatus run(IProgressMonitor monitor) {
+				monitor.beginTask("Copy resources", IProgressMonitor.UNKNOWN);
+				try {
+					for (int i = 0; i < p.length; i++) {
+						Object obj = p[i].getLastSegment();
+						if (obj instanceof AMResource) {
+							AMResource amres = (AMResource) obj;
+							if (amres.getValue().getWsType().equals(ResourceDescriptor.TYPE_CONTENT_RESOURCE))
+								try {
+									ResourceDescriptor rd = amres.getWsClient().get(monitor, amres.getValue(), null);
+									amres.setValue(rd);
+									ANode parent = amres.getParent();
+									int indx = parent.getChildren().indexOf(obj);
+									parent.removeChild(amres);
+									obj = ResourceFactory.getResource(parent, rd, indx);
+								} catch (Exception e) {
+									e.printStackTrace();
+								}
+							((AMResource) obj).setCut(true);
+							rlist.add((AMResource) obj);
+						}
+						if (monitor.isCanceled())
+							return Status.CANCEL_STATUS;
+					}
+					UIUtils.getDisplay().syncExec(new Runnable() {
+
+						@Override
+						public void run() {
+							if (!rlist.isEmpty())
+								Clipboard.getDefault().setContents(rlist);
+							treeViewer.refresh();
+						}
+					});
+				} finally {
+					monitor.done();
+				}
+				return Status.OK_STATUS;
 			}
-		}
-		if (!rlist.isEmpty())
-			Clipboard.getDefault().setContents(rlist);
+		};
+		job.setPriority(Job.LONG);
+		job.schedule();
 	}
 }
