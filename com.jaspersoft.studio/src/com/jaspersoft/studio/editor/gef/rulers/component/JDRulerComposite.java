953db4b36c1b0d6639a7ee19ce7fe4e04f3d4190
morlandin <morlandin@tibco.com>
2017-08-01 08:30:52 +0000

    fixed more reliable way to obtain the display
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@7824 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/editor/gef/rulers/component/JDRulerComposite.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/editor/gef/rulers/component/JDRulerComposite.java
@@ -49,6 +49,8 @@ import com.jaspersoft.studio.editor.gef.rulers.JDRulerContextMenuProvider;
 import com.jaspersoft.studio.editor.gef.rulers.ReportRuler;
 import com.jaspersoft.studio.editor.report.AbstractVisualEditor;
 
+import net.sf.jasperreports.eclipse.ui.util.UIUtils;
+
 public class JDRulerComposite extends Composite {
 
 	private EditDomain rulerEditDomain;
@@ -311,22 +313,22 @@ public class JDRulerComposite extends Composite {
 					RulerProvider rh = (RulerProvider) diagramViewer.getProperty(RulerProvider.PROPERTY_HORIZONTAL_RULER);
 					if (rh != null)
 						((ReportRuler) rh.getRuler()).setHoffset((Integer) evt.getNewValue());
-					Display.getCurrent().asyncExec(runnable);
+					UIUtils.getDisplay().asyncExec(runnable);
 				} else if (property.equals(ReportRuler.PROPERTY_HEND)) { //$NON-NLS-1$
 					RulerProvider rh = (RulerProvider) diagramViewer.getProperty(RulerProvider.PROPERTY_HORIZONTAL_RULER);
 					if (rh != null)
 						((ReportRuler) rh.getRuler()).setHend((Integer) evt.getNewValue());
-					Display.getCurrent().asyncExec(runnable);
+					UIUtils.getDisplay().asyncExec(runnable);
 				} else if (property.equals(ReportRuler.PROPERTY_VOFFSET)) { //$NON-NLS-1$
 					RulerProvider rh = (RulerProvider) diagramViewer.getProperty(RulerProvider.PROPERTY_VERTICAL_RULER);
 					if (rh != null)
 						((ReportRuler) rh.getRuler()).setVoffset((Integer) evt.getNewValue());
-					Display.getCurrent().asyncExec(runnable);
+					UIUtils.getDisplay().asyncExec(runnable);
 				} else if (property.equals(ReportRuler.PROPERTY_VEND)) { //$NON-NLS-1$
 					RulerProvider rh = (RulerProvider) diagramViewer.getProperty(RulerProvider.PROPERTY_VERTICAL_RULER);
 					if (rh != null)
 						((ReportRuler) rh.getRuler()).setVend((Integer) evt.getNewValue());
-					Display.getCurrent().asyncExec(runnable);
+					UIUtils.getDisplay().asyncExec(runnable);
 				}
 
 				if (RulerProvider.PROPERTY_HORIZONTAL_RULER.equals(property)) {
@@ -371,7 +373,7 @@ public class JDRulerComposite extends Composite {
 		if (container.getContents() != ruler) {
 			container.setContents(ruler);
 			needToLayout = true;
-			Display.getCurrent().asyncExec(runnable);
+			UIUtils.getDisplay().asyncExec(runnable);
 		}
 	}
 
