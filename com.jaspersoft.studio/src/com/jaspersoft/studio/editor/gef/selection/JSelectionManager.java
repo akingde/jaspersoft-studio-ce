8aab3beedf63d0856f6f02ea5c914e9b107093dc
morlandin <morlandin@tibco.com>
2017-11-06 12:12:26 +0000

    added group selection reviewer: mrabbi@tibco.com
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@8049 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/editor/gef/selection/JSelectionManager.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/editor/gef/selection/JSelectionManager.java
@@ -8,8 +8,13 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+import org.eclipse.draw2d.IFigure;
 import org.eclipse.gef.EditPart;
+import org.eclipse.gef.EditPartViewer;
+import org.eclipse.gef.LayerConstants;
 import org.eclipse.gef.SelectionManager;
+import org.eclipse.gef.editparts.LayerManager;
+import org.eclipse.gef.editparts.ZoomManager;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
@@ -18,16 +23,65 @@ import com.jaspersoft.studio.JSSCompoundCommand;
 import com.jaspersoft.studio.editor.gef.parts.FigureEditPart;
 import com.jaspersoft.studio.editor.gef.parts.band.BandEditPart;
 import com.jaspersoft.studio.model.ANode;
+import com.jaspersoft.studio.preferences.bindings.BindingsPreferencePersistence;
+
+import net.sf.jasperreports.eclipse.IKeyboardEvent;
+import net.sf.jasperreports.eclipse.JasperReportsPlugin;
 
 /**
  * Class that extend the default SelectionManager to change the behavior when a selection of more elements 
- * is done
+ * is done. Also it allow to handle a group selection
+ * 
  * @author Orlandin Marco
  *
  */
 public class JSelectionManager extends SelectionManager {
 	
 	private static EditPart lastSelected = null;
+	
+	/**
+	 * The current selection, this list is a reference to the same list used in the superclass.
+	 * The superclass selection list is private but it is initialized in the interalInitialize method,
+	 * keeping a copy of the reference allow to modify the content of the list from this class
+	 */
+	private List<Object> selection;
+	
+	/**
+	 * The edit part shown when the user is doing a group selection
+	 */
+	private GroupSelectionEditPart selectionPart;
+	
+	/**
+	 * The keyboard handler used to show or hide a group selection
+	 */
+	private IKeyboardEvent keyboardEvent = new IKeyboardEvent() {
+		
+		@Override
+		public void keyUp(int keycode) {
+			if (getGroupSelectionPart().isActive() && !BindingsPreferencePersistence.isPressed(GroupSelectionEditPart.BINDING_KEY_ID)) {
+				setSelection(new StructuredSelection(selection));
+			}
+		}
+		
+		@Override
+		public void keyDown(int keycode) {
+			if (BindingsPreferencePersistence.isPressed(GroupSelectionEditPart.BINDING_KEY_ID)) {
+				setSelection(new StructuredSelection(selection));
+			}
+			
+		}
+	};
+	
+	/**
+	 * Initialize the selection manager adding the keyboard listener and coyping the selection
+	 * list reference
+	 */
+	@Override
+	public void internalInitialize(EditPartViewer viewer, List selection, Runnable notifier) {
+		super.internalInitialize(viewer, selection, notifier);
+		this.selection = selection;
+		JasperReportsPlugin.addKeyListener(keyboardEvent);
+	}
 	
 	/**
 	 * Take a node from the selection check if for its model the refresh is ignored
@@ -78,10 +132,111 @@ public class JSelectionManager extends SelectionManager {
 	}
 	
 	/**
+	 * Return the group selection edit part. The part is created in a lazy way so if it was not
+	 * created it is created now and returned, otherwise it is simply returned
+	 * 
+	 * @return a not null {@link GroupSelectionEditPart}
+	 */
+	protected GroupSelectionEditPart getGroupSelectionPart() {
+		if (selectionPart == null) {
+			//initialize the zoom manager of the edit part
+			ZoomManager zoomManager = (ZoomManager)getViewer().getProperty(ZoomManager.class.toString());
+			selectionPart = new GroupSelectionEditPart(this, zoomManager);
+		}
+		return selectionPart;
+	}
+	
+	/**
+	 * Sets the selection with a group selection 
+	 * 
+	 * @param newSelection the new selection
+	 */
+	public void setGroupSelection(ISelection newSelection) {
+		if (!(newSelection instanceof IStructuredSelection))
+			return;
+
+		List<?> orderedSelection = ((IStructuredSelection) newSelection).toList();
+
+		getViewer().setFocus(null);
+		
+		List<Object> clearedSelection = new ArrayList<Object>();
+		for(Object selectedElement : orderedSelection) {
+			if (!(selectedElement instanceof BandEditPart)) {
+				clearedSelection.add(selectedElement);
+			}
+		}
+		
+		selection.clear();
+		selection.addAll(clearedSelection);
+
+		if (!clearedSelection.isEmpty()) {
+			EditPart firstPart = (EditPart)clearedSelection.get(0);
+			IFigure layer = LayerManager.Helper.find(firstPart).getLayer(LayerConstants.HANDLE_LAYER);
+			getGroupSelectionPart().updateBounds(true);
+			layer.add(getGroupSelectionPart().getFigure());
+			getGroupSelectionPart().createSelectionHandles(layer);
+		}
+		fireSelectionChanged();
+	}
+	
+	/**
+	 * Erase the feedback on the passed selection
+	 * 
+	 * @param selection the selection from where the feedback must be remvoed
+	 */
+	public void eraseFeedback(List<?> selection) {
+		EditPart part;
+		// Fix for 458416: adjust the focus through the viewer only (to give
+		// AbstractEditPartViewer a change to update its focusPart field).
+		// AbstractEditPartViewer#setFocus() should call back setFocus(null)
+		// here, so both focus part values should stay in sync.
+		getViewer().setFocus(null);
+		for (int i = 0; i < selection.size(); i++) {
+			part = (EditPart) selection.get(i);
+			part.setSelected(EditPart.SELECTED_NONE);
+		}
+	}
+	
+	/**
+	 * Append an edit part to the current selection. Check also if the group key is pressed
+	 */
+	@Override
+	public void appendSelection(EditPart editpart) {
+		getGroupSelectionPart().clear();
+		if(BindingsPreferencePersistence.isPressed(GroupSelectionEditPart.BINDING_KEY_ID)) {
+			List<Object> newSelectionList = new ArrayList<Object>(selection);
+			if (!newSelectionList.contains(editpart)) {
+				newSelectionList.add(editpart);
+			}
+			IStructuredSelection newSelection = removeUnselectableParts(newSelectionList);
+			if (newSelection.size() > 1) {
+				eraseFeedback(selection);
+				setGroupSelection(newSelection);
+				return;
+			}	
+		}
+		super.appendSelection(editpart);
+	}
+	
+	@Override
+	public void deselectAll() {
+		getGroupSelectionPart().clear();
+		super.deselectAll();
+	}
+	
+	/**
+	 * Check if for the current selection should be shown a group selection. This is done looking if the current
+	 * selection is bigger than one element and if the key for the group selection is pressed
+	 */
+	protected boolean isGroupSelection(IStructuredSelection selection) {
+		return BindingsPreferencePersistence.isPressed(GroupSelectionEditPart.BINDING_KEY_ID) && selection.size() > 1;
+	}
+	
+	/**
 	 * Sets the selection, override the original method to store and give the status of selected primary
 	 * to the item that was primary before the setSelection. If the element isn't in the new selected items
 	 * the default behavior will be used (the primary element will be the last on the list of the new selected 
-	 * items).
+	 * items). It also handle the case where a group selection is done.
 	 * 
 	 * @param newSelection
 	 *            the new selection
@@ -89,6 +244,7 @@ public class JSelectionManager extends SelectionManager {
 	 */
 	@Override
 	public void setSelection(ISelection newSelection) {
+		getGroupSelectionPart().clear();
 		if (!(newSelection instanceof IStructuredSelection))
 			return;
 		List<?> orderedSelection = ((IStructuredSelection) newSelection).toList();
@@ -109,18 +265,30 @@ public class JSelectionManager extends SelectionManager {
 				if (lastSelected != null && !orderedSelection.contains(lastSelected)){
 					lastSelected = null;
 				} 
-				super.setSelection(removeUnselectableParts(orderedSelection));
+				IStructuredSelection cleanSelection = removeUnselectableParts(orderedSelection);
+				if (isGroupSelection(cleanSelection)) {
+					eraseFeedback(orderedSelection);
+					setGroupSelection(cleanSelection);
+					//If even the last selected element wasn't in the selection the first element of the selection will be chosen
+					if (lastSelected == null){
+						lastSelected = (EditPart)orderedSelection.get(0);
+					}
+					
+					return;
+				} else {
+					super.setSelection(cleanSelection);
+				}
 				//If even the last selected element wasn't in the selection the first element of the selection will be chosen
 				if (lastSelected == null){
 					lastSelected = (EditPart)orderedSelection.get(0);
 				}
 				
+				if (lastSelected.isSelectable()) lastSelected.setSelected(EditPart.SELECTED_PRIMARY);
 				itr = orderedSelection.iterator();
 				while (itr.hasNext()) {
 						part = (EditPart) itr.next();
-						if (part.isSelectable()) part.setSelected(EditPart.SELECTED);
+						if (part.isSelectable() && part != lastSelected) part.setSelected(EditPart.SELECTED);
 				}
-				if (lastSelected.isSelectable()) lastSelected.setSelected(EditPart.SELECTED_PRIMARY);
 			}
 		}else {
 			super.deselectAll();
