b7ea560c40e0a16a2d17b0ec16f5ab53e4066310
mrabbi <mrabbi@tibco.com>
2017-11-27 13:27:51 +0000

    Fix for Community Bug #10151. Changed URLs to https for community references. Reviewer: morlandi@tibco.com
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@8078 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.community/src/com/jaspersoft/studio/community/CommunityConstants.java
+++ com.jaspersoft.studio.community/src/com/jaspersoft/studio/community/CommunityConstants.java
@@ -12,16 +12,16 @@ package com.jaspersoft.studio.community;
  */
 public final class CommunityConstants {
 
-	public static final String LOGIN_URL = "http://community.jaspersoft.com/community-api/user/login"; //$NON-NLS-1$
-	public static final String FILE_UPLOAD_URL = "http://community.jaspersoft.com/community-api/file.json"; //$NON-NLS-1$
-	public static final String ISSUE_CREATION_URL = "http://community.jaspersoft.com/community-api/node.json"; //$NON-NLS-1$
+	public static final String LOGIN_URL = "https://community.jaspersoft.com/community-api/user/login"; //$NON-NLS-1$
+	public static final String FILE_UPLOAD_URL = "https://community.jaspersoft.com/community-api/file.json"; //$NON-NLS-1$
+	public static final String ISSUE_CREATION_URL = "https://community.jaspersoft.com/community-api/node.json"; //$NON-NLS-1$
 	public static final String JSON_CONTENT_TYPE = "application/json"; //$NON-NLS-1$
 	public static final String REQUEST_CHARSET = "UTF-8"; //$NON-NLS-1$
 	public static final String SECURE_PREFSTORE_PATHNAME = "jaspersoft_community_site"; //$NON-NLS-1$
 	public static final int JSSPROJECT_COMMUNITY_ID = 496;
-	public static final String NODE_CONTENT_URL_PREFIX = "http://community.jaspersoft.com/community-api/node/"; //$NON-NLS-1$
+	public static final String NODE_CONTENT_URL_PREFIX = "https://community.jaspersoft.com/community-api/node/"; //$NON-NLS-1$
 	public static final String ISSUE_SUBMISSION_WIZARD_IMG = "resources/images/softwareBug.png"; //$//$NON-NLS-1$
-	public static final String NEW_COMMUNITY_TRACKER_ACCOUNT_URL = "http://community.jaspersoft.com/modal_forms/nojs/login"; //$//$NON-NLS-1$
+	public static final String NEW_COMMUNITY_TRACKER_ACCOUNT_URL = "https://community.jaspersoft.com/modal_forms/nojs/login"; //$//$NON-NLS-1$
 	
 	private CommunityConstants(){
 		// prevent instantiation...
