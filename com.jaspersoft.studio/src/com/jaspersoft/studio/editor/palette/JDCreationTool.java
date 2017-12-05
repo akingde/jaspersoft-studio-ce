ddab9c6cd645dbea5f0e132bc73c740f44325cf4
morlandin <morlandin@tibco.com>
2017-12-05 14:14:59 +0000

    fixed http://jira.jaspersoft.com/browse/JSS-680 #JSS-680 reviewer: mrabbi@tibco.com
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@8100 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio/src/com/jaspersoft/studio/editor/palette/JDCreationTool.java
+++ com.jaspersoft.studio/src/com/jaspersoft/studio/editor/palette/JDCreationTool.java
@@ -4,6 +4,7 @@
  ******************************************************************************/
 package com.jaspersoft.studio.editor.palette;
 
+import org.eclipse.gef.EditPart;
 import org.eclipse.gef.SharedCursors;
 import org.eclipse.gef.commands.Command;
 import org.eclipse.gef.requests.CreationFactory;
@@ -13,7 +14,9 @@ import org.eclipse.swt.graphics.Cursor;
 import org.eclipse.wb.swt.ResourceManager;
 
 import com.jaspersoft.studio.JaspersoftStudioPlugin;
+import com.jaspersoft.studio.editor.gef.parts.FigureEditPart;
 import com.jaspersoft.studio.model.DialogEnabledCommand;
+import com.jaspersoft.studio.model.IContainer;
 
 /**
  * Custom creation tool that add support for dialog prompted during the creation of an element dragged from the palette.
@@ -59,5 +62,45 @@ public final class JDCreationTool extends CreationTool {
 			setCurrentCommand(null);
 		}
 		super.performCreation(button);
+	}
+	
+	/**
+	 * Take an edit part and search it's container if it is not a container.
+	 * 
+	 * @return the container of the actual target edit part, if the target edit part is not a container, or the target
+	 *         edit part itself otherwise
+	 */
+	private EditPart getContainer(EditPart target) {
+		if (target instanceof FigureEditPart) {
+			return ((FigureEditPart)target).getDropContainer();
+		} else if (target instanceof IContainer) {
+			return target;
+		} else {
+			return FigureEditPart.getParentEditPart(target);
+		}
+	}
+	
+	
+	/**
+	 * Updates the target editpart and returns <code>true</code> if the target
+	 * changes. The target is updated by using the target conditional and the
+	 * target request. If the target has been locked, this method does nothing
+	 * and returns <code>false</code>.
+	 * 
+	 * @return <code>true</code> if the target was changed
+	 */
+	protected boolean updateTargetUnderMouse() {
+		if (!isTargetLocked()) {
+			EditPart editPart = null;
+			if (getCurrentViewer() != null)
+				editPart = getCurrentViewer().findObjectAtExcluding(getLocation(), getExclusionSet(),getTargetingConditional());
+			if (editPart != null)
+				editPart = editPart.getTargetEditPart(getTargetRequest());
+			EditPart container = getContainer(editPart);
+			boolean changed = getTargetEditPart() != container;
+			setTargetEditPart(container);
+			return changed;
+		} else
+			return false;
 	}
 }
