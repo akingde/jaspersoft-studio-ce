b6b71c590ed6d519be9c16c8d894c4781b0f9221
morlandin <morlandin@tibco.com>
2017-12-12 14:21:38 +0000

    fixed http://jira.jaspersoft.com/browse/JSS-683 #JSS-683 reviewer: mrabbi@tibco.com
    git-svn-id: http://code.jaspersoft.com/svn/repos/jaspersoftstudio/trunk@8122 6833c83e-4fd0-4c68-a3df-283c6148db30

--- com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/crosstab/model/crosstab/command/wizard/CrosstabWizard.java
+++ com.jaspersoft.studio.components/src/com/jaspersoft/studio/components/crosstab/model/crosstab/command/wizard/CrosstabWizard.java
@@ -451,47 +451,38 @@ public class CrosstabWizard extends JSSWizard {
 		if (function == AgregationFunctionEnum.UNIQUE)
 			exp.setText(oldExpText);
 		else if (function == AgregationFunctionEnum.YEAR) {
-			exp.setText("new SimpleDateFormat(\"yyyy\").format(" + oldExpText
-					+ ")");
-			bucket.setValueClassName(String.class.getName());
+			exp.setText("new SimpleDateFormat(\"yyyy\").format(" + oldExpText + ")");
+			bucket.setValueClassName(Integer.class.getName());
 		} else if (function == AgregationFunctionEnum.MONTH) {
-			exp.setText("new SimpleDateFormat(\"MM\").format("
-					+ oldExpText + ")");
-			bucket.setValueClassName(String.class.getName());
+			exp.setText("new SimpleDateFormat(\"MM\").format(" + oldExpText + ")");
+			bucket.setValueClassName(Integer.class.getName());
 		} else if (function == AgregationFunctionEnum.WEEK) {
-			exp.setText("new SimpleDateFormat(\"ww\").format("
-					+ oldExpText + ")");
-			bucket.setValueClassName(String.class.getName());
+			exp.setText("new SimpleDateFormat(\"ww\").format(" + oldExpText + ")");
+			bucket.setValueClassName(Integer.class.getName());
 		} else if (function == AgregationFunctionEnum.DAY) {
-			exp.setText("new SimpleDateFormat(\"dd\").format("
-					+ oldExpText + ")");
-			bucket.setValueClassName(String.class.getName());
+			exp.setText("new SimpleDateFormat(\"dd\").format(" + oldExpText + ")");
+			bucket.setValueClassName(Integer.class.getName());
 		} else if (function == AgregationFunctionEnum.DAY_OF_THE_WEEK) {
-			exp.setText("new SimpleDateFormat(\"u\").format("
-					+ oldExpText + ")");
-			bucket.setValueClassName(String.class.getName());
+			exp.setText("new SimpleDateFormat(\"u\").format(" + oldExpText + ")");
+			bucket.setValueClassName(Integer.class.getName());
 		} else if (function == AgregationFunctionEnum.HOUR) {
-			exp.setText("new SimpleDateFormat(\"HH\").format("
-					+ oldExpText + ")");
-			bucket.setValueClassName(String.class.getName());
+			exp.setText("new SimpleDateFormat(\"HH\").format(" + oldExpText + ")");
+			bucket.setValueClassName(Integer.class.getName());
 		} else if (function == AgregationFunctionEnum.MINUTE) {
-			exp.setText("new SimpleDateFormat(\"mm\").format("
-					+ oldExpText + ")");
-			bucket.setValueClassName(String.class.getName());
+			exp.setText("new SimpleDateFormat(\"mm\").format(" + oldExpText + ")");
+			bucket.setValueClassName(Integer.class.getName());
 		} else if (function == AgregationFunctionEnum.SECOND) {
-			exp.setText("new SimpleDateFormat(\"ss\").format("
-					+ oldExpText + ")");
-			bucket.setValueClassName(String.class.getName());
+			exp.setText("new SimpleDateFormat(\"ss\").format(" + oldExpText + ")");
+			bucket.setValueClassName(Integer.class.getName());
 		} else if (function == AgregationFunctionEnum.MILLISECOND) {
-			exp.setText("new SimpleDateFormat(\"SSSS\").format("
-					+ oldExpText + ")");
-			bucket.setValueClassName(String.class.getName());
+			exp.setText("new SimpleDateFormat(\"SSSS\").format(" + oldExpText + ")");
+			bucket.setValueClassName(Integer.class.getName());
 		} else if (function == AgregationFunctionEnum.QUARTER) {
 			exp.setText("(Integer.parseInt(new SimpleDateFormat(\"MM\").format(" + oldExpText + "))/4)+1");
-			bucket.setValueClassName(String.class.getName());
+			bucket.setValueClassName(Integer.class.getName());
 		} else if (function == AgregationFunctionEnum.SEMESTER) {
 			exp.setText("(Integer.parseInt(new SimpleDateFormat(\"MM\").format(" + oldExpText + "))/6)+1");
-			bucket.setValueClassName(String.class.getName());
+			bucket.setValueClassName(Integer.class.getName());
 		}
 
 		bucket.setExpression(exp);
